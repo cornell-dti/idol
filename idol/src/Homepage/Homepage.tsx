d0d87cbf08de1fc25b61e87e8bc633473345d98a
JBoss925 <jaggerbrulato@gmail.com>
2020-09-04 02:21:35 -0400

    try get request for members

--- idol/src/Homepage/Homepage.tsx
+++ idol/src/Homepage/Homepage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './Homepage.module.css';
+import { MembersAPI } from '../API/MembersAPI';
 
 const Homepage: React.FC = () => {
+  MembersAPI.getAllMembers().then(mems => console.log(mems));
   return (
     <div className={styles.Homepage} data-testid="Homepage">
 
