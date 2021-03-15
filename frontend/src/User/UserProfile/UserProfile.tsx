c9972249625efef2df7a6d84696625b7c5eecd8f
Morgan Belous <mhb224@cornell.edu>
2021-03-15 19:13:45 -0400

    cleaned up duplicated code

--- frontend/src/User/UserProfile/UserProfile.tsx
+++ frontend/src/User/UserProfile/UserProfile.tsx
@@ -114,24 +114,16 @@ const UserProfile: React.FC = () => {
     }
   };
 
-  return role === 'admin' || role === 'lead' ? (
-    <Form
-      style={{
-        width: '80%',
-        alignSelf: 'center',
-        margin: 'auto',
-        marginTop: '10vh'
-      }}
-    >
+  let name;
+  if (role === 'admin' || role === 'lead') {
+    name = (
       <Form.Group widths="equal">
         <Form.Input
           fluid
           label="First name"
           value={firstName}
           onChange={(event) => {
-            if (role === 'admin') {
-              setFirstName(event.target.value);
-            }
+            setFirstName(event.target.value);
           }}
           required
         />
@@ -140,87 +132,21 @@ const UserProfile: React.FC = () => {
           label="Last name"
           value={lastName}
           onChange={(event) => {
-            if (role === 'admin') {
-              setLastName(event.target.value);
-            }
+            setLastName(event.target.value);
           }}
           required
         />
       </Form.Group>
+    );
+  } else {
+    name = (
+      <h2 style={{ fontFamily: 'var(--mainFontFamily)', marginBottom: '2vh' }}>
+        {firstName} {lastName}
+      </h2>
+    );
+  }
 
-      <Form.Group widths="equal">
-        <Form.Input
-          fluid
-          label="Graduation"
-          value={graduation}
-          onChange={(event) => setGraduation(event.target.value)}
-          required
-        />
-        <Form.Input
-          fluid
-          label="Hometown"
-          value={hometown}
-          onChange={(event) => setHometown(event.target.value)}
-          required
-        />
-      </Form.Group>
-
-      <Form.Group widths="equal">
-        <Form.Input
-          fluid
-          label="Major"
-          value={major}
-          onChange={(event) => setMajor(event.target.value)}
-          required
-        />
-        <Form.Input
-          fluid
-          label="Double Major"
-          value={doubleMajor || ''}
-          onChange={(event) => setDoubleMajor(event.target.value)}
-        />
-        <Form.Input
-          fluid
-          label="Minor"
-          value={minor}
-          onChange={(event) => setMinor(event.target.value)}
-        />
-      </Form.Group>
-
-      <Form.Input
-        label="About"
-        name="about"
-        value={about}
-        control={TextArea}
-        onChange={(event) => setAbout(event.target.value)}
-        style={{ minHeight: '25vh' }}
-        required
-      />
-
-      <Form.Group widths="equal">
-        <Form.Input
-          fluid
-          label="Website"
-          value={website}
-          onChange={(event) => setWebsite(event.target.value)}
-        />
-        <Form.Input
-          fluid
-          label="LinkedIn"
-          value={linkedin}
-          onChange={(event) => setLinkedin(event.target.value)}
-        />
-        <Form.Input
-          fluid
-          label="GitHub"
-          value={github}
-          onChange={(event) => setGithub(event.target.value)}
-        />
-      </Form.Group>
-
-      <Form.Button onClick={saveProfileInfo}>Save</Form.Button>
-    </Form>
-  ) : (
+  return (
     <Form
       style={{
         width: '80%',
@@ -229,9 +155,7 @@ const UserProfile: React.FC = () => {
         marginTop: '10vh'
       }}
     >
-      <h2 style={{ fontFamily: 'var(--mainFontFamily)', marginBottom: '2vh' }}>
-        {firstName} {lastName}
-      </h2>
+      {name}
 
       <Form.Group widths="equal">
         <Form.Input
