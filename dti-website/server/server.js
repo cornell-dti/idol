c94e5bc58f88f46bf3dc4beb6bba8c472b877305
Henry Li <hl738@cornell.edu>
2021-03-23 17:48:40 -0400

    remove file

--- dti-website/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, './../out')));
-
-app.listen(3000, () => console.log('DTI Website is listening on PORT 3000'));
