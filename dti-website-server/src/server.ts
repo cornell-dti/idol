ac9fccdca0c0fccf1828a0dc1dd2ff700b88e440
JBoss925 <jaggerbrulato@gmail.com>
2021-03-24 04:11:46 -0400

    fix incorrect path

--- dti-website-server/src/server.ts
+++ dti-website-server/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 8000;
 
-router.use(express.static(path.join(__dirname, '../../dti-website/out/index.html')));
+router.use(express.static(path.join(__dirname, '../../dti-website/out/')));
 
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../template/index.html')));
 app.use('/.netlify/functions/server', router);
