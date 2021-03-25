4f7730631defc5b59038cd4dd8fc4d42b61cd7fc
Sam <sam@developersam.com>
2021-03-25 12:48:53 -0400

    Setup a CI cron job to update idol member json

--- dti-website/pull-from-idol.ts
+++ dti-website/pull-from-idol.ts
@@ -3,9 +3,10 @@
 /* eslint-disable no-console */
 
 import { spawnSync } from 'child_process';
-import { writeFileSync } from 'fs';
+import { readFileSync, unlinkSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+import { Octokit } from '@octokit/rest';
 import fetch from 'node-fetch';
 
 async function getIdolMembers(): Promise<readonly IdolMember[]> {
@@ -54,14 +55,93 @@ function convertIdolMemberToNovaMember(idolMember: IdolMember): NovaMember {
   };
 }
 
+function runCommand(program: string, ...programArguments: readonly string[]) {
+  const programArgumentsQuoted = programArguments
+    .map((it) => (it.includes(' ') ? `"${it}"` : it))
+    .join(' ');
+  console.log(`> ${program} ${programArgumentsQuoted}`);
+  return spawnSync(program, programArguments, { stdio: 'inherit' });
+}
+
 async function main(): Promise<void> {
+  // Create new json
   const idolMembers = await getIdolMembers();
   const novaMembers = idolMembers.map(convertIdolMemberToNovaMember);
   const jsonPath = join('src', 'data', 'all-members.json');
-  writeFileSync(jsonPath, `${JSON.stringify(novaMembers, undefined, 2)}\n`);
-  spawnSync('git', ['add', jsonPath]);
-  const diff = spawnSync('git', ['diff', 'master', jsonPath]).stdout.toString();
-  console.log(diff);
+  const content = `${JSON.stringify(novaMembers, undefined, 2)}\n`;
+
+  // Change detection
+  const existingContent = readFileSync(jsonPath).toString();
+  writeFileSync(jsonPath, content);
+  if (existingContent === content) {
+    console.log('No changes.');
+    return;
+  }
+
+  // Compute diff when not on CI
+  if (!process.env.CI) {
+    writeFileSync('existing.json', existingContent);
+    spawnSync('diff', ['--unified=0', 'existing.json', jsonPath], {
+      stdio: 'inherit'
+    });
+    unlinkSync('existing.json');
+    return;
+  }
+
+  // Create commit
+  runCommand('git', 'config', '--global', 'user.name', 'dti-github-bot');
+  runCommand('git', 'config', '--global', 'user.email', 'admin@cornelldti.org');
+  const gitBranch = 'dti-github-bot/pull-from-idol';
+  const commitMessage =
+    '[bot] Automatically pull latest data from IDOL backend';
+  runCommand('git', 'add', '.');
+  runCommand('git', 'fetch', '--all');
+  runCommand('git', 'checkout', 'master');
+  runCommand('git', 'checkout', '-b', gitBranch);
+  if (runCommand('git', 'commit', '-m', commitMessage).status === 0) {
+    if (runCommand('git', 'push', '-f', 'origin', gitBranch).status !== 0) {
+      runCommand('git', 'push', '-f', '--set-upstream', 'origin', gitBranch);
+    }
+  }
+
+  // Create PR
+  const octokit = new Octokit({
+    auth: `token ${process.env.BOT_TOKEN}`,
+    userAgent: 'cornell-dti/big-diff-warning'
+  });
+  const prBody = `## Summary
+
+This is a PR auto-generated from \`yarn workspace dti-website pull-from-idol\`.
+It updates the members JSON with latest data from IDOL backend.
+Please review it carefully to ensure nothing bad is here.
+
+## Test Plan
+
+:eyes:`;
+  const existingPR = (
+    await octokit.pulls.list({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      state: 'open'
+    })
+  ).data.find((pr) => pr.title === commitMessage);
+  if (existingPR == null) {
+    await octokit.pulls.create({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      title: commitMessage,
+      body: prBody,
+      base: 'master',
+      head: gitBranch
+    });
+  } else {
+    await octokit.pulls.update({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      pull_number: existingPR.number,
+      body: prBody
+    });
+  }
 }
 
 main();
