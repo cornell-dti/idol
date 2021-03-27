8e4a9b61c1f8b4c967cac1b6f3fca89898af0d4d
JBoss925 <jaggerbrulato@gmail.com>
2021-03-26 22:30:19 -0400

    removed unused line, trigger rebuild

--- dti-website-server/src/server.ts
+++ dti-website-server/src/server.ts
@@ -4,7 +4,6 @@ import serverless from 'serverless-http';
 // Constants and configurations
 const app = express();
 const router = express.Router();
-const PORT = process.env.PORT || 8000;
 
 router.get('/', (req: Request, res: Response) => {
   console.log('Ping!');
