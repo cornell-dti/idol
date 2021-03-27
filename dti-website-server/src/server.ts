118a187d4cfbeb9c9978235e649df1d97f6401d4
JBoss925 <jaggerbrulato@gmail.com>
2021-03-26 21:34:42 -0400

    merged in master, and added functions for dti-website as well (in case we want to add interactivity ever) as well as scheduled the new DTI netlify site to redeploy every 2 hours and in so doing, it will pull-from-idol during the build updating the data if the PR isn't merged yet

--- dti-website-server/src/server.ts
+++ dti-website-server/src/server.ts
@@ -1,7 +1,5 @@
 import express, { RequestHandler, Request, Response } from 'express';
 import serverless from 'serverless-http';
-import path from 'path';
-import { exec } from 'child_process';
 
 // Constants and configurations
 const app = express();
@@ -9,12 +7,8 @@ const router = express.Router();
 const PORT = process.env.PORT || 8000;
 
 router.get('/', (req: Request, res: Response) => {
-  console.log("Connection made!");
-  // Check whether to update?
-  exec('ls -l', (err, stdout, stderr) => {
-    console.log(err, stdout, stderr);
-  });
-  res.send('');
+  console.log("Ping!");
+  res.send('Pong!');
 });
 
 app.use('/.netlify/functions/server', router);
