eb218abea7999c1b13a19ea2200c5fc80b866ff5
Henry Li <hl738@cornell.edu>
2021-03-30 20:25:00 -0400

    setup dev firebase instance

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -2,26 +2,37 @@ import admin from 'firebase-admin';
 import { DBTeam } from './DataTypes';
 
 require('dotenv').config();
+const isProdDb: boolean = JSON.parse(process.env.IS_PROD_DB as string);
 
-const serviceAccount = require('../resources/idol-b6c68-firebase-adminsdk-h4e6t-40e4bd5536.json');
+const serviceAccount = require(isProdDb
+  ? '../resources/idol-b6c68-firebase-adminsdk-h4e6t-40e4bd5536.json'
+  : '../resources/cornelldti-idol-firebase-adminsdk-ifi28-9aaca97159.json');
 
 const configureAccount = (sa) => {
   const configAcc = sa;
   let parsedPK;
   try {
-    parsedPK = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string);
+    parsedPK = isProdDb
+      ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string)
+      : JSON.parse(process.env.FIREBASE_DEV_PRIVATE_KEY as string);
   } catch (err) {
-    parsedPK = process.env.FIREBASE_PRIVATE_KEY;
+    parsedPK = isProdDb
+      ? process.env.FIREBASE_PRIVATE_KEY
+      : process.env.FIREBASE_DEV_PRIVATE_KEY;
   }
   configAcc.private_key = parsedPK;
-  configAcc.private_key_id = process.env.FIREBASE_PRIVATE_KEY_ID;
+  configAcc.private_key_id = isProdDb
+    ? process.env.FIREBASE_PRIVATE_KEY_ID
+    : process.env.FIREBASE_DEV_PRIVATE_KEY_ID;
   return configAcc;
 };
 
 export const app = admin.initializeApp({
   credential: admin.credential.cert(configureAccount(serviceAccount)),
   databaseURL: 'https://idol-b6c68.firebaseio.com',
-  storageBucket: 'gs://idol-b6c68.appspot.com'
+  storageBucket: isProdDb
+    ? 'gs://idol-b6c68.appspot.com'
+    : 'gs://cornelldti-idol.appspot.com'
 });
 
 export const bucket = admin.storage().bucket();
