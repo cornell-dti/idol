ac522100f85c6989ae06a394a944e5b751aadff3
riyajaggi <jaggiriya20@gmail.com>
2021-04-05 10:46:33 -0400

    Fix code style

--- backend/data/download-images.ts
+++ backend/data/download-images.ts
@@ -5,17 +5,17 @@ import { ProfileImage } from '../src/DataTypes';
 
 const fs = require('fs-extra');
 
-const dirPath = './data/members/images/'; 
+const dirPath = './data/members/images/';
 
 async function getMemberImages(): Promise<readonly ProfileImage[]> {
-  const { images } : { images: readonly ProfileImage[] } = await fetch(
+  const { images }: { images: readonly ProfileImage[] } = await fetch(
     'https://idol.cornelldti.org/.netlify/functions/api/allMemberImages'
   ).then((response) => response.json());
   return images;
 }
 
 async function downloadFile(image: ProfileImage) {
-  const {fileName} = image;
+  const { fileName } = image;
   const filePath = dirPath + fileName;
   const response = await fetch(image.url);
   const buffer = await response.buffer();
@@ -25,7 +25,9 @@ async function downloadFile(image: ProfileImage) {
 async function main(): Promise<void> {
   fs.emptyDirSync(dirPath);
   const memberImages = await getMemberImages();
-  memberImages.forEach((image) => {downloadFile(image)});
+  memberImages.forEach((image) => {
+    downloadFile(image);
+  });
 }
 
 main();
