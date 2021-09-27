e57c536d1b9b566a83f40d2db9832be0100c9110
Sam Zhou <sam@developersam.com>
2021-09-27 09:38:08 -0400

    Bump dependencies (#158)

--- dti-website/next.config.js
+++ dti-website/next.config.js
@@ -1,20 +1,10 @@
-const withEsbuildLoader = (config, options) => {
-  const jsLoaders = config.module.rules.filter((rule) => rule.test && rule.test.test('.tsx'));
-  jsLoaders.forEach((jsLoader) => {
-    jsLoader.use.loader = require.resolve('esbuild-loader');
-    jsLoader.use.options = options;
-  });
-};
-
 module.exports = {
   webpack5: true,
-  webpack(config, { webpack }) {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack']
     });
-    config.plugins.push(new webpack.ProvidePlugin({ React: 'react' }));
-    withEsbuildLoader(config, { loader: 'tsx', target: 'es2017' });
     return config;
   }
 };
