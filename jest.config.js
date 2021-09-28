97535b1baaa9939862d986fdf1c8a843b09a58d4
Sam Zhou <sam@developersam.com>
2021-09-28 17:00:29 -0400

    Use jest directly for testing (#162)

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const babelJestPath = require.resolve('babel-jest');
+const identityObjectProxyPath = require.resolve('identity-obj-proxy');
+
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': babelJestPath
+  },
+  transformIgnorePatterns: [
+    `/node_modules/(?!(lib-react)).+\\.js$`,
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    '^.+\\.module\\.(css|sass|scss)$': identityObjectProxyPath,
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    // Handle image imports
+    '^.+\\.(jpg|jpeg|png|gif|webp)$': `<rootDir>/__mocks__/fileMock.js`
+  }
+};
