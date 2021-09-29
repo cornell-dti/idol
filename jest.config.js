426994ef7e77012d128bf107d547a6e2bd386f14
Henry Li <hl738@cornell.edu>
2021-09-29 16:12:11 -0400

    change root directory for backend tests

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,33 @@
-const babelJestPath = require.resolve('babel-jest');
-const identityObjectProxyPath = require.resolve('identity-obj-proxy');
-
 module.exports = {
-  projects: ['<rootDir>/frontend/*', '<rootDir>/backend/*'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': babelJestPath
-  },
-  transformIgnorePatterns: [
-    `/node_modules/(?!(lib-react)).+\\.js$`,
-    '^.+\\.module\\.(css|sass|scss)$'
-  ],
-  moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
-    '^.+\\.module\\.(css|sass|scss)$': identityObjectProxyPath,
-    // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    // Handle image imports
-    '^.+\\.(jpg|jpeg|png|gif|webp)$': `<rootDir>/__mocks__/fileMock.js`
-  }
+  projects: [
+    {
+      displayName: 'IDOL Frontend Tests',
+      testMatch: ['<rootDir>/frontend/**/*.test.(ts|tsx)'],
+      testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+      testEnvironment: 'jsdom',
+      transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+      },
+      transformIgnorePatterns: [
+        `/node_modules/(?!(lib-react)).+\\.js$`,
+        '^.+\\.module\\.(css|sass|scss)$'
+      ],
+      moduleNameMapper: {
+        // Handle CSS imports (with CSS modules)
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+        // Handle CSS imports (without CSS modules)
+        '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        // Handle image imports
+        '^.+\\.(jpg|jpeg|png|gif|webp)$': `<rootDir>/__mocks__/fileMock.js`
+      }
+    },
+    {
+      displayName: 'IDOL Backend Tests',
+      testMatch: ['<rootDir>/backend/**/*.test.ts'],
+      transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+      },
+      rootDir: '<rootDir>/backend/'
+    }
+  ]
 };
