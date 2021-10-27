c19f37667f50eab9fe99442366fb543c562b4e4a
jks273 <jks273@cornell.edu>
2021-10-26 21:15:38 -0400

    Update jest unit tests.

--- backend/tests/TeamEventsDao.test.ts
+++ backend/tests/TeamEventsDao.test.ts
@@ -1,35 +1,34 @@
-import TeamEventsDao from "./dao/TeamEventsDao"; 
+import TeamEventsDao from "../src/dao/TeamEventsDao"; 
+import { TeamEvent } from "../src/DataTypes";
 
-const jaggerData : IdolMember = {
-    graduation: "May 2021",
-    website: "https://jagger.netlify.app/",
-    subteams: ["leads", "dev-leads"],
-    formerSubteams: ["idol"],
-    github: "https://github.com/JBoss925/",
-    linkedin: "https://www.linkedin.com/in/jagger-brulato-896968149/",
-    hometown: "Mooresville, NC",
-    minor: null,
-    roleDescription: "Lead",
-    firstName: "Jagger",
-    email: "jb2375@cornell.edu",
-    major: "Computer Science",
-    netid: "jb2375",
-    doubleMajor: "Economics",
-    lastName: "Brulato",
-    about: "Hey! I'm Jagger. I'm a senior majoring in CS and Economics. I like to make funk-fusion music, and I do a lot of board sports. I also play a lot of chess (d4 > e4, @Fischer) and Tetris.",
-    role: "lead"
-}
+const test_user: IdolMember = {
+  "email": "aa753@cornell.edu",
+  "role": "lead"
+} as IdolMember;
 
-const mockTeamEvent : TeamEvent = {
-  name: "te1",
-  attendees: [jaggerData],
-  uuid: ""
-}
+const test_event_1: TeamEvent = {
+  uuid: "788d2998-7c37-4795-8ab7-9084765afeee",
+  name: "test1",
+  attendees: []
+};
+
+const test_event_2: TeamEvent = {...test_event_1, attendees: [test_user]};
+
+/* Cleanup database after running tests */
+afterAll(async () => TeamEventsDao.deleteTeamEvent(test_event_1))
 
 test('Add new event', () => {
-  return TeamEventsDao.createTeamEvent(mockTeamEvent).then(() => {
+  TeamEventsDao.createTeamEvent(test_event_1).then(() => {
     TeamEventsDao.getAllTeamEvents().then((event) => {
-      expect(event).toEqual([mockTeamEvent]);
+      expect(event).toEqual(test_event_1);
     });
   });
+});
+
+test('Update event', () => {
+  TeamEventsDao.updateTeamEvent(test_event_2).then(() => {
+    TeamEventsDao.getAllTeamEvents().then((event) => {
+      expect(event).toEqual(test_event_2);
+    });
+  })
 });
