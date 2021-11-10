cbaccd6c5ea2214d406dd680448ab71c17dd3eb2
riyajaggi <jaggiriya20@gmail.com>
2021-11-10 12:24:10 -0500

    Refactor permissions

--- backend/src/permissions.ts
+++ backend/src/permissions.ts
@@ -4,35 +4,39 @@ export const allRoles: readonly Role[] = ['lead', 'tpm', 'pm', 'developer', 'des
 
 export class PermissionsManager {
   static async canEditMembers(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canDeploySite(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canEditSignIn(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canEditTeams(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canReviewChanges(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canGetShoutouts(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   static async canEditTeamEvent(mem: IdolMember): Promise<boolean> {
-    return mem.role === 'lead' || this.isAdmin(mem);
+    return this.isLeadOrAdmin(mem);
   }
 
   public static async isAdmin(mem: IdolMember): Promise<boolean> {
     const member = (await adminCollection.doc(mem.email).get()).data();
     return mem.role === 'lead' || member !== undefined;
+  }
+
+  public static async isLeadOrAdmin(mem: IdolMember): Promise<boolean> { 
+    return mem.role === 'lead' || this.isAdmin(mem) 
   }
 }
