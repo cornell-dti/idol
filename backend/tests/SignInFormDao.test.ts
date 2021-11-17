323a4cd71899a7562fb5a603d92c6a595f49e43b
riyajaggi <jaggiriya20@gmail.com>
2021-11-17 00:58:19 -0500

    Fix mock users

--- backend/tests/SignInFormDao.test.ts
+++ backend/tests/SignInFormDao.test.ts
@@ -3,11 +3,10 @@ import SignInFormDao from '../src/dao/SignInFormDao';
 import { SignInForm } from '../src/DataTypes';
 import { approvedMemberCollection } from '../src/firebase';
 import mockForms from './data/mock-signin.json';
-import mockUsers from './data/mock-users.json';
 
 /* Adding mock user for testing sign-ins */
 beforeAll(async () => {
-  const mockUser = mockUsers.mu1 as IdolMember;
+  const mockUser = mockForms.users.mus1 as IdolMember;
   await MembersDao.setMember(mockUser.email, mockUser);
 });
 
@@ -18,8 +17,8 @@ afterAll(async () => {
       await SignInFormDao.deleteSignIn(id);
     })
   );
-  await MembersDao.deleteMember(mockUsers.mu1.email);
-  await approvedMemberCollection.doc(mockUsers.mu1.email).delete();
+  await MembersDao.deleteMember(mockForms.users.mus1.email);
+  await approvedMemberCollection.doc(mockForms.users.mus1.email).delete();
 });
 
 test('Create new sign-in form', async () => {
@@ -32,20 +31,22 @@ test('Create new sign-in form', async () => {
 
 test('Sign in into an open form', async () => {
   const openForm = mockForms.openSignin as SignInForm;
-  await SignInFormDao.signIn(openForm.id, mockUsers.mu1.email);
+  await SignInFormDao.signIn(openForm.id, mockForms.users.mus1.email);
 
   const formData = await SignInFormDao.allSignInForms();
   const userData = formData.filter((forms) => forms.id === openForm.id)[0];
-  expect(userData.users).toContainEqual(expect.objectContaining({ user: mockUsers.mu1 }));
+  expect(userData.users).toContainEqual(expect.objectContaining({ user: mockForms.users.mus1 }));
 });
 
 test('Sign in attempted for an expired form', async () => {
-    const expiredForm = mockForms.expiredSignin as SignInForm;
-    await SignInFormDao.createSignIn(expiredForm.id, expiredForm.expireAt);
-    await expect(() => SignInFormDao.signIn(expiredForm.id, mockUsers.mu1.email)).rejects.toThrow();
-})
+  const expiredForm = mockForms.expiredSignin as SignInForm;
+  await SignInFormDao.createSignIn(expiredForm.id, expiredForm.expireAt);
+  await expect(() =>
+    SignInFormDao.signIn(expiredForm.id, mockForms.users.mus1.email)
+  ).rejects.toThrow();
+});
 
 test('Sign in attempted for an invalid id', async () => {
-    const invalidId = 'invalid-id'
-    await expect(() => SignInFormDao.signIn(invalidId, mockUsers.mu1.email)).rejects.toThrow();
-})
+  const invalidId = 'invalid-id';
+  await expect(() => SignInFormDao.signIn(invalidId, mockForms.users.mus1.email)).rejects.toThrow();
+});
