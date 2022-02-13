80384d06767c31358897c552aeeff6975392bc17
Riya Jaggi <jaggiriya20@gmail.com>
2022-02-13 18:13:38 -0500

    Add getAllTeamEvents to team events api

--- frontend/src/API/TeamEventsAPI.ts
+++ frontend/src/API/TeamEventsAPI.ts
@@ -1,4 +1,5 @@
 import { backendURL } from '../environment';
+import { Emitters } from '../utils';
 import APIWrapper from './APIWrapper';
 
 type TeamEventResponseObj = {
@@ -23,6 +24,27 @@ export type TeamEvent = {
 };
 
 export class TeamEventsAPI {
+    public static getAllTeamEvents(): Promise<TeamEvent[]> {
+        console.log("get all events")
+        const eventsProm = APIWrapper.get(`${backendURL}/getAllTeamEvents`).then((res) => res.data);
+        console.log(eventsProm);
+        return eventsProm.then((val) => {
+            if (val.error) {
+                Emitters.generalError.emit({
+                    headerMsg: "Couldn't get all events",
+                    contentMsg: `Error was: ${val.err}`
+                });
+                return [];
+            }
+            const events = val.events as TeamEvent[];
+            return events;
+        });
+    }
+
+    public static createTeamEventForm(teamEvent: TeamEvent): Promise<TeamEventResponseObj> {
+        return APIWrapper.post(`${backendURL}/createTeamEvent`, teamEvent).then((res) => res.data);
+    }
+
     public static requestTeamEventCredit(teamEvent: TeamEvent): Promise<TeamEventResponseObj> {
         // need to add image processing
         console.log("here")
