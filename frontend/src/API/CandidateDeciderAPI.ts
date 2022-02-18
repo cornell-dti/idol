250643ff5e83a71ecc51a06c976f620baa8b953b
Henry Li <hl738@cornell.edu>
2022-02-18 13:19:46 -0500

    add return type

--- frontend/src/API/CandidateDeciderAPI.ts
+++ frontend/src/API/CandidateDeciderAPI.ts
@@ -7,10 +7,8 @@ export default class CandidateDeciderAPI {
     return response.then((val) => val.data.instances);
   }
 
-  static async createNewInstance(instance: CandidateDeciderInstance) {
-    return APIWrapper.post(`${backendURL}/createNewCandidateDeciderInstance`, instance).then(
-      (res) => res.data.instances
-    );
+  static async createNewInstance(instance: CandidateDeciderInstance): Promise<void> {
+    APIWrapper.post(`${backendURL}/createNewCandidateDeciderInstance`, instance);
   }
 
   static async toggleInstance(uuid: string): Promise<void> {
