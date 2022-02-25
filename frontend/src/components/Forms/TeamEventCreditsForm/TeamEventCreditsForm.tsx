ad2aa5fdfc09e0eb1eace42f17cd0c85a6d70f67
Aira <aa2235@cornell.edu>
2022-02-25 17:29:15 -0500

    Add member side TEC dashboard

--- frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
+++ frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
@@ -63,6 +63,47 @@ const TeamEventCreditForm: React.FC = () => {
     }
   };
 
+  type MemberTeamEventCreditStatus = {
+    countApprovedCredits: number;
+    countRemainingCredits: number;
+    approvedTEC: string;
+    pendingTEC: string;
+  };
+
+  const currMemberTECStatus: MemberTeamEventCreditStatus = {
+    countApprovedCredits: 0,
+    countRemainingCredits: 3,
+    approvedTEC: '',
+    pendingTEC: ''
+  };
+
+  const teamEventCreditStatusLogic = () => {
+    for (const currTeamEvent of teamEvents) {
+      for (const currApprovedMember of currTeamEvent.attendees) {
+        if (currApprovedMember.member.email === userInfo.email) {
+          // add Hours logic
+          currMemberTECStatus.countApprovedCredits += 1;
+          if (currMemberTECStatus.countRemainingCredits > 0)
+            currMemberTECStatus.countRemainingCredits -= 1;
+          if (currMemberTECStatus.approvedTEC.length === 0)
+            currMemberTECStatus.approvedTEC = currTeamEvent.name;
+          else
+            currMemberTECStatus.approvedTEC = `${currMemberTECStatus.approvedTEC}, ${currTeamEvent.name}`;
+        }
+      }
+      for (const currApprovedMember of currTeamEvent.requests) {
+        if (currApprovedMember.member.email === userInfo.email) {
+          if (currMemberTECStatus.pendingTEC.length === 0)
+            currMemberTECStatus.pendingTEC = currTeamEvent.name;
+          else
+            currMemberTECStatus.pendingTEC = `${currMemberTECStatus.pendingTEC}, ${currTeamEvent.name}`;
+        }
+      }
+    }
+  };
+
+  if (teamEvents != null) teamEventCreditStatusLogic();
+
   return (
     <div>
       <Form
@@ -73,22 +114,11 @@ const TeamEventCreditForm: React.FC = () => {
           padding: '3rem 0 5rem 0'
         }}
       >
-        <h1>DTI Team Event Credits</h1>
+        <h1>Submit Team Event Credits</h1>
         <p>
           Earn team event credits for participating in DTI events! Fill out this form every time and
           attach a picture of yourself at the event to receive credit.
         </p>
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Name:</label>
-          <p>
-            {userInfo.firstName} {userInfo.lastName}
-          </p>
-        </div>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Role:</label>
-          <p>{userInfo.roleDescription}</p>
-        </div>
 
         <div style={{ margin: '2rem 0' }}>
           <label style={{ fontWeight: 'bold' }}>
@@ -184,6 +214,33 @@ const TeamEventCreditForm: React.FC = () => {
         <Form.Button floated="right" onClick={submitTeamEventCredit}>
           Submit
         </Form.Button>
+
+        <div style={{ margin: '8rem 0' }}></div>
+        <h1>Check Team Event Credits</h1>
+        <p>
+          Check your team event credit status for this semester here! Every DTI member must complete
+          3 team event credits to fulfill this requirement.
+        </p>
+
+        <div style={{ margin: '2rem 0' }}>
+          <label style={{ fontWeight: 'bold' }}>Approved Credits:</label>
+          <p>{currMemberTECStatus.countApprovedCredits}</p>
+        </div>
+
+        <div style={{ margin: '2rem 0' }}>
+          <label style={{ fontWeight: 'bold' }}>Approved Events:</label>
+          <p>{`[${currMemberTECStatus.approvedTEC}]`}</p>
+        </div>
+
+        <div style={{ margin: '2rem 0' }}>
+          <label style={{ fontWeight: 'bold' }}>Remaining Credits Needed:</label>
+          <p>{currMemberTECStatus.countRemainingCredits}</p>
+        </div>
+
+        <div style={{ margin: '2rem 0' }}>
+          <label style={{ fontWeight: 'bold' }}>Pending Approval For:</label>
+          <p>{`[${currMemberTECStatus.pendingTEC}]`}</p>
+        </div>
       </Form>
     </div>
   );
