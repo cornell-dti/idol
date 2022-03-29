ec8d94443972403bf9f008398abf0fc088662e05
Aira Agrawal <70241445+airaagrawal1@users.noreply.github.com>
2022-03-28 20:46:58 -0400

    Change styling of TEC member-side dashboard  (#264)
    * change format of TEC display to cards
    
    * Refactoring Team Event Credit Dashboard into seperate component
    
    * add .module.css file
    
    * more inline styling fixes
    
    * design changes for number of credits approved/remaining

--- frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
+++ frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
@@ -3,6 +3,8 @@ import { Form, Segment, Label, Button, Dropdown } from 'semantic-ui-react';
 import { Emitters, getNetIDFromEmail } from '../../../utils';
 import { useSelf } from '../../Common/FirestoreDataProvider';
 import { TeamEventsAPI } from '../../../API/TeamEventsAPI';
+import TeamEventCreditDashboard from './TeamEventsCreditDasboard';
+import styles from './TeamEventCreditsForm.module.css';
 import ImagesAPI from '../../../API/ImagesAPI';
 
 const TeamEventCreditForm: React.FC = () => {
@@ -71,55 +73,19 @@ const TeamEventCreditForm: React.FC = () => {
     }
   };
 
-  const [countApprovedCredits, setCountApprovedCredits] = useState(0);
-  const [countRemainingCredits, setCountRemainingCredits] = useState(3);
-  const [approvedTEC, setApprovedTEC] = useState('No Events Yet');
-  const [pendingTEC, setPendingTEC] = useState('No Events Yet');
-
-  useEffect(() => {
-    if (teamEvents != null) {
-      teamEvents.forEach((currTeamEvent) => {
-        currTeamEvent.attendees.forEach((currApprovedMember) => {
-          if (currApprovedMember.member.email === userInfo.email) {
-            const currCredits = Number(currTeamEvent.numCredits);
-            setCountApprovedCredits(countApprovedCredits + currCredits);
-            if (countRemainingCredits - currCredits < 0) setCountRemainingCredits(0);
-            else setCountRemainingCredits(countRemainingCredits - currCredits);
-            if (approvedTEC === 'No Events Yet') setApprovedTEC(currTeamEvent.name);
-            else setApprovedTEC(`${approvedTEC}, ${currTeamEvent.name}`);
-          }
-        });
-        currTeamEvent.requests.forEach((currApprovedMember) => {
-          if (currApprovedMember.member.email === userInfo.email) {
-            if (pendingTEC === 'No Events Yet') setPendingTEC(currTeamEvent.name);
-            else setPendingTEC(`${pendingTEC}, ${currTeamEvent.name}`);
-          }
-        });
-      });
-    }
-  }, [teamEvents]);
-
   return (
     <div>
-      <Form
-        style={{
-          width: '60%',
-          alignSelf: 'center',
-          margin: 'auto',
-          padding: '3rem 0 5rem 0'
-        }}
-      >
+      <Form className={styles.form_style}>
         <h1>Submit Team Event Credits</h1>
         <p>
           Earn team event credits for participating in DTI events! Fill out this form every time and
           attach a picture of yourself at the event to receive credit.
         </p>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>
-            Select a Team Event: <span style={{ color: '#db2828' }}>*</span>
+        <div className={styles.inline}>
+          <label className={styles.bold}>
+            Select a Team Event: <span className={styles.red_color}>*</span>
           </label>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div className={styles.center_and_flex}>
             {teamEvents && !teamEvent ? (
               <Dropdown
                 placeholder="Select a Team Event"
@@ -138,13 +104,7 @@ const TeamEventCreditForm: React.FC = () => {
             ) : undefined}
 
             {teamEvent ? (
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center'
-                }}
-              >
+              <div className={styles.row_direction}>
                 <div>
                   <Segment>
                     <h4>{teamEvent.name}</h4>
@@ -160,7 +120,7 @@ const TeamEventCreditForm: React.FC = () => {
                     setTeamEvent(undefined);
                     setHours('0');
                   }}
-                  style={{ marginLeft: '1.5em' }}
+                  className={styles.inline}
                 >
                   Clear
                 </Button>
@@ -168,11 +128,10 @@ const TeamEventCreditForm: React.FC = () => {
             ) : undefined}
           </div>
         </div>
-
         {teamEvent?.hasHours ? (
           <div>
-            <label style={{ fontWeight: 'bold' }}>
-              How many hours did you attend the event? <span style={{ color: '#db2828' }}>*</span>
+            <label className={styles.bold}>
+              How many hours did you attend the event? <span className={styles.red_color}>*</span>
             </label>
             <Form.Input
               fluid
@@ -181,17 +140,16 @@ const TeamEventCreditForm: React.FC = () => {
               size="large"
               value={hours}
               onChange={(event) => setHours(event.target.value)}
-              style={{ width: '20%' }}
+              className={styles.width_20}
               required
             />
           </div>
         ) : undefined}
-
-        <div style={{ margin: '2rem 0' }}>
-          <label htmlFor="newImage" style={{ fontWeight: 'bold' }}>
-            Upload your event picture here! <span style={{ color: '#db2828' }}>*</span>
+        <div className={styles.inline}>
+          <label htmlFor="newImage" className={styles.bold}>
+            Upload your event picture here! <span className={styles.red_color}>*</span>
           </label>
-          <p style={{ marginBottom: '0' }}>
+          <p className={styles.margin_bottom_zero}>
             Please include a picture of yourself (and others) and/or an email chain only if the
             former is not possible.
           </p>
@@ -205,33 +163,7 @@ const TeamEventCreditForm: React.FC = () => {
         <Form.Button floated="right" onClick={submitTeamEventCredit}>
           Submit
         </Form.Button>
-
-        <div style={{ margin: '8rem 0' }}></div>
-        <h1>Check Team Event Credits</h1>
-        <p>
-          Check your team event credit status for this semester here! Every DTI member must complete
-          3 team event credits to fulfill this requirement.
-        </p>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Approved Credits:</label>
-          <p>{countApprovedCredits}</p>
-        </div>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Approved Events:</label>
-          <p>{`[${approvedTEC}]`}</p>
-        </div>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Remaining Credits Needed:</label>
-          <p>{countRemainingCredits}</p>
-        </div>
-
-        <div style={{ margin: '2rem 0' }}>
-          <label style={{ fontWeight: 'bold' }}>Pending Approval For:</label>
-          <p>{`[${pendingTEC}]`}</p>
-        </div>
+        <TeamEventCreditDashboard teamEvents={teamEvents} />
       </Form>
     </div>
   );
---
+++ frontend/src/components/Forms/TeamEventCreditsForm/TeamEventsCreditDasboard.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Card, Message } from 'semantic-ui-react';
+import { useSelf } from '../../Common/FirestoreDataProvider';
+import styles from './TeamEventCreditsForm.module.css';
+
+const TeamEventCreditDashboard = (props: { teamEvents: TeamEvent[] }): JSX.Element => {
+  // When the user is logged in, `useSelf` always return non-null data.
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const userInfo = useSelf()!;
+  const { teamEvents } = props;
+
+  const [countApprovedCredits, setCountApprovedCredits] = useState(0);
+  const [countRemainingCredits, setCountRemainingCredits] = useState(3);
+  const [approvedTEC, setApprovedTEC] = useState<TeamEvent[]>([]);
+  const [pendingTEC, setPendingTEC] = useState<TeamEvent[]>([]);
+
+  useEffect(() => {
+    teamEvents.forEach((currTeamEvent) => {
+      currTeamEvent.attendees.forEach((currApprovedMember) => {
+        if (currApprovedMember.member.email === userInfo.email) {
+          const currCredits = Number(currTeamEvent.numCredits);
+          setCountApprovedCredits((countApprovedCredits) => countApprovedCredits + currCredits);
+          setCountRemainingCredits((countRemainingCredits) => {
+            if (countRemainingCredits - currCredits <= 0) return 0;
+            return countRemainingCredits - currCredits;
+          });
+          setApprovedTEC((approvedTEC) => [...approvedTEC, currTeamEvent]);
+        }
+      });
+      currTeamEvent.requests.forEach((currApprovedMember) => {
+        if (currApprovedMember.member.email === userInfo.email) {
+          setPendingTEC((pendingTEC) => [...pendingTEC, currTeamEvent]);
+        }
+      });
+    });
+  }, [teamEvents]);
+
+  return (
+    <div>
+      <Form>
+        <div className={styles.header}></div>
+        <h1>Check Team Event Credits</h1>
+        <p>
+          Check your team event credit status for this semester here! Every DTI member must complete
+          3 team event credits to fulfill this requirement.
+        </p>
+
+        <div className={styles.inline}>
+          <label className={styles.bold}>
+            Your Approved Credits:{' '}
+            <span className={styles.dark_grey_color}>{countApprovedCredits}</span>
+          </label>
+        </div>
+
+        <div className={styles.inline}>
+          <label className={styles.bold}>
+            Remaining Credits Needed:{' '}
+            <span className={styles.dark_grey_color}>{countRemainingCredits}</span>
+          </label>
+        </div>
+
+        <div className={styles.inline}>
+          <label className={styles.bold}>Approved Events:</label>
+          {approvedTEC.length !== 0 ? (
+            <Card.Group>
+              {approvedTEC.map((teamEvent) => (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>{teamEvent.name} </Card.Header>
+                    <Card.Meta>{teamEvent.date}</Card.Meta>
+                    <Card.Meta>{`Number of Credits: ${teamEvent.numCredits}`}</Card.Meta>
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          ) : (
+            <Message>You have not been approved for any team events yet.</Message>
+          )}
+        </div>
+
+        <div className={styles.inline}>
+          <label className={styles.bold}>Pending Approval For:</label>
+          {pendingTEC.length !== 0 ? (
+            <Card.Group>
+              {pendingTEC.map((teamEvent) => (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>{teamEvent.name} </Card.Header>
+                    <Card.Meta>{teamEvent.date}</Card.Meta>
+                    <Card.Meta>{`Number of Credits: ${teamEvent.numCredits}`}</Card.Meta>
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          ) : (
+            <Message>You are not currently pending approval for any team events.</Message>
+          )}
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default TeamEventCreditDashboard;
