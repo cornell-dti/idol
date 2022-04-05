51c36581c6864729ba2f91f69bd86da6ea154048
Jackson Staniec <81320443+JacksonStaniec@users.noreply.github.com>
2022-04-04 17:38:16 -0700

    Refactor `SignInFormDAO` and `signinformAPI` (#268)
    * Refactor signinformDAO.
    
    * Revise test suite to account for refactor.

--- backend/src/signinformAPI.ts
+++ backend/src/signinformAPI.ts
@@ -1,6 +1,6 @@
 import SignInFormDao from './dao/SignInFormDao';
-import { PermissionError, BadRequestError } from './errors';
-import { signInFormCollection } from './firebase';
+import { PermissionError, BadRequestError, NotFoundError } from './errors';
+import { signInFormCollection, memberCollection } from './firebase';
 import PermissionsManager from './permissions';
 
 const checkIfDocExists = async (id: string): Promise<boolean> =>
@@ -21,19 +21,29 @@ export const createSignInForm = async (
   expireAt: number,
   user: IdolMember
 ): Promise<{ id: string; createdAt: number; expireAt: number }> => {
-  if (!PermissionsManager.canEditSignIn(user)) {
+  const canEdit = await PermissionsManager.canEditSignIn(user);
+  if (!canEdit) {
     throw new PermissionError("You don't have permission to create a sign-in form!");
   }
   if (expireAt <= Date.now()) {
     throw new BadRequestError('Invalid Date: Expiry Date cannot be in the past!');
+  }
+  const formExists = await signInFormExists(id);
+  if (formExists) {
+    throw new NotFoundError(`A form with id '${id}' already exists!`);
   }
   await SignInFormDao.createSignIn(id, expireAt);
   return { id, createdAt: Date.now(), expireAt };
 };
 
 export const deleteSignInForm = async (id: string, user: IdolMember): Promise<void> => {
-  if (!PermissionsManager.canEditSignIn(user)) {
+  const canEdit = await PermissionsManager.canEditSignIn(user);
+  if (!canEdit) {
     throw new PermissionError("You don't have permission to delete a sign-in form!");
+  }
+  const formExists = await signInFormExists(id);
+  if (!formExists) {
+    throw new NotFoundError(`No form with id '${id}' found.`);
   }
   await SignInFormDao.deleteSignIn(id);
 };
@@ -41,7 +51,8 @@ export const deleteSignInForm = async (id: string, user: IdolMember): Promise<vo
 export const allSignInForms = async (
   user: IdolMember
 ): Promise<{ readonly forms: SignInForm[] }> => {
-  if (!PermissionsManager.canEditSignIn(user)) {
+  const canEdit = await PermissionsManager.canEditSignIn(user);
+  if (!canEdit) {
     return { forms: [] };
   }
   return { forms: await SignInFormDao.allSignInForms() };
@@ -51,6 +62,20 @@ export const signIn = async (
   id: string,
   user: IdolMember
 ): Promise<{ signedInAt: number; id: string }> => {
+  const formExists = await signInFormExists(id);
+  if (!formExists) {
+    throw new NotFoundError(`No form with id '${id}' found.`);
+  }
+  const formExpired = await signInFormExpired(id);
+  if (formExpired) {
+    throw new BadRequestError(`User is not allowed to sign into expired form with id '${id}.`);
+  }
+  const userDoc = memberCollection.doc(user.email);
+  const userRef = await userDoc.get();
+  if (!userRef.exists) {
+    throw new NotFoundError(`No user with email '${user.email}' found.`);
+  }
+
   const signedInAt = await SignInFormDao.signIn(id, user.email);
   return { id, signedInAt };
 };
