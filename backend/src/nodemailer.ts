1a70fc6c01456c453068a7581551da61a8bfab11
Riya Jaggi <jaggiriya20@gmail.com>
2022-04-27 18:20:26 -0400

    Add transporter object

---
+++ backend/src/nodemailer.ts
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer';
+
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+});
+
+export default transporter;
