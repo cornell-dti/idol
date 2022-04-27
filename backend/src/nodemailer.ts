66170802d8b78be98e9ce27bbb66d29dce21f8d6
Riya Jaggi <jaggiriya20@gmail.com>
2022-04-27 19:07:40 -0400

    Fix format

--- backend/src/nodemailer.ts
+++ backend/src/nodemailer.ts
@@ -3,15 +3,15 @@ import nodemailer from 'nodemailer';
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN
-    }
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+  }
 });
 
 export default transporter;
