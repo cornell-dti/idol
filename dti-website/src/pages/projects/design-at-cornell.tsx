525d3cce83a5e074f890013068aad4b792254315
Henry Li <hl738@cornell.edu>
2022-05-01 17:10:10 -0400

    Update product pages  (#285)
    * update /projects page and product pages
    
    * reduce card image sizes
    
    * fix qmi banner colors
    
    * add zing hero iamge
    
    * update hero images for reviews, cp, qmi
    
    * modify carriage hero image
    
    * change order of products by release date
    
    * update hero and feature padding
    
    * revise feature titles
    
    * add more padding between features
    
    * make feature titles consistent
    
    * update cureviews description
    
    * fix typo for apartments

---
+++ dti-website/src/pages/projects/design-at-cornell.tsx
@@ -0,0 +1,5 @@
+import DtiProject from '../../components/DtiProject';
+
+export default function DesignAtCornell(): JSX.Element {
+  return <DtiProject teamId="design-at-cornell" />;
+}
--- dti-website/src/pages/projects/samwise.tsx
+++
@@ -1,5 +0,0 @@
-import DtiProject from '../../components/DtiProject';
-
-export default function Samwise(): JSX.Element {
-  return <DtiProject teamId="samwise" />;
-}
