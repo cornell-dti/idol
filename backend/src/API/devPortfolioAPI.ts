a3f00ed64e3dbf3b4bdac2387b007292f9a0fb05
axc2@cornell.edu <andrew032012@gmail.com>
2022-05-07 21:59:52 -0400

    remove unused import

--- backend/src/API/devPortfolioAPI.ts
+++ backend/src/API/devPortfolioAPI.ts
@@ -1,6 +1,6 @@
 import DevPortfolioDao from '../dao/DevPortfolioDao';
 import PermissionsManager from '../utils/permissionsManager';
-import { PermissionError, BadRequestError, UnauthorizedError } from '../utils/errors';
+import { PermissionError, BadRequestError } from '../utils/errors';
 import validateSubmission from '../utils/githubUtil';
 
 export const getAllDevPortfolios = async (): Promise<DevPortfolio[]> =>
