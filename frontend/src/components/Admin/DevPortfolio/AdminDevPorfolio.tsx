dd6550d80542f0c7a81ebe1f931f3febf75ebfd4
Henry Li <hl738@cornell.edu>
2022-05-23 22:00:36 -0400

    temporary commit

--- frontend/src/components/Admin/DevPortfolio/AdminDevPorfolio.tsx
+++ frontend/src/components/Admin/DevPortfolio/AdminDevPorfolio.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Container, Loader, Form } from 'semantic-ui-react';
+import { Container, Loader, Form, Button, Header, Label, Divider } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import DevPortfolioAPI from '../../../API/DevPortfolioAPI';
+import styles from './AdminDevPortfolio.module.css';
 
 const AdminDevPortfolio: React.FC = () => {
   const [devPortfolios, setDevPortfolios] = useState<DevPortfolio[]>([]);
@@ -15,7 +17,7 @@ const AdminDevPortfolio: React.FC = () => {
   }, [setDevPortfolios]);
 
   return (
-    <Container>
+    <Container className={styles.container}>
       <Container>
         <AdminDevPortfolioForm />
       </Container>
@@ -28,22 +30,43 @@ const AdminDevPortfolio: React.FC = () => {
 
 const AdminDevPortfolioForm: React.FC = () => {
   const [name, setName] = useState<string>('');
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [dateError, setDateError] = useState<boolean>(false);
   const [deadline, setDeadline] = useState<Date>(new Date());
   const [earliestDate, setEarliestDate] = useState<Date>(new Date());
 
+  const handleSubmit = () => {
+    if (name === '') {
+      setNameError(true);
+    } else setNameError(false);
+    if (deadline < earliestDate) {
+      setDateError(true);
+    } else setNameError(false);
+  };
+
   return (
     <Form>
-      <Form.Input label="Name" value={name} onChange={(e) => setName(e.target.value)} />
-      <DatePicker
-        selected={deadline}
-        dateFormat="MMM d, yyyys"
-        onChange={(date: Date) => setDeadline(date)}
-      />
+      <Header as="h3">Name</Header>
+      <Form.Input error={nameError} value={name} onChange={(e) => setName(e.target.value)} />
+      <Header as="h3">Earliest Date</Header>
       <DatePicker
         selected={earliestDate}
-        dateFormat="MMM d, yyyy"
+        dateFormat="MMMM do yyyy"
         onChange={(date: Date) => setEarliestDate(date)}
       />
+      <Header as="h3">Deadline</Header>
+      <DatePicker
+        selected={deadline}
+        dateFormat="MMMM do yyyy"
+        onChange={(date: Date) => setDeadline(date)}
+      />
+      {dateError ? (
+        <Label pointing>The dates for the deadline and earliest date are invalid.</Label>
+      ) : undefined}
+      <Divider />
+      <Button id={styles.submitButton} onClick={() => handleSubmit()}>
+        Create Assignment
+      </Button>
     </Form>
   );
 };
