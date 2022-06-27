46e6149274ea293d4e0d8a276fe4a2447b471434
Henry Li <hl738@cornell.edu>
2022-06-26 22:21:37 -0400

    icons were being weird

--- dti-website-redesign/src/components/Footer.tsx
+++ dti-website-redesign/src/components/Footer.tsx
@@ -5,11 +5,58 @@ import facebookIcon from '../../public/static/icons/facebook.png';
 import githubIcon from '../../public/static/icons/github.png';
 import googleIcon from '../../public/static/icons/google.png';
 import instagramIcon from '../../public/static/icons/instagram.png';
+import { isContext } from 'vm';
+import { IncomingMessage } from 'http';
+
+type Icon = {
+  link: string;
+  src: string;
+  alt: string;
+};
+
+const icons: Icon[] = [
+  {
+    src: '/static/icons/app-store.png',
+    link: 'https://www.example.com',
+    alt: 'app store icon'
+  },
+  {
+    src: '/static/icons/email.png',
+    link: 'emailto: hello@cornelldti.org',
+    alt: 'email icon'
+  },
+  {
+    src: '/static/icons/facebook.png',
+    link: 'https://www.facebook.com/cornelldti',
+    alt: 'facebook icon'
+  },
+  {
+    src: '/static/icons/github.png',
+    link: 'https://www.github.com/cornell-dti',
+    alt: 'github icon'
+  },
+  {
+    src: '/static/icons/google.png',
+    link: 'https://www.example.com',
+    alt: 'play store icon'
+  },
+  {
+    src: '/static/icons/instagram.png',
+    link: 'https://www.instagram.com/cornelldti',
+    alt: 'instagram icon'
+  }
+];
 
 const Footer: React.FC = () => (
   <div className={styles.footer}>
     <div className={styles.iconContainer}>
-      <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
+      {icons.map((icon) => (
+        <a href={icon.link} target="_blank" rel="noreferrer noopener">
+          <img src={icon.src} alt={icon.alt} />
+        </a>
+      ))}
+
+      {/* <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
         <img src={appStoreIcon} alt="app store icon" />
       </a>
       <a href="mailto: hello@cornelldti.org">
@@ -26,7 +73,7 @@ const Footer: React.FC = () => (
       </a>
       <a href="http://www.instagram.com/cornelldti" target="_blank" rel="noreferrer noopener">
         <img src={instagramIcon} alt="instagram icon" />
-      </a>
+      </a> */}
     </div>
     <div className={styles.copyrightContainer}>Cornell Design & Tech Initiative &copy; 2022</div>
   </div>
