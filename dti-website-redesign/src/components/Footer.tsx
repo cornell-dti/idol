c7d41e1a61558ee852d153cc75f93259e324e6e1
Henry Li <hl738@cornell.edu>
2022-06-26 22:09:09 -0400

    repalce Image with img

--- dti-website-redesign/src/components/Footer.tsx
+++ dti-website-redesign/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './Footer.module.css';
 import appStoreIcon from '../../public/static/icons/app-store.png';
 import emailIcon from '../../public/static/icons/email.png';
@@ -11,22 +10,22 @@ const Footer: React.FC = () => (
   <div className={styles.footer}>
     <div className={styles.iconContainer}>
       <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
-        <Image src={appStoreIcon} />
+        <img src={appStoreIcon} alt="app store icon" />
       </a>
       <a href="mailto: hello@cornelldti.org">
-        <Image src={emailIcon} />
+        <img src={emailIcon} alt="email icon" />
       </a>
       <a href="https://facebook.com/cornelldti" target="_blank" rel="noreferrer noopener">
-        <Image src={facebookIcon} />
+        <img src={facebookIcon} alt="facebook icon" />
       </a>
       <a href="https://github.com/cornell-dti" target="_blank" rel="noreferrer noopener">
-        <Image src={githubIcon} />
+        <img src={githubIcon} alt="github icon" />
       </a>
       <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
-        <Image src={googleIcon} />
+        <img src={googleIcon} alt="google icon" />
       </a>
       <a href="http://www.instagram.com/cornelldti" target="_blank" rel="noreferrer noopener">
-        <Image src={instagramIcon} />
+        <img src={instagramIcon} alt="instagram icon" />
       </a>
     </div>
     <div className={styles.copyrightContainer}>Cornell Design & Tech Initiative &copy; 2022</div>
