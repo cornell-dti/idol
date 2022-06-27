7a91939a74d13aa26ec50433817d9dbed3338cc1
Henry Li <hl738@cornell.edu>
2022-06-26 22:25:56 -0400

    remove comments and unused imports

--- dti-website-redesign/src/components/Footer.tsx
+++ dti-website-redesign/src/components/Footer.tsx
@@ -1,12 +1,4 @@
 import styles from './Footer.module.css';
-import appStoreIcon from '../../public/static/icons/app-store.png';
-import emailIcon from '../../public/static/icons/email.png';
-import facebookIcon from '../../public/static/icons/facebook.png';
-import githubIcon from '../../public/static/icons/github.png';
-import googleIcon from '../../public/static/icons/google.png';
-import instagramIcon from '../../public/static/icons/instagram.png';
-import { isContext } from 'vm';
-import { IncomingMessage } from 'http';
 
 type Icon = {
   link: string;
@@ -55,25 +47,6 @@ const Footer: React.FC = () => (
           <img src={icon.src} alt={icon.alt} />
         </a>
       ))}
-
-      {/* <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
-        <img src={appStoreIcon} alt="app store icon" />
-      </a>
-      <a href="mailto: hello@cornelldti.org">
-        <img src={emailIcon} alt="email icon" />
-      </a>
-      <a href="https://facebook.com/cornelldti" target="_blank" rel="noreferrer noopener">
-        <img src={facebookIcon} alt="facebook icon" />
-      </a>
-      <a href="https://github.com/cornell-dti" target="_blank" rel="noreferrer noopener">
-        <img src={githubIcon} alt="github icon" />
-      </a>
-      <a href="https://www.example.com" target="_blank" rel="noreferrer noopener">
-        <img src={googleIcon} alt="google icon" />
-      </a>
-      <a href="http://www.instagram.com/cornelldti" target="_blank" rel="noreferrer noopener">
-        <img src={instagramIcon} alt="instagram icon" />
-      </a> */}
     </div>
     <div className={styles.copyrightContainer}>Cornell Design & Tech Initiative &copy; 2022</div>
   </div>
