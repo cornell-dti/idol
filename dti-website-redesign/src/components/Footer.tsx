c7e2a445b49342364ba0d835805db6ed5601d320
Henry Li <hl738@cornell.edu>
2022-06-28 23:34:21 -0400

    address pr comments

--- dti-website-redesign/src/components/Footer.tsx
+++ dti-website-redesign/src/components/Footer.tsx
@@ -8,47 +8,39 @@ type Icon = {
 
 const icons: Icon[] = [
   {
-    src: '/static/icons/app-store.png',
-    link: 'https://www.example.com',
-    alt: 'app store icon'
-  },
-  {
-    src: '/static/icons/email.png',
-    link: 'emailto: hello@cornelldti.org',
+    src: '/static/icons/email.svg',
+    link: 'mailto:hello@cornelldti.org',
     alt: 'email icon'
   },
   {
-    src: '/static/icons/facebook.png',
+    src: '/static/icons/instagram.svg',
+    link: 'https://www.instagram.com/cornelldti',
+    alt: 'instagram icon'
+  },
+  {
+    src: '/static/icons/facebook.svg',
     link: 'https://www.facebook.com/cornelldti',
     alt: 'facebook icon'
   },
   {
-    src: '/static/icons/github.png',
+    src: '/static/icons/github.svg',
     link: 'https://www.github.com/cornell-dti',
     alt: 'github icon'
-  },
-  {
-    src: '/static/icons/google.png',
-    link: 'https://www.example.com',
-    alt: 'play store icon'
-  },
-  {
-    src: '/static/icons/instagram.png',
-    link: 'https://www.instagram.com/cornelldti',
-    alt: 'instagram icon'
   }
 ];
 
 const Footer: React.FC = () => (
   <div className={styles.footer}>
-    <div className={styles.iconContainer}>
-      {icons.map((icon) => (
-        <a href={icon.link} target="_blank" rel="noreferrer noopener">
-          <img src={icon.src} alt={icon.alt} />
-        </a>
-      ))}
+    <div className={styles.innerFooter}>
+      <div className={styles.iconContainer}>
+        {icons.map((icon) => (
+          <a href={icon.link} target="_blank" rel="noreferrer noopener">
+            <img className={styles.socialIcon} src={icon.src} alt={icon.alt} />
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyrightContainer}>Cornell Design & Tech Initiative &copy; 2022</div>
     </div>
-    <div className={styles.copyrightContainer}>Cornell Design & Tech Initiative &copy; 2022</div>
   </div>
 );
 
