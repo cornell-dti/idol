056e684b1403b4196717aabd44e8266af26ae058
Daniel Jin <69334328+dan-ieljin@users.noreply.github.com>
2022-07-12 23:13:35 -0400

    Clear fields of admin forms after each submission (#301)
    * Clear fields of admin forms after each submission
    
    * Add missing code for clearing file input
    
    * reset fields after success
    
    Co-authored-by: Henry Li <hl738@cornell.edu>

--- frontend/src/components/Admin/AdminCandidateDecider/AdminCandidateDecider.tsx
+++ frontend/src/components/Admin/AdminCandidateDecider/AdminCandidateDecider.tsx
@@ -53,6 +53,7 @@ const CandidateDeciderInstanceCreator = ({
   const [responses, setResponses] = useState<string[][]>([[]]);
   const [authorizedMembers, setAuthorizedMembers] = useState<IdolMember[]>([]);
   const [authorizedRoles, setAuthorizedRoles] = useState<Role[]>([]);
+  const [fileInKey, setFileInKey] = useState('0');
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -86,7 +87,15 @@ const CandidateDeciderInstanceCreator = ({
     };
     CandidateDeciderAPI.createNewInstance(instance)
       .then((newInstance) => setInstances((instances) => [...instances, newInstance]))
-      .then(() => setSuccess(true));
+      .then(() => setSuccess(true))
+      .then(() => {
+        setName('');
+        setHeaders([]);
+        setResponses([[]]);
+        setAuthorizedMembers([]);
+        setAuthorizedRoles([]);
+        setFileInKey(Date.now().toString());
+      });
   };
 
   return (
@@ -94,7 +103,7 @@ const CandidateDeciderInstanceCreator = ({
       <Header as="h2">Create a new Candidate Decider instance</Header>
       <Form success={success}>
         <Form.Input label="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="file" accept=".csv" onChange={handleFileUpload} />
+        <input type="file" accept=".csv" onChange={handleFileUpload} key={fileInKey || ''} />
         <Message>
           All leads and IDOL admins have permission to all Candidate Decider instances
         </Message>
