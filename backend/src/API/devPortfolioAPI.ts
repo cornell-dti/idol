e81ddfd35e6ca2a9af7cecd3b654a179bae8eaad
Henry Li <hl738@cornell.edu>
2022-10-16 14:53:49 -0400

    add late deadline support

--- backend/src/API/devPortfolioAPI.ts
+++ backend/src/API/devPortfolioAPI.ts
@@ -48,11 +48,16 @@ export const createNewDevPortfolio = async (
 
   const updatedDeadline = new Date(instance.deadline);
   const updatedEarliestValidDate = new Date(instance.earliestValidDate);
+<<<<<<< HEAD
   const updatedLateDeadline = instance.lateDeadline ? new Date(instance.lateDeadline) : null;
+=======
+  const updatedLateDeadline = instance.lateDeadline ? new Date(instance.lateDeadline) : undefined;
+>>>>>>> 286cb9a (add late deadline support)
   const modifiedInstance = {
     ...instance,
     deadline: updatedDeadline.setHours(23, 59, 59),
-    earliestValidDate: updatedEarliestValidDate.setHours(0, 0, 0)
+    earliestValidDate: updatedEarliestValidDate.setHours(0, 0, 0),
+    lateDeadline: updatedLateDeadline ? updatedLateDeadline.setHours(23, 59, 59) : undefined
   };
   if (updatedLateDeadline) {
     modifiedInstance.lateDeadline = updatedLateDeadline.setHours(23, 59, 59);
