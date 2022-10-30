cad2e875ddc1f5c04630a8f59c582c2059663221
Andrew Chen <andrew032012@gmail.com>
2022-10-29 21:15:12 -0400

    create test templates

---
+++ backend/tests/githubUtil.test.ts
@@ -0,0 +1,26 @@
+import { validateSubmission } from '../src/utils/githubUtil';
+
+describe('Malformed URL', () => {
+
+});
+
+
+describe('No GitHub username', () => {
+
+});
+
+describe('Cannot use own PR for review requirement', () => {
+
+});
+
+describe('Trivial Review', () => {
+
+});
+
+describe('No review comments made between start and end dates', () => {
+
+});
+
+describe('Normal reviews', () => {
+
+});
