e65ef567bcd6dedd028b4b3f348d899b7eb2cf99
Aira <aa2235@cornell.edu>
2022-11-02 20:23:54 -0400

    adding date range for admins to display shoutouts

--- frontend/src/components/Admin/AdminShoutouts/AdminShoutouts.tsx
+++ frontend/src/components/Admin/AdminShoutouts/AdminShoutouts.tsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Item, Card } from 'semantic-ui-react';
+import { Form, Item, Card } from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Emitters } from '../../../utils';
 import { ShoutoutsAPI, Shoutout } from '../../../API/ShoutoutsAPI';
 import styles from './AdminShoutouts.module.css';
 
 const AdminShoutouts: React.FC = () => {
   const [shoutouts, setShoutouts] = useState<Shoutout[]>([]);
+  const [displayShoutouts, setDisplayShoutouts] = useState<Shoutout[]>([]);
   useEffect(() => {
-    ShoutoutsAPI.getAllShoutouts().then((shoutouts) => setShoutouts(shoutouts));
+    ShoutoutsAPI.getAllShoutouts().then((shoutouts) => {
+      setShoutouts(shoutouts);
+      setDisplayShoutouts(shoutouts);
+    });
   }, []);
+  const [earlyDate, setEarlyDate] = useState<Date>(new Date('2022/01/01'));
+  const [lastDate, setLastDate] = useState<Date>(new Date());
 
   const fromString = (shoutout: Shoutout): string => {
     const dateString = ` (Date: ${new Date(shoutout.timestamp).toDateString()})`;
@@ -18,16 +27,53 @@ const AdminShoutouts: React.FC = () => {
     return `From: Anonymous${dateString}`;
   };
 
+  const updateShoutouts = () => {
+    if (lastDate < earlyDate) {
+      Emitters.generalError.emit({
+        headerMsg: 'Invalid Date Range',
+        contentMsg: 'Please make sure the latest shoutout date is after the earliest shoutout date.'
+      });
+    } else {
+      setDisplayShoutouts(
+        shoutouts.filter(
+          (shoutout) =>
+            new Date(shoutout.timestamp) >= earlyDate && new Date(shoutout.timestamp) <= lastDate
+        )
+      );
+    }
+  };
+
   return (
     <div>
+      <Form className={styles.shoutoutForm}>
+        <h2 className={styles.formTitle}>Select date range to display shoutouts:</h2>
+        <Form.Group width="equals">
+          <DatePicker
+            selected={earlyDate}
+            dateFormat="MMMM do yyyy"
+            onChange={(date: Date) => setEarlyDate(date)}
+          />
+          <DatePicker
+            selected={lastDate}
+            dateFormat="MMMM do yyyy"
+            onChange={(date: Date) => setLastDate(date)}
+          />
+        </Form.Group>
+
+        <Form.Button onClick={updateShoutouts} floated="right" className={styles.updateButton}>
+          Update
+        </Form.Button>
+      </Form>
+
       <div className={styles.shoutoutsListContainer}>
-        {shoutouts.length === 0 ? (
+        <h2 className={styles.formTitle}>Shoutouts List! 📣</h2>
+        {displayShoutouts.length === 0 ? (
           <Card className={styles.noShoutoutsContainer}>
-            <Card.Content>No shoutouts.</Card.Content>
+            <Card.Content>No shoutouts in this date range.</Card.Content>
           </Card>
         ) : (
           <Item.Group divided>
-            {shoutouts
+            {displayShoutouts
               .sort((a, b) => a.timestamp - b.timestamp)
               .map((shoutout, i) => (
                 <Item key={i}>
