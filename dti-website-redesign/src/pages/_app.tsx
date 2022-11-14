09b3cd6fac46d63727e148602a980b8771f6a89b
Andrew Chen <59291082+andrew032011@users.noreply.github.com>
2022-11-14 18:18:57 -0500

    Product Page (#367)
    * initial implementation of card component
    
    * draft implementation
    
    * add images of products, create vertical bar, refactor all css into index.module.css
    
    * change footer flex to absolute
    
    * change absolute back to relative
    
    * add site url templates
    
    * run prettier
    
    * Move import to before
    
    * add overlapping card when hovered
    
    * update css
    
    * Move product info into json

--- dti-website-redesign/src/pages/_app.tsx
+++ dti-website-redesign/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from 'react';
 
 import type { AppProps } from 'next/app';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './App.css';
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => <Component {...pageProps} />;
