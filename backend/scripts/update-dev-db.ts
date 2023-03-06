c4a1c0430f9e4ec4515216b1c64c9170544bd8d5
Henry Li <hl738@cornell.edu>
2023-03-06 17:42:13 -0500

    Refactor backend firebase config (#401)

--- backend/scripts/update-dev-db.ts
+++ backend/scripts/update-dev-db.ts
@@ -1,4 +1,5 @@
 import admin from 'firebase-admin';
+import configureAccount from '../src/utils/firebase-utils';
 
 require('dotenv').config();
 
@@ -12,23 +13,6 @@ type DbData = {
     data: unknown;
   }[];
 }[];
-
-const configureAccount = (sa: unknown, isProd: boolean): unknown => {
-  const configAcc = sa;
-  let parsedPK;
-  try {
-    parsedPK = isProd
-      ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string)
-      : JSON.parse(process.env.FIREBASE_DEV_PRIVATE_KEY as string);
-  } catch (err) {
-    parsedPK = isProd ? process.env.FIREBASE_PRIVATE_KEY : process.env.FIREBASE_DEV_PRIVATE_KEY;
-  }
-  configAcc.private_key = parsedPK;
-  configAcc.private_key_id = isProd
-    ? process.env.FIREBASE_PRIVATE_KEY_ID
-    : process.env.FIREBASE_DEV_PRIVATE_KEY_ID;
-  return configAcc;
-};
 
 async function deleteCollection(
   db: FirebaseFirestore.Firestore,
