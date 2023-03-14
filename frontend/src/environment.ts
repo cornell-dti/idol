33bcd0b93a1122f9c68d2aa9f75f111f5c471b55
jks273 <jks273@cornell.edu>
2023-03-14 18:51:03 -0400

    Add default values for `environment.ts`

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,17 +1,23 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 
-export const isStaging: boolean = JSON.parse(process.env.NEXT_PUBLIC_IS_STAGING as string);
+export const isStaging: boolean = process.env.NEXT_PUBLIC_IS_STAGING
+  ? JSON.parse(process.env.NEXT_PUBLIC_IS_STAGING as string)
+  : false;
 
 /** Switch to true when using prod API for dev. */
-export const useProdBackendForDev: boolean = JSON.parse(
-  process.env.NEXT_PUBLIC_USE_PROD_BACKEND_FOR_DEV as string
-);
+export const useProdBackendForDev: boolean = process.env.NEXT_PUBLIC_USE_PROD_BACKEND_FOR_DEV
+  ? JSON.parse(process.env.NEXT_PUBLIC_USE_PROD_BACKEND_FOR_DEV as string)
+  : false;
 
 /** Switch to false to use development Firebase instance. */
-export const useProdDb: boolean = JSON.parse(process.env.NEXT_PUBLIC_USE_PROD_DB as string);
+export const useProdDb: boolean = process.env.NEXT_PUBLIC_USE_PROD_DB
+  ? JSON.parse(process.env.NEXT_PUBLIC_USE_PROD_DB as string)
+  : true;
 
 /** Switch to false to test IDOL as a non-admin user. */
-export const allowAdmin: boolean = JSON.parse(process.env.NEXT_PUBLIC_ALLOW_ADMIN as string);
+export const allowAdmin: boolean = process.env.NEXT_PUBLIC_ALLOW_ADMIN
+  ? JSON.parse(process.env.NEXT_PUBLIC_ALLOW_ADMIN as string)
+  : true;
 
 export const backendURL =
   isProduction || !useProdBackendForDev
