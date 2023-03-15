c8f8bb6a1c6eecf9931a480c9e8609524750f335
Samuel Yeboah <sy433@cornell.edu>
2023-03-15 12:42:48 -0400

    Changed create, update - Create method so that it returns new TeamEventAttendance object with accurate info - Update to use spread - Added spec

--- backend/src/dao/TeamEventAttendanceDao.ts
+++ backend/src/dao/TeamEventAttendanceDao.ts
@@ -1,51 +1,52 @@
-import * as admin from 'firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
 import { memberCollection, teamEventAttendanceCollection } from '../firebase';
 import { DBTeamEventAttendance } from '../types/DataTypes';
 
+/**
+ * Creates a new TEC Attendance for member for a given team event
+ * @param teamEventAttendance - Newly created TeamEventAttendance object.
+ * If provided, the object uuid will be used. If not, a new one will be generated.
+ * The pending field will be set to true by default.
+ */
 export default class TeamEventAttendanceDao {
   static async createTeamEventAttendance(
     teamEventAttendance: TeamEventAttendance
   ): Promise<TeamEventAttendance> {
     const teamAttendanceRef: DBTeamEventAttendance = {
       ...teamEventAttendance,
-      member: memberCollection.doc(teamEventAttendance.member.email),
-      ...(typeof teamEventAttendance.hoursAttended === 'number'
-        ? { hoursAttended: teamEventAttendance.hoursAttended }
-        : {}),
       pending: true,
+      member: memberCollection.doc(teamEventAttendance.member.email),
       uuid: teamEventAttendance.uuid ? teamEventAttendance.uuid : uuidv4()
     };
     await teamEventAttendanceCollection.doc(teamAttendanceRef.uuid).set(teamAttendanceRef);
+
+    const createdEventAttendance = {
+      ...teamAttendanceRef,
+      member: teamEventAttendance.member
+    };
+    return createdEventAttendance;
+  }
+
+  /**
+   * Updates a TEC Attendance
+   * @param teamEventAttendance - updated TeamEventAttendance object
+   *  If hoursAttended is originally defined, it cannot be overwritten afterwards
+   */
+  static async updateTeamEventAttendance(
+    teamEventAttendance: TeamEventAttendance
+  ): Promise<TeamEventAttendance> {
+    const teamAttendanceRef: DBTeamEventAttendance = {
+      ...teamEventAttendance,
+      member: memberCollection.doc(teamEventAttendance.member.email)
+    };
+    await teamEventAttendanceCollection.doc(teamAttendanceRef.uuid).update(teamAttendanceRef);
     return teamEventAttendance;
   }
 
-  static async updateTeamEventAttendance(
-    attendance: TeamEventAttendance
-  ): Promise<TeamEventAttendance> {
-    const teamAttendanceRef: DBTeamEventAttendance = {
-      ...attendance,
-      member: memberCollection.doc(attendance.member.email)
-    };
-
-    const docRef = await teamEventAttendanceCollection.doc(teamAttendanceRef.uuid);
-    const currentDoc = await docRef.get();
-    const currentAttendance = currentDoc.data() as DBTeamEventAttendance;
-
-    if (
-      teamAttendanceRef.hoursAttended === undefined &&
-      currentAttendance.hoursAttended !== undefined
-    ) {
-      await docRef.update(
-        new admin.firestore.FieldPath('hoursAttended'),
-        admin.firestore.FieldValue.delete()
-      );
-    }
-    await docRef.update(teamAttendanceRef);
-
-    return attendance;
-  }
-
+  /**
+   * Updates a TEC Attendance
+   * @param teamEventAttendance - updated TeamEventAttendance object
+   */
   static async deleteTeamEventAttendance(uuid: string): Promise<void> {
     const docRef = teamEventAttendanceCollection.doc(uuid);
     await docRef.delete();
