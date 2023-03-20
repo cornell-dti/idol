be1fdd7d1908edc024e7163959edb758830df16d
Henry Li <hl738@cornell.edu>
2023-03-19 21:44:05 -0400

    add specs to BaseDao

--- backend/src/dao/BaseDao.ts
+++ backend/src/dao/BaseDao.ts
@@ -17,6 +17,11 @@ export default abstract class BaseDao<E, D> {
     this.serializeData = serializeData;
   }
 
+  /**
+   * Gets a document with docID
+   * @param docID
+   * @returns The document with id: docID
+   */
   protected async getDocument(docID: string): Promise<E | null> {
     const doc = await this.collection.doc(docID).get();
     const data = doc.data();
@@ -24,21 +29,40 @@ export default abstract class BaseDao<E, D> {
     return this.materializeData(data);
   }
 
+  /**
+   * @returns All documents in the collection
+   */
   protected async getAllDocuments(): Promise<E[]> {
     const docRefs = await this.collection.get();
     return Promise.all(docRefs.docs.map((doc) => this.materializeData(doc.data())));
   }
 
+  /**
+   * Creates a document with id: docID and data: data
+   * @param docID - The id of the document
+   * @param data - The data in the document
+   * @returns The new document
+   */
   protected async createDocument(docID: string, data: E): Promise<E> {
     const doc = await this.serializeData(data);
     await this.collection.doc(docID).set(doc);
     return data;
   }
 
+  /**
+   * Deletes document with id docID
+   * @param docID - The ide of the document to delete
+   */
   protected async deleteDocument(docID: string): Promise<void> {
     await this.collection.doc(docID).delete();
   }
 
+  /**
+   * Updates a document
+   * @param docID - The id of the document to update
+   * @param data - The updated data for the document
+   * @returns - The newly updated document
+   */
   protected async updateDocument(docID: string, data: E): Promise<E> {
     const doc = await this.serializeData(data);
     await this.collection.doc(docID).update(doc as firestore.UpdateData);
