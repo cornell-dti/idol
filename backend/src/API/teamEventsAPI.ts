713f55c9cde2517a769dd63270b732f8626e3dab
Oscar Wang <oscarwang22396@gmail.com>
2023-03-27 17:19:43 -0400

    implement getTECAByUser

--- backend/src/API/teamEventsAPI.ts
+++ backend/src/API/teamEventsAPI.ts
@@ -1,3 +1,4 @@
+import TeamEventAttendanceDao from '../dao/TeamEventAttendanceDao';
 import TeamEventsDao from '../dao/TeamEventsDao';
 import { PermissionError } from '../utils/errors';
 import PermissionsManager from '../utils/permissionsManager';
@@ -75,3 +76,14 @@ export const getAllTeamEventsForMember = async (
   email: string,
   isPending: boolean
 ): Promise<TeamEventInfo[]> => TeamEventsDao.getTeamEventsForMember(email, isPending);
+
+export const getTeamEventAttendanceByUser = async (
+  user: IdolMember
+): Promise<TeamEventAttendance[]> => {
+  const canEditTeamEvent = await PermissionsManager.canEditTeamEvent(user);
+  if (!canEditTeamEvent)
+    throw new PermissionError(
+      `User with email ${user.email} does not have sufficient permissions to get all team event attendance by user.`
+    );
+  return TeamEventAttendanceDao.getTeamEventAttendanceByUser(user);
+};
