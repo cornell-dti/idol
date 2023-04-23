d86cf2b987c50db8e9adde7c1a589cd011bbaf19
kembayeb <125849200+kembayeb@users.noreply.github.com>
2023-04-23 00:41:02 -0400

    Refactor TeamEventAPI and TeamEventDao to recieve TEAttencanceDao data (#452)
    * changed firebase to reference TeamEventInfo instead of DBTeamEvent
    
    * made changes in TeamEventsDao that allows for the replacement of TeamEvent type with TeamEventInfo
    
    * made necessary changes in TeamEventAPI so that its able to recieve attendance info from its dao
    
    * changed getAllTeamEvents
    
    * deleted DBTeamEvent type, ran formatter
    
    * removed calls to getAllTeamEventsForMember as its now deleted
    
    * package lock
    
    * Revert "package lock"
    
    This reverts commit 8fba7f05df30c170f49f7b90855fb63b10f107d6.
    
    * changed to account for BaseDao changes
    
    * Changes to deleteTeamEvent and RequestTeamEventCredit
    - Changed deleteTeamEvent so that it properly deletes each team event attendance for the event
    - Changed RequestTeamEventCredit, so to takes in an idol user, and checks the attendance member and user have same email
    
    * made sure to delete requests as well when deleting teamEvent, code polishes
    
    * attendance.pending instead of === true
    
    ---------
    
    Co-authored-by: Samuel Yeboah sy433 <sy433@cornell.edu>

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -51,8 +51,7 @@ import {
   getTeamEvent,
   updateTeamEvent,
   clearAllTeamEvents,
-  requestTeamEventCredit,
-  getAllTeamEventsForMember
+  requestTeamEventCredit
 } from './API/teamEventsAPI';
 import {
   getAllCandidateDeciderInstances,
@@ -315,14 +314,10 @@ loginCheckedDelete('/clearAllTeamEvents', async (_, user) => {
 loginCheckedGet('/getAllTeamEventInfo', async () => ({
   allTeamEventInfo: await getAllTeamEventInfo()
 }));
-loginCheckedPost('/requestTeamEventCredit', async (req, _) => {
-  await requestTeamEventCredit(req.body.uuid, req.body.request);
+loginCheckedPost('/requestTeamEventCredit', async (req, user) => {
+  await requestTeamEventCredit(req.body.uuid, user);
   return {};
 });
-loginCheckedGet('/getAllTeamEventsForMember', async (_, user) => ({
-  pending: await getAllTeamEventsForMember(user.email, true),
-  approved: await getAllTeamEventsForMember(user.email, false)
-}));
 
 // Team Events Proof Image
 loginCheckedGet('/getEventProofImage/:name(*)', async (req, user) => ({
