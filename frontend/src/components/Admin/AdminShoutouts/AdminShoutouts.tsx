bf8691aa4228b0974f7657f72c8d241001cfeb3d
Samuel Yeboah sy433 <sy433@cornell.edu>
2023-05-09 00:34:22 -0400

    firebase listener in shoutouts

--- frontend/src/components/Admin/AdminShoutouts/AdminShoutouts.tsx
+++ frontend/src/components/Admin/AdminShoutouts/AdminShoutouts.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Item, Card, Modal, Header, Loader } from 'semantic-ui-react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { Button, Form, Item, Card, Modal, Header } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
+import { db } from '../../../firebase';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Emitters } from '../../../utils';
 import ShoutoutsAPI from '../../../API/ShoutoutsAPI';
@@ -31,7 +33,7 @@ const getListTitle = (view: ViewMode): string => {
 
 const HideModal = (props: {
   shoutout: Shoutout;
-  setAllShoutouts: React.Dispatch<React.SetStateAction<Shoutout[] | undefined>>;
+  setAllShoutouts: React.Dispatch<React.SetStateAction<Shoutout[]>>;
 }): JSX.Element => {
   const { shoutout, setAllShoutouts } = props;
 
@@ -39,7 +41,7 @@ const HideModal = (props: {
     const oppHide = !shoutout.hidden;
     ShoutoutsAPI.hideShoutout(shoutout.uuid, oppHide).then(() => {
       setAllShoutouts((shoutouts) =>
-        shoutouts?.map((s) => (s.uuid === shoutout.uuid ? { ...s, hidden: oppHide } : s))
+        shoutouts.map((s) => (s.uuid === shoutout.uuid ? { ...s, hidden: oppHide } : s))
       );
       if (oppHide) {
         Emitters.generalSuccess.emit({
@@ -95,13 +97,10 @@ const DisplayList = ({
   view,
   setAllShoutouts
 }: {
-  displayShoutouts: Shoutout[] | undefined;
+  displayShoutouts: Shoutout[];
   view: ViewMode;
-  setAllShoutouts: React.Dispatch<React.SetStateAction<Shoutout[] | undefined>>;
+  setAllShoutouts: React.Dispatch<React.SetStateAction<Shoutout[]>>;
 }): JSX.Element => {
-  if (displayShoutouts === undefined) {
-    return <Loader active size="big" content="Loading..." />;
-  }
   if (displayShoutouts.length === 0)
     return (
       <Card className={styles.noShoutoutsContainer}>
@@ -173,7 +172,26 @@ const AdminShoutouts: React.FC = () => {
 
   useEffect(() => {
     ShoutoutsAPI.getAllShoutouts().then((shoutouts) => setAllShoutouts(shoutouts));
-  }, []);
+
+    const shoutoutCollection = collection(db, 'shoutouts');
+    const unsubscribe = onSnapshot(shoutoutCollection, (snapshot) => {
+      snapshot.docChanges().forEach((document) => {
+        if (document.type === 'added') {
+          const newShoutout = document.doc.data() as Shoutout;
+          setAllShoutouts((oldShoutouts = []) => [...oldShoutouts, newShoutout]);
+        }
+        if (document.type === 'removed') {
+          const removedShoutout = document.doc.data() as Shoutout;
+          setAllShoutouts((updatedList = []) =>
+            updatedList.filter((x) => x.uuid !== removedShoutout.uuid)
+          );
+        }
+      });
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [setAllShoutouts]);
 
   if (lastDate < earlyDate) {
     Emitters.generalError.emit({
@@ -182,18 +200,18 @@ const AdminShoutouts: React.FC = () => {
     });
   }
 
-  const displayShoutouts = allShoutouts
-    ?.filter((shoutout) => {
-      const shoutoutDate = new Date(shoutout.timestamp);
-      return (
-        (view === 'ALL' ||
-          (view === 'PRESENT' && !shoutout.hidden) ||
-          (view === 'HIDDEN' && shoutout.hidden)) &&
-        shoutoutDate >= earlyDate &&
-        shoutoutDate <= lastDate
-      );
-    })
-    .sort((a, b) => a.timestamp - b.timestamp);
+  // const displayShoutouts = allShoutouts
+  // ?.filter((shoutout) => {
+  //   const shoutoutDate = new Date(shoutout.timestamp);
+  //   return (
+  //     (view === 'ALL' ||
+  //       (view === 'PRESENT' && !shoutout.hidden) ||
+  //       (view === 'HIDDEN' && shoutout.hidden)) &&
+  //     shoutoutDate >= earlyDate &&
+  //     shoutoutDate <= lastDate
+  //   );
+  // })
+  // .sort((a, b) => a.timestamp - b.timestamp);
 
   return (
     <div>
@@ -218,9 +236,13 @@ const AdminShoutouts: React.FC = () => {
       <div className={styles.shoutoutsListContainer}>
         <Header className={styles.formTitle} content={getListTitle(view)}></Header>
         <DisplayList
-          displayShoutouts={displayShoutouts}
+          displayShoutouts={allShoutouts?.length ? allShoutouts : []}
           view={view}
-          setAllShoutouts={setAllShoutouts}
+          setAllShoutouts={(shoutouts) => {
+            if (shoutouts) {
+              setAllShoutouts((shoutouts = []) => shoutouts);
+            }
+          }}
         />
       </div>
     </div>
