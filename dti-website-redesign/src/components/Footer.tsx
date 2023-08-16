7f40a968de86d271679d5190a450bd41801f8d75
Andrew Chen <andrew032012@gmail.com>
2023-08-15 19:53:46 -0700

    prettier

--- dti-website-redesign/src/components/Footer.tsx
+++ dti-website-redesign/src/components/Footer.tsx
@@ -66,7 +66,9 @@ const Footer: React.FC = () => (
           </a>
         ))}
       </div>
-      <div className={styles.copyrightContainer}>Cornell Digital Technology & Innovation &copy; 2023</div>
+      <div className={styles.copyrightContainer}>
+        Cornell Digital Technology & Innovation &copy; 2023
+      </div>
     </div>
   </div>
 );
