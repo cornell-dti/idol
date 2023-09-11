9a92c485cf27a24a64f71d31780ef992eaf144da
Andrew Chen <59291082+andrew032011@users.noreply.github.com>
2023-09-11 17:26:17 -0400

    change out nav logo (#474)

--- dti-website-redesign/src/components/Navbar.tsx
+++ dti-website-redesign/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = ({ hidden }: Props): JSX.Element =>
         <Link href="/">
           {/* eslint-disable jsx-a11y/anchor-is-valid */}
           <a className={styles.navLogo}>
-            <img src="/branding/wordmark.svg" alt="wordmark" />
+            <img src="/branding/wordmark.svg" className={styles.navLogoImage} alt="wordmark" />
           </a>
         </Link>
 
