55fc8cc31ce8715dae9e0195634d7a046bd86b30
Andrew Chen <andrew032012@gmail.com>
2023-09-24 11:39:27 -0400

    remove unused useEffect

--- frontend/src/components/Candidate-Decider/CandidateDecider.tsx
+++ frontend/src/components/Candidate-Decider/CandidateDecider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Dropdown, Checkbox } from 'semantic-ui-react';
 import CandidateDeciderAPI from '../../API/CandidateDeciderAPI';
 import ResponsesPanel from './ResponsesPanel';
