5d740d8dd09b6dfd76d78172d04ea1c777c098f7
Andrew Chen <andrew032012@gmail.com>
2023-10-12 18:17:55 -0400

    format

--- new-dti-website/src/app/page.tsx
+++ new-dti-website/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <Button>This is a ShadCN Button.</Button>
     </div>
-  )
+  );
 }
