6a2bb358c8858ce557d8607684683c7ea5f86bfd
Andrew Chen <andrew032012@gmail.com>
2023-10-13 16:53:28 -0400

    add next/font and fix linting errors

--- new-dti-website/lib/utils.ts
+++ new-dti-website/lib/utils.ts
@@ -1,4 +1,4 @@
-import { type ClassValue, clsx } from 'clsx';
+import { type ClassValue, clsx, } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 // eslint-disable-next-line import/prefer-default-export
