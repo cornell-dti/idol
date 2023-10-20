6c8503be6c1f4552186b7d51efeef7769e1270f8
Kevin Ram <iamkevinmram@gmail.com>
2023-10-20 12:42:42 -0400

    ^

--- new-dti-website/components/icons.tsx
+++ new-dti-website/components/icons.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface IconProps {
   defaultClass: string;
   altText: string;
+  dataIndex: number;
 }
 
-const Icons: React.FC<IconProps> = ({ defaultClass, altText }) => (
+const Icons: React.FC<IconProps> = ({ defaultClass, altText, dataIndex }) => (
   <div 
     className={`icon-container ${defaultClass}`}
     aria-label={altText} 
     role="img"
+    data-index={dataIndex}
   ></div>
 );
 
--- new-dti-website/src/app/page.tsx
+++ new-dti-website/src/app/page.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './assets/hero.css';
 import Icons from '../../components/icons';
+import Slideshow from '../../components/slideshow';
 
 const Home: React.FC = () => (
   <div className="hero-section" style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingLeft: '20%' }}>
-    <Icons defaultClass="dti" altText="DTI" />
-    <Icons defaultClass="family" altText="Family" />
-    <Icons defaultClass="collaboration" altText="Collaboration" />
-    <Icons defaultClass="events" altText="Events" />
-    <Icons defaultClass="initiatives" altText="Initiatives" />
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginRight: '50px' }}>
+      <h2 style={{ color: 'white', marginBottom: '20px', fontSize: '36px' }}>Cornell Digital <br></br> Tech & Innovation</h2>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+        <Icons defaultClass="dti" altText="DTI" dataIndex={0} />
+        <Icons defaultClass="family" altText="Family" dataIndex={1} />
+        <Icons defaultClass="collaboration" altText="Collaboration" dataIndex={2} />
+        <Icons defaultClass="events" altText="Events" dataIndex={3} />
+        <Icons defaultClass="initiatives" altText="Initiatives" dataIndex={4} />
+      </div>
+    </div>
+    <Slideshow />
   </div>
 );
 
