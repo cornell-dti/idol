c6e9ba21811f0f6387ab75ec9a744062d6b59eec
Alyssa Zhang <alyssayzhang@gmail.com>
2023-10-21 13:38:02 -0400

    social svg icons

---
+++ new-dti-website/components/footer.tsx
@@ -0,0 +1,8 @@
+
+import GitHub from "../assets/social/github.svg";
+import Facebook from "../assets/social/facebook.svg";
+import Instagram from "../assets/social/instagram.svg";
+import Linkedin from "../assets/social/linkedin.svg";
+import Medium from "../assets/social/medium.svg";
+
+
