0dcaeb2547e541d2d652c6643363862ca62d12a9
Andrew Chen <andrew032012@gmail.com>
2023-10-22 20:48:40 -0400

    Add Firebase Utils Documentation

--- backend/src/utils/firebase-utils.ts
+++ backend/src/utils/firebase-utils.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+/**
+ * Takes service account credentials and populates the private_key_id and private_key fields with data from the .env.
+ * @param serviceAccount - Service Account object containing non-sensitive Firebase credentials.
+ * @param isProd - Whether or not we should use the Firebase prod credentials. If False, the development DB credentials are used.
+ * @returns - Credentials object with private key and private key id.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-export const configureAccount = (sa: any, isProd: boolean) => {
-  const configAcc = sa;
+export const configureAccount = (serviceAccount: any, isProd: boolean) => {
+  const configAcc = serviceAccount;
   let parsedPK;
   try {
     parsedPK = isProd
@@ -17,11 +24,17 @@ export const configureAccount = (sa: any, isProd: boolean) => {
   return configAcc;
 };
 
+/**
+ * Deletes an entire collection by deleting the documents in batches of batchSize.
+ * @param db - The database in which the collection will be deleted.
+ * @param collectionPath - Path associated with the collection to be deleted.
+ * @param batchSize - Whether or not we should use the Firebase prod credentials. If False, the development DB credentials are used.
+ */
 export async function deleteCollection(
   db: FirebaseFirestore.Firestore,
   collectionPath: string,
   batchSize: number
-) {
+): Promise<void> {
   const collectionRef = db.collection(collectionPath);
   const query = collectionRef.orderBy('__name__').limit(batchSize);
 
@@ -30,11 +43,17 @@ export async function deleteCollection(
   });
 }
 
+/**
+ * Recursive function that deletes all documents in the db that match query.
+ * @param db - The database in which the documents will be deleted.
+ * @param query - Query describing the documents that will be deleted.
+ * @param resolve - The callback function called upon deleting all documents that match the query.
+ */
 export async function deleteQueryBatch(
   db: FirebaseFirestore.Firestore,
   query: FirebaseFirestore.Query,
-  resolve
-) {
+  resolve: () => void
+): Promise<void> {
   const snapshot = await query.get();
 
   const batchSize = snapshot.size;
