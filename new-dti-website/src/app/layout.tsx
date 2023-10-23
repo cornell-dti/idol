673241d4a50c4122d36008950caa7731cb8406fb
Oscar Wang <oscarwang22396@gmail.com>
2023-10-23 10:08:18 -0400

    removed scroll testing images

--- new-dti-website/src/app/layout.tsx
+++ new-dti-website/src/app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }): JSX.Element => (
   <html lang="en">
-    <body className={`${inter.className} bg-black`}>{children}</body>
+    <body className={inter.className}>{children}</body>
   </html>
 );
 
--- new-dti-website/src/app/page.tsx
+++ new-dti-website/src/app/page.tsx
@@ -4,24 +4,7 @@ import Navbar from '../../components/navbar';
 const Home = (): JSX.Element => (
   <div>
     <Navbar />
-    <div className="">
-      <Button>This is a ShadCN Button.</Button>
-      <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F04%2FGettyImages-1153369684.jpg&f=1&nofb=1&ipt=ba5abf1adc619994565e92a9dba42a634a7abe644a1e89890eb66475380ff327&ipo=images"
-        alt="A cute cat."
-        className="w-screen"
-      />
-      <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.REa9Uiv1NBhD5mdU-N6d_QHaE8%26pid%3DApi&f=1&ipt=b6bc513da1feb8e6824ea09f2a19c86068012f3466447546143e0cea792b70b7&ipo=images"
-        alt="A cute cat."
-        className="w-screen"
-      />
-      <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.tenor.com%2F7t63GFnoIPUAAAAd%2Fhuh-cat-huh-m4rtin.gif&f=1&nofb=1&ipt=b3787e00d10b1997812af5a006e4302a9c3b3a7760bb6424cdfe2dfd77b0d91e&ipo=images"
-        alt="A cute cat."
-        className="w-screen"
-      />
-    </div>
+    <Button>This is a ShadCN Button.</Button>
   </div>
 );
 
