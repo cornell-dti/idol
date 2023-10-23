6b1373a4d981bd8fa805bb9cc2a37b2578fc84ca
Alyssa Zhang <alyssayzhang@gmail.com>
2023-10-23 18:17:55 -0400

    implement static footer at bottom of screen

--- new-dti-website/components/footer.tsx
+++ new-dti-website/components/footer.tsx
@@ -1,8 +1,66 @@
+import React from 'react';
+import Image from 'next/image'
 
-import GitHub from "../assets/social/github.svg";
-import Facebook from "../assets/social/facebook.svg";
-import Instagram from "../assets/social/instagram.svg";
-import Linkedin from "../assets/social/linkedin.svg";
-import Medium from "../assets/social/medium.svg";
+type Icon = {
+  src: string;
+  link: string;
+  alt: string;
+};
 
+const socialIcons: Icon[] = [
+  {
+    src: '/github.svg',
+    link: 'https://www.github.com/cornell-dti',
+    alt: 'Cornell DTI Github',
+  },
+  {
+    src: '/facebook.svg',
+    link: 'https://www.facebook.com/cornelldti',
+    alt: 'Cornell DTI Facebook',
+  },
+  {
+    src: '/instagram.svg',
+    link: 'https://www.instagram.com/cornelldti',
+    alt: 'Cornell DTI Instagram',
+  },
+  {
+    src: '/linkedin.svg',
+    link: 'https://www.linkedin.com/company/cornelldti',
+    alt: 'Cornell DTI LinkedIn',
+  },
+  {
+    src: '/medium.svg',
+    link: 'https://medium.com/cornelldti',
+    alt: 'Cornell DTI Medium',
 
+  }
+];
+
+const Footer: React.FC = () => (
+  <div className="w-full h-[146px] px-[60px] fixed inset-x-0 bottom-0 bg-stone-950 inline-flex justify-between items-center">
+    <div className="text-neutral-50 text-xl font-medium font-['IBM Plex Mono']">
+      © 2023 Cornell Digital Tech & Innovation
+    </div>
+    <div className="flex gap-4">
+      {socialIcons.map((icon, index) => (
+        <a
+          key={index}
+          href={icon.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-9 h-9 relative"
+        >
+        <Image
+          className="w-full h-full"
+          src= {icon.src}
+          width={36}
+          height={36}
+          alt={icon.alt}
+        />
+        </a>
+      ))}
+    </div>
+  </div>
+);
+
+export default Footer;
--- new-dti-website/src/app/page.tsx
+++ new-dti-website/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from '../../components/ui/button';
+import Footer from '../../components/footer';
 
 const Home = (): JSX.Element => (
   <div>
     <Button>This is a ShadCN Button.</Button>
+    <Footer />
   </div>
 );
 
