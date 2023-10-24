9551e7dbf55953e80eb23a234d22a4f62969655e
Oscar Wang <oscarwang22396@gmail.com>
2023-10-24 15:52:31 -0400

    pending in teamEventCreditsForm.tsx

--- frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
+++ frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
@@ -75,6 +75,7 @@ const TeamEventCreditForm: React.FC = () => {
         hoursAttended: teamEvent.hasHours ? Number(hours) : undefined,
         image: `eventProofs/${getNetIDFromEmail(userInfo.email)}/${new Date().toISOString()}`,
         eventUuid: teamEvent.uuid,
+        pending: true,
         status: 'pending' as Status,
         reason: '',
         uuid: ''
