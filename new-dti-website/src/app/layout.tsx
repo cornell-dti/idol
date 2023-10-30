f29cf5c1b9033a30e522dec28b9b5fe67661bd36
Alyssa Zhang <alyssayzhang@gmail.com>
2023-10-30 17:49:32 -0400

    edit footer text

--- new-dti-website/src/app/layout.tsx
+++ new-dti-website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter, IBM_Plex_Mono } from 'next/font/google';
 import Footer from '../../components/footer';
+import Navbar from '../../components/navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,10 +12,14 @@ export const ibm_plex_mono = IBM_Plex_Mono({
 
 const RootLayout = ({ children }: { children: React.ReactNode }): JSX.Element => (
   <html lang="en">
-    <body className={inter.className}>{children}</body>
-    {/* <div className={ibm_plex_mono.className}>
-      <Footer />
-    </div> */}
+    <div className={inter.className}>
+      {' '}
+      <Navbar />{' '}
+    </div>
+    <div className={ibm_plex_mono.className}>
+      {' '}
+      <Footer />{' '}
+    </div>
   </html>
 );
 
