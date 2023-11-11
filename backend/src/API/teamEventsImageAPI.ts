d52b70d8f02de649d6cb219fc3455f0e1b2b818d
Alyssa Zhang <alyssayzhang@gmail.com>
2023-11-10 20:24:38 -0500

    add specs to methods

--- backend/src/API/teamEventsImageAPI.ts
+++ backend/src/API/teamEventsImageAPI.ts
@@ -2,6 +2,12 @@ import { bucket } from '../firebase';
 import { getNetIDFromEmail } from '../utils/memberUtil';
 import { NotFoundError } from '../utils/errors';
 
+/**
+ * Sets TEC proof image for member
+ * @param name - the name of the image
+ * @param user - an IdolMember object
+ * @returns a Promise to the signed URL to the image file
+ */
 export const setEventProofImage = async (name: string, user: IdolMember): Promise<string> => {
   const file = bucket.file(`${name}.jpg`);
   const signedURL = await file.getSignedUrl({
@@ -12,6 +18,13 @@ export const setEventProofImage = async (name: string, user: IdolMember): Promis
   return signedURL[0];
 };
 
+/**
+ * Gets TEC proof image for member
+ * @param name - the name of the image
+ * @param user - an IdolMember object
+ * @throws NotFoundError if the request image does not exist
+ * @returns a Promise to the signed URL to the image file
+ */
 export const getEventProofImage = async (name: string, user: IdolMember): Promise<string> => {
   const file = bucket.file(`${name}.jpg`);
   const fileExists = await file.exists().then((result) => result[0]);
@@ -25,6 +38,11 @@ export const getEventProofImage = async (name: string, user: IdolMember): Promis
   return signedUrl[0];
 };
 
+/**
+ * Gets all TEC proof images associated with the IdolMember
+ * @param user - an IdolMember object
+ * @returns a Promise which results in an array of EventProofImage with file name and signed URL
+ */
 export const allEventProofImagesForMember = async (
   user: IdolMember
 ): Promise<readonly EventProofImage[]> => {
@@ -43,22 +61,29 @@ export const allEventProofImagesForMember = async (
       };
     })
   );
-
   images
     .filter((image) => image.fileName.length > 'eventProofs/'.length)
     .map((image) => ({
       ...image,
       fileName: image.fileName.slice(image.fileName.indexOf('/') + 1)
     }));
-
   return images;
 };
 
+/**
+ * Deletes TEC proof image for member
+ * @param name - the name of the image
+ * @param user - an IdolMember object
+ */
 export const deleteEventProofImage = async (name: string, user: IdolMember): Promise<void> => {
   const imageFile = bucket.file(`${name}.jpg`);
   await imageFile.delete();
 };
 
+/**
+ * Deletes all TEC proof images for given member
+ * @param user - an IdolMember object
+ */
 export const deleteEventProofImagesForMember = async (user: IdolMember): Promise<void> => {
   const netId: string = getNetIDFromEmail(user.email);
   const files = await bucket.getFiles({ prefix: `eventProofs/${netId}` });
