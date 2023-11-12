01543b4c53e391a7da316e6b6a3bb068fdafa178
Patricia Huang <pattycthuang@gmail.com>
2023-11-12 01:36:41 -0500

    add documentation to mail api

--- backend/src/API/mailAPI.ts
+++ backend/src/API/mailAPI.ts
@@ -8,6 +8,14 @@ import { PermissionError } from '../utils/errors';
 import { getAllTeamEvents, getTeamEventAttendanceByUser } from './teamEventsAPI';
 import { env } from '../firebase';
 
+/**
+ * Sends an email to the specified member's email
+ * @param to - The member's email
+ * @param subject - The subject of the email
+ * @param text - The body of the email
+ * @param user - The member trying to send the email
+ * @returns - The information in the email
+ */
 export const sendMail = async (
   to: string,
   subject: string,
@@ -42,6 +50,12 @@ export const sendMail = async (
   return info;
 };
 
+/**
+ * Determines which URL to use to send an email, depending on if the production 
+ * environment is used
+ * @param req - The request made by the user
+ * @returns - The URL to use to send an email
+ */
 const getSendMailURL = (req: Request): string => {
   if (isProd) {
     return `https://${req.hostname}/.netlify/functions/api/sendMail`;
@@ -49,6 +63,13 @@ const getSendMailURL = (req: Request): string => {
   return 'http://localhost:9000/.netlify/functions/api/sendMail';
 };
 
+/**
+ * Send an email to the admins
+ * @param req - The request made when sending the email
+ * @param subject - The subject of the email
+ * @param text - The body of the email
+ * @returns - The information of the admins being sent the emails
+ */
 const emailAdmins = async (req: Request, subject: string, text: string) => {
   const url = getSendMailURL(req);
   const adminEmails = await AdminsDao.getAllAdminEmails();
@@ -63,6 +84,14 @@ const emailAdmins = async (req: Request, subject: string, text: string) => {
   });
 };
 
+/**
+ * Send an email to a member
+ * @param req - The request made when sending the email
+ * @param member - The member being sent the email
+ * @param subject - The subject of the email
+ * @param text - The body of the email
+ * @returns - The information of the member being sent the email
+ */
 const emailMember = async (req: Request, member: IdolMember, subject: string, text: string) => {
   const url = getSendMailURL(req);
   const idToken = req.headers['auth-token'] as string;
@@ -78,6 +107,11 @@ const emailMember = async (req: Request, member: IdolMember, subject: string, te
   );
 };
 
+/**
+ * Send an email about a member updating their notifications
+ * @param req - The request made when sending the email
+ * @returns - The information of the admins being sent the emails
+ */
 export const sendMemberUpdateNotifications = async (req: Request): Promise<Promise<void>[]> => {
   const subject = 'IDOL Member Profile Change';
   const text =
@@ -85,6 +119,12 @@ export const sendMemberUpdateNotifications = async (req: Request): Promise<Promi
   return emailAdmins(req, subject, text);
 };
 
+/**
+ * Send an email reminder to members who do not have enough TEC credits
+ * @param req - The request made when sending the email
+ * @param member - The member being sent the email
+ * @returns - The information of the member being sent the email
+ */
 export const sendTECReminder = async (req: Request, member: IdolMember): Promise<AxiosResponse> => {
   const subject = 'TEC Reminder';
   const allEvents = await getAllTeamEvents(req.body);
