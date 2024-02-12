45fee829fdcbdcf27ecb74e5de572b072dd34709
Oscar Wang <oscarwang22396@gmail.com>
2024-02-11 23:37:15 -0500

    update middleware file

---
+++ frontend/src/pages/_middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { useHasAdminPermission } from '../components/Common/FirestoreDataProvider';
+
+export default async function middleware(req: NextRequest) {
+  const hasAdminPersissions = useHasAdminPermission();
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith('/admin') && !hasAdminPersissions) {
+    return NextResponse.redirect('/');
+  }
+  return NextResponse.next();
+}
