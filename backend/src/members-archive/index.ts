f66dfef2572478ab6cc3bd6df221b93a8392781e
Oscar Wang <oscarwang22396@gmail.com>
2024-02-11 21:23:06 -0500

    add fa23 to index.ts

--- backend/src/members-archive/index.ts
+++ backend/src/members-archive/index.ts
@@ -1,11 +1,13 @@
 import sp21 from './sp21.json';
 import fa21 from './fa21.json';
 import sp23 from './sp23.json';
+import fa23 from './fa23.json';
 
 export const archivedMembersBySemesters: Readonly<Record<string, readonly IdolMember[]>> = {
   'Spring 2021': sp21.members as unknown as readonly IdolMember[],
   'Fall 2021': fa21.members as unknown as readonly IdolMember[],
-  'Spring 2023': sp23.members as unknown as readonly IdolMember[]
+  'Spring 2023': sp23.members as unknown as readonly IdolMember[],
+  'Fall 2023': fa23.members as unknown as readonly IdolMember[]
 };
 
 export const archivedMembersByEmail: Readonly<Record<string, IdolMember>> = (() => {
