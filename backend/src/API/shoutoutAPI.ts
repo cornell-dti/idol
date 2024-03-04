8335d0fad9e13775a4c5310680358b539b76135e
keverie <kmr232@cornell.edu>
2024-03-03 22:27:32 -0500

    removed redundant @param from so-api-documentation

--- backend/src/API/shoutoutAPI.ts
+++ backend/src/API/shoutoutAPI.ts
@@ -12,8 +12,6 @@ export const getAllShoutouts = (): Promise<Shoutout[]> => shoutoutsDao.getAllSho
 
 /**
  * Creates a shoutout, ensuring the giver has the correct permissions.
- * @param body - The Shoutout object to be created.
- * @param user - The IdolMember who is giving the shoutout.
  * @throws {PermissionError} If the user attempting to give a shoutout is not the same as the giver specified in the shoutout.
  * @returns A promise that resolves to the created Shoutout object.
  */
@@ -28,9 +26,6 @@ export const giveShoutout = async (body: Shoutout, user: IdolMember): Promise<Sh
 
 /**
  * Retrieves shoutouts based on the type (given or received) for a specific member.
- * @param memberEmail - The email of the member whose shoutouts are to be retrieved.
- * @param type - The type of shoutouts to retrieve ('given' or 'received').
- * @param user - The IdolMember requesting the shoutouts.
  * @throws {PermissionError} If the user does not have permission to view the shoutouts.
  * @returns A promise that resolves to an array of Shoutout objects.
  */
@@ -50,9 +45,6 @@ export const getShoutouts = async (
 
 /**
  * Hides or unhides a shoutout based on the user's permissions.
- * @param uuid - The unique identifier of the shoutout to be hidden or unhidden.
- * @param hide - A boolean indicating whether to hide (true) or unhide (false) the shoutout.
- * @param user - The IdolMember performing the action.
  * @throws {PermissionError} If the user does not have permission to hide shoutouts.
  * @throws {NotFoundError} If no shoutout with the provided uuid exists.
  */
@@ -74,8 +66,6 @@ export const hideShoutout = async (
 
 /**
  * Deletes a shoutout, ensuring the user has the necessary permissions or ownership.
- * @param uuid - The unique identifier of the shoutout to be deleted.
- * @param user - The IdolMember attempting to delete the shoutout.
  * @throws {NotFoundError} If no shoutout with the provided uuid is found.
  * @throws {PermissionError} If the user is neither the giver of the shoutout nor an admin or lead.
  */
