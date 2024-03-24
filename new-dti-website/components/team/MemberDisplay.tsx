6a2abe7f88ec414bf397b669fc5c2222b1dd852a
Chris Chen <cchen6479@gmail.com>
2024-03-23 23:47:33 -0400

    refactoring and fixing nits

--- new-dti-website/components/team/MemberDisplay.tsx
+++ new-dti-website/components/team/MemberDisplay.tsx
@@ -2,111 +2,30 @@ import { useRef, useState } from 'react';
 import MemberGroup from './MemberGroup';
 import Icon from '../icons';
 import FA23Members from '../../../backend/src/members-archive/fa23.json';
+import teamRoles from './team.json';
+import { populateMembers } from '../../src/app/utils';
+
+type DisplayCategory = 'Full Team' | 'Leads' | 'Design' | 'Product' | 'Business' | 'Development';
 
 const MemberDisplay: React.FC = () => {
-  const [selectedRole, setSelectedRole] = useState<string>('Full Team');
+  const [selectedRole, setSelectedRole] = useState<DisplayCategory>('Full Team');
   const [selectedMember, setSelectedMember] = useState<IdolMember | undefined>(undefined);
 
   const memberDetailsRef = useRef<HTMLInputElement>(null);
 
   const allMembers = FA23Members.members as IdolMember[];
-  const roles: {
+
+  const { roleIcons } = teamRoles;
+  const roles = teamRoles.roles as {
     [key in Role]: {
       roleName: string;
       description: string;
       members: IdolMember[];
       order: string[];
     };
-  } = {
-    lead: {
-      roleName: 'Leads',
-      description: 'Striving to connect and mentor our members for their best growth.',
-      members: [],
-      order: ['Full Team Lead', 'Product Lead', 'Developer Lead', 'Design Lead', 'Business Lead']
-    },
-    designer: {
-      roleName: 'Design',
-      description: 'Designing quality products, keeping users at the center of it all.',
-      members: [],
-      order: []
-    },
-    pm: {
-      roleName: 'Product',
-      description: 'Leading product development to positively impact the community.',
-      members: [],
-      order: ['Product Manager', 'Associate Product Manager']
-    },
-    business: {
-      roleName: 'Business',
-      description: 'Bringing products and events to the community.',
-      members: [],
-      order: ['Product Marketing Manager', 'Internal Business']
-    },
-    developer: {
-      roleName: 'Development',
-      description: 'Building, testing, and optimizing our applications.',
-      members: [],
-      order: ['Technical PM', 'Developer']
-    },
-    tpm: {
-      roleName: '',
-      description: '',
-      members: [],
-      order: []
-    }
   };
 
-  for (const [key, value] of Object.entries(roles)) {
-    value.members = allMembers
-      .filter((member) =>
-        key === 'developer'
-          ? member.role === 'developer' || member.role === 'tpm'
-          : member.role === key
-      )
-      .sort(
-        (mem1, mem2) =>
-          value.order.indexOf(mem1.roleDescription) - value.order.indexOf(mem2.roleDescription)
-      );
-  }
-
-  const roleIcons = [
-    {
-      src: '/icons/full-team',
-      altText: 'Full Team',
-      width: 66,
-      height: 66
-    },
-    {
-      src: '/icons/leads',
-      altText: 'Leads',
-      width: 66,
-      height: 66
-    },
-    {
-      src: '/icons/design',
-      altText: 'Design',
-      width: 66,
-      height: 66
-    },
-    {
-      src: '/icons/product',
-      altText: 'Product',
-      width: 66,
-      height: 66
-    },
-    {
-      src: '/icons/business',
-      altText: 'Business',
-      width: 66,
-      height: 66
-    },
-    {
-      src: '/icons/dev',
-      altText: 'Development',
-      width: 72,
-      height: 66
-    }
-  ];
+  populateMembers(roles, allMembers);
 
   return (
     <div
@@ -145,7 +64,7 @@ const MemberDisplay: React.FC = () => {
                 altText={role.altText}
                 isActive={selectedRole === role.altText}
                 onClick={() => {
-                  setSelectedRole(role.altText);
+                  setSelectedRole(role.altText as DisplayCategory);
                   setSelectedMember(undefined);
                 }}
                 width={role.width}
---
+++ new-dti-website/src/app/utils.ts
@@ -0,0 +1,35 @@
+const populateMembers = (
+  roles: {
+    [key in Role]: {
+      roleName: string;
+      description: string;
+      members: IdolMember[];
+      order: string[];
+    };
+  },
+  allMembers: IdolMember[]
+) => {
+  for (const [key, value] of Object.entries(roles)) {
+    value.members = allMembers
+      .filter((member) =>
+        key === 'developer'
+          ? member.role === 'developer' || member.role === 'tpm'
+          : member.role === key
+      )
+      .sort(
+        (mem1, mem2) =>
+          value.order.indexOf(mem1.roleDescription) - value.order.indexOf(mem2.roleDescription)
+      );
+  }
+};
+
+const getFullRoleFromDescription = (roleDescription: RoleDescription): string => {
+  switch (roleDescription) {
+    case 'Technical PM':
+      return 'Technical Product Manager';
+    default:
+      return roleDescription;
+  }
+};
+
+export { getFullRoleFromDescription, populateMembers };
