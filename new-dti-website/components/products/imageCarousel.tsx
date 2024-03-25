5ce75b423ab52b821991740c3f1117523703ecd1
Oscar Wang <oscarwang22396@gmail.com>
2024-03-25 14:58:56 -0400

    Carousel Component (#595)
    * zoomy carousel
    
    * handle click
    
    * non-jumpy scroll to
    
    * highlight selected
    
    * factored out + dynammic highlighting
    
    * spacing
    
    * semifinal
    
    * fixed spacing
    
    * mobile styling
    
    * changed other files back
    
    * naming again
    
    * remove more extraneous stuff
    
    * lint
    
    * use client
    
    * use client
    
    * removed type imports
    
    * added types import exports back
    
    * bumped prettier
    
    * ran prettier again
    
    * camelCase and cursor pointer
    
    * prettier

---
+++ new-dti-website/components/products/imageCarousel.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import Autoplay from 'embla-carousel-autoplay';
+import { Carousel, CarouselContent, CarouselItem, type CarouselApi } from '../ui/carousel';
+import useScreenSize from '../../src/hooks/useScreenSize';
+
+interface carouselItem {
+  alt: string;
+  path: string;
+}
+
+const ImageCarousel = (props: { items: carouselItem[] }) => {
+  const plugin = React.useRef(Autoplay({ delay: 1300, stopOnInteraction: false }));
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+  const [carouselApi, setCarouselApi] = React.useState<CarouselApi>();
+  const { width } = useScreenSize();
+  const highlightIndex = width < 1024 ? 1 : 3;
+
+  useEffect(() => {
+    if (carouselApi) {
+      carouselApi.on('select', () => {
+        setCurrentSlide(carouselApi.selectedScrollSnap());
+      });
+    }
+  }, [currentSlide, carouselApi]);
+
+  return (
+    <div className="bg-black overflow-x-hidden">
+      <Carousel
+        className="lg:w-[1688px] h-40 md:h-52 lg:h-64 xl:h-80 2xl:h-96 lg:-ml-[105px]"
+        opts={{
+          align: 'start',
+          loop: true
+        }}
+        plugins={[plugin.current]}
+        setApi={setCarouselApi}
+        canClick={true}
+        offset={highlightIndex}
+      >
+        <CarouselContent>
+          {props.items.map((product, index) => (
+            <CarouselItem
+              key={product.alt}
+              className="select-none lg:basis-[15%] basis-1/3 cursor-pointer"
+            >
+              <Image
+                className={`${
+                  index !== (currentSlide + highlightIndex) % props.items.length ? 'opacity-70' : ''
+                } md:w-[211px] md:h-[211px] sm:w-[115px] sm:h-[115px]`}
+                src={product.path}
+                alt={product.alt}
+                width={174}
+                height={174}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
+
+export default ImageCarousel;
