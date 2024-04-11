6d5fbcff409a775a2b0ee097a1035afa2a35f805
Chris Chen <cchen6479@gmail.com>
2024-04-10 23:46:35 -0400

    Fix more nits
    * make populateMembers functional
    * add consts.ts
    * comment canInsertMemberDetails

--- new-dti-website/src/app/utils.ts
+++ new-dti-website/src/app/utils.ts
@@ -1,27 +1,39 @@
 const populateMembers = (
   roles: {
-    [key in Role]: {
+    [key: string]: {
       roleName: string;
       description: string;
       members: IdolMember[];
       order: string[];
+      color: string;
     };
   },
   allMembers: IdolMember[]
-) => {
-  for (const [key, value] of Object.entries(roles)) {
-    value.members = allMembers
-      .filter((member) =>
-        key === 'developer'
-          ? member.role === 'developer' || member.role === 'tpm' || member.role === 'dev-advisor'
-          : member.role === key
-      )
-      .sort(
-        (mem1, mem2) =>
-          value.order.indexOf(mem1.roleDescription) - value.order.indexOf(mem2.roleDescription)
-      );
-  }
-};
+): {
+  [key: string]: {
+    roleName: string;
+    description: string;
+    members: IdolMember[];
+    order: string[];
+    color: string;
+  };
+} =>
+  Object.fromEntries(
+    Object.entries(roles).map((role) => {
+      const [key, value] = role;
+      const sortedMembers = allMembers
+        .filter((member) =>
+          key === 'developer'
+            ? member.role === 'developer' || member.role === 'tpm' || member.role === 'dev-advisor'
+            : member.role === key
+        )
+        .sort(
+          (mem1, mem2) =>
+            value.order.indexOf(mem1.roleDescription) - value.order.indexOf(mem2.roleDescription)
+        );
+      return [key, { ...value, members: sortedMembers }];
+    })
+  );
 
 const getFullRoleFromDescription = (roleDescription: RoleDescription): string => {
   switch (roleDescription) {
