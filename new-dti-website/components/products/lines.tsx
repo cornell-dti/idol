847c85725e7eed46813e034dba9020cc5b127293
Oscar Wang <oscarwang22396@gmail.com>
2024-04-15 10:52:51 -0400

    connector done

--- new-dti-website/components/products/lines.tsx
+++ new-dti-website/components/products/lines.tsx
@@ -21,15 +21,12 @@ const Connector: React.FC<ConnectorProps> = (props: ConnectorProps) => {
   const connectorRef = useRef<HTMLDivElement>(null);
   const { scrollY } = useScrollPosition();
 
-  const handleClick = () => {
-    if (connectorRef.current != null) {
-      console.log(connectorRef.current.offsetTop);
-    }
-  };
-
   const getOffset = () => {
     if (connectorRef.current != null) {
-      return connectorRef.current.offsetTop;
+      return Math.min(
+        props.height,
+        Math.max(0, scrollY - connectorRef.current.offsetTop + props.height / 2)
+      );
     }
     return 0;
   };
@@ -37,73 +34,58 @@ const Connector: React.FC<ConnectorProps> = (props: ConnectorProps) => {
   return (
     <div
       ref={connectorRef}
-      onClick={handleClick}
-      className="flex flex-row flex-shrink w-full justify-center px-10"
+      className="lg:flex flex-row relative flex-shrink w-full justify-center px-10 sm:hidden"
     >
-      {props.orientation === 'left' ? (
-        <svg
-          width={props.width}
-          height={props.height}
-          viewBox={`0 0 ${props.width} ${props.height}`}
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d={`M${props.strokeWidth / 2} 0 V${props.height / 2 - props.strokeWidth} H${
-              props.width - props.strokeWidth / 2
-            } V${props.height}`}
-            stroke="#FEFEFE"
-            strokeOpacity="0.2"
-            strokeWidth="6"
-            strokeLinecap="square"
-          />
-          <path
-            d={`M${props.strokeWidth / 2} 0 V${props.height / 2 - props.strokeWidth} H${
-              props.width - props.strokeWidth / 2
-            } V${props.height}`}
-            stroke="#FEFEFE"
-            strokeWidth={props.strokeWidth}
-            strokeLinecap="square"
-            strokeDasharray={`${props.width + props.height} ${props.width + props.height}`}
-            strokeDashoffset={
-              props.width +
-              props.height -
-              (scrollY + window.innerHeight / 2 + 400 - getOffset() > 0
-                ? scrollY + window.innerHeight / 2 + 400 - getOffset()
-                : 0)
-            }
-          />
-        </svg>
-      ) : (
-        <svg
-          width={props.width}
-          height={props.height}
-          viewBox={`0 0 ${props.width} ${props.height}`}
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d={`M${props.width - props.strokeWidth / 2} 0 V${
-              props.height / 2 - props.strokeWidth / 2
-            } H${props.strokeWidth / 2} V${props.height}`}
-            stroke="#FEFEFE"
-            strokeOpacity="0.2"
-            strokeWidth={props.strokeWidth}
-            strokeLinecap="square"
-          />
-          <path
-            d={`M${props.width - props.strokeWidth / 2} 0 V${
-              props.height / 2 - props.strokeWidth / 2
-            } H${props.strokeWidth / 2} V${props.height}`}
-            stroke="#FEFEFE"
-            strokeWidth={props.strokeWidth}
-            strokeLinecap="square"
-            strokeDasharray={`${props.width + props.height} ${props.width + props.height}`}
-            strokeDashoffset={props.width + props.height - scrollY}
-          />
-        </svg>
-      )}
-      ;
+      <svg
+        width={props.width}
+        height={props.height}
+        viewBox={`0 0 ${props.width} ${props.height}`}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d={
+            props.orientation === 'left'
+              ? `M${props.strokeWidth / 2} 0 V${props.height / 2 - props.strokeWidth} H${
+                  props.width - props.strokeWidth / 2
+                } V${props.height}`
+              : `M${props.width - props.strokeWidth / 2} 0 V${
+                  props.height / 2 - props.strokeWidth / 2
+                } H${props.strokeWidth / 2} V${props.height}`
+          }
+          stroke="#FEFEFE"
+          strokeOpacity="0.2"
+          strokeWidth="6"
+          strokeLinecap="square"
+        />
+      </svg>
+      <svg
+        className={`absolute top-0 left-50% object-cover h-[${scrollY}px]`}
+        width={props.width}
+        height={props.height}
+        viewBox={`0 0 ${props.width} ${props.height}`}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d={
+            props.orientation === 'left'
+              ? `M${props.strokeWidth / 2} 0 V${props.height / 2 - props.strokeWidth} H${
+                  props.width - props.strokeWidth / 2
+                } V${props.height}`
+              : `M${props.width - props.strokeWidth / 2} 0 V${
+                  props.height / 2 - props.strokeWidth / 2
+                } H${props.strokeWidth / 2} V${props.height}`
+          }
+          stroke="#FEFEFE"
+          strokeWidth={props.strokeWidth}
+          strokeLinecap="square"
+          strokeDasharray={`${props.width + props.height} ${props.width + props.height}`}
+          strokeDashoffset={
+            props.width + props.height - (getOffset() / props.height) * (props.width + props.height)
+          }
+        />
+      </svg>
     </div>
   );
 };
--- new-dti-website/src/app/products/page.tsx
+++ new-dti-website/src/app/products/page.tsx
@@ -1,14 +1,10 @@
 import ImageCarousel from '../../../components/products/imageCarousel';
-import { Connector, NodeLine } from '../../../components/products/lines';
-import { products } from '../../../components/products/productIcons.json';
+import { Connector } from '../../../components/products/lines';
+import products from '../../../components/products/productIcons.json';
 
 const Page = () => (
   <div>
-    <ImageCarousel items={products} />
-    <ImageCarousel items={products} />
-    <ImageCarousel items={products} />
-    <ImageCarousel items={products} />
-    <ImageCarousel items={products} />
+    <ImageCarousel items={products.products} />
 
     <Connector orientation="left" width={780} height={470} strokeWidth={6} />
     <Connector orientation="right" width={780} height={470} strokeWidth={6} />
