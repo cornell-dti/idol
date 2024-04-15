166d5ecc11772cd30a1a1370dbeb6497b01ca392
Chris Chen <cchen6479@gmail.com>
2024-04-14 23:41:58 -0400

    Add team page hero and about

---
+++ new-dti-website/src/app/team/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import TeamHero from '../../../components/team/TeamHero';
+import TeamAbout from '../../../components/team/TeamAbout';
+
+const TeamPage = () => (
+  <>
+    <TeamHero />
+    <TeamAbout />
+  </>
+);
+
+export default TeamPage;
