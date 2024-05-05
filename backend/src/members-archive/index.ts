2182fa39bdbd30b1eacaa0695f94cf553cb639b9
Chris Chen <cchen6479@gmail.com>
2024-05-05 17:02:25 -0400

    add member archive script

--- backend/src/members-archive/index.ts
+++ backend/src/members-archive/index.ts
@@ -2,6 +2,8 @@ import sp21 from './sp21.json';
 import fa21 from './fa21.json';
 import sp23 from './sp23.json';
 import fa23 from './fa23.json';
+import { allApprovedMembers } from '../API/memberAPI';
+import { allMemberImages } from '../API/imageAPI';
 
 export const archivedMembersBySemesters: Readonly<Record<string, readonly IdolMember[]>> = {
   'Spring 2021': sp21.members as unknown as readonly IdolMember[],
@@ -19,3 +21,38 @@ export const archivedMembersByEmail: Readonly<Record<string, IdolMember>> = (() 
   });
   return map;
 })();
+
+const generateArchive = async (): Promise<{ [key: string]: NovaMember[] }> => {
+  const allMembers: Set<string> = new Set();
+  const archive: { [key: string]: NovaMember[] } = { current: [], alumni: [], inactive: [] };
+
+  const currentMembers = await allApprovedMembers();
+  const images = await allMemberImages();
+
+  const addToArchive = (key: string, member: IdolMember) => {
+    const image = images.find((image) => image.fileName.split('.')[0] === member.netid);
+    archive[key].push({
+      ...member,
+      image: image ? image.url : null,
+      coffeeChatLink: `mailto:${member.email}`
+    });
+  };
+
+  currentMembers.forEach((member) => {
+    allMembers.add(member.netid);
+    addToArchive('current', member);
+  });
+
+  Object.values(archivedMembersBySemesters).forEach((semester) => {
+    semester.forEach((member) => {
+      if (!allMembers.has(member.netid)) {
+        allMembers.add(member.netid);
+        Date.parse(member.graduation) < Date.now()
+          ? addToArchive('alumni', member)
+          : addToArchive('inactive', member);
+      }
+    });
+  });
+
+  return archive;
+};
