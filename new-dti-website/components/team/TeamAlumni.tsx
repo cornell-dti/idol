505df18958cdfb90ccf8228340911e92bbc1d9f4
Chris Chen <cchen6479@gmail.com>
2024-05-16 20:01:34 -0400

    factor out constants

--- new-dti-website/components/team/TeamAlumni.tsx
+++ new-dti-website/components/team/TeamAlumni.tsx
@@ -3,7 +3,8 @@ import companies from './data/companies.json';
 import useScreenSize from '../../src/hooks/useScreenSize';
 import { TABLET_BREAKPOINT } from '../../src/consts';
 import { MemberCard } from './MemberGroup';
-import { members } from '../../../backend/src/members-archive/fa21.json';
+import alumniMembers from '../../../backend/src/members-archive/fa21.json';
+import config from '../../config.json';
 
 const AlumniDisplay = () => (
   <div className="flex justify-center bg-[#F6F6F6] pb-36">
@@ -13,7 +14,7 @@ const AlumniDisplay = () => (
         className="grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-2 md:gap-10 
           xs:gap-x-1.5 xs:gap-y-5 md:mt-10 xs:mt-5"
       >
-        {members.map((member, index) =>
+        {alumniMembers.members.map((member, index) =>
           index <= 5 ? (
             <a href={member.linkedin ?? undefined}>
               <MemberCard
@@ -59,8 +60,7 @@ const BeyondDTI = () => {
 };
 
 const TeamAlumni = () => {
-  const semester = 'Spring 2024';
-
+  const isApplicationOpen = Date.parse(config.applicationDeadline) >= Date.now();
   return (
     <div className="flex flex-col">
       <AlumniDisplay />
@@ -76,12 +76,17 @@ const TeamAlumni = () => {
           />
           <div className="flex flex-col gap-[20px] items-start">
             <h1 className="font-semibold lg:text-[32px] md:text-2xl">Want to join the family?</h1>
-            <p className="lg:text-[22px] md:text-lg">Applications for {semester} are open now!</p>
-            <button className="rounded-xl py-3 px-[14px] bg-[#A52424] text-white font-bold hover:bg-white hover:text-[#A52424]">
-              <a href={'/apply'} target="_blank" rel="noopener noreferrer">
-                Apply here
-              </a>
-            </button>
+            <p className="lg:text-[22px] md:text-lg">
+              Applications for {config.semester} are{' '}
+              {isApplicationOpen ? 'now open!' : 'now closed.'}
+            </p>
+            {isApplicationOpen && (
+              <button className="rounded-xl py-3 px-[14px] bg-[#A52424] text-white font-bold hover:bg-white hover:text-[#A52424]">
+                <a href={'/apply'} target="_blank" rel="noopener noreferrer">
+                  Apply here
+                </a>
+              </button>
+            )}
           </div>
         </div>
       </div>
