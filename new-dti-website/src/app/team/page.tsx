c149addf7ca0302e37b6b858a232bfe6e22ef78d
Chris Chen <cchen6479@gmail.com>
2024-08-27 15:16:09 -0400

    fix page.tsx

--- new-dti-website/src/app/team/page.tsx
+++ new-dti-website/src/app/team/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import TeamHero from '../../../components/team/TeamHero';
 import MemberDisplay from '../../../components/team/MemberDisplay';
 import TeamAlumni from '../../../components/team/TeamAlumni';
+import TeamAbout from '../../../components/team/TeamAbout';
 
 const TeamPage = () => (
   <>
+    <TeamHero />
+    <TeamAbout />
     <MemberDisplay />
     <TeamAlumni />;
   </>
