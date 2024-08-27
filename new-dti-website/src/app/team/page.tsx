482361412ab996f00d7ec345030a448e79bd8f6d
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2024-08-27 15:12:24 -0400

    Add team page hero and about (#603)
    * Add team page hero and about
    
    * Add team page hero and about
    
    * adjust carousel to agree with figma
    
    * add and update images
    
    * prettier and use constants
    
    * add leads to carousel and carousel frame
    
    * fix modal show bug and blob overshadowing navbar
    
    * refactoring and making code functional
    
    * make center carousel image draggable

--- new-dti-website/src/app/team/page.tsx
+++ new-dti-website/src/app/team/page.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import TeamHero from '../../../components/team/TeamHero';
+import TeamAbout from '../../../components/team/TeamAbout';
+import MemberDisplay from '../../../components/team/MemberDisplay';
 
-const TeamPage = () => <TeamHero />;
+const TeamPage = () => (
+  <>
+    <TeamHero />
+    <TeamAbout />
+    <MemberDisplay />
+  </>
+);
 
 export default TeamPage;
