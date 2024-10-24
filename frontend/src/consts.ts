632a4c7b552caf1140b04f008a2318bdd7ed1ac1
Patricia Huang <ph444@cornell.edu>
2024-10-24 17:40:51 -0400

    Fix client side error

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -4,9 +4,3 @@ export const REQUIRED_LEAD_TEC_CREDITS = 6;
 export const ALL_STATUS: Status[] = ['approved', 'pending', 'rejected'];
 export const INITIATIVE_EVENTS = false;
 export const ENABLE_COFFEE_CHAT = true;
-export const COFFEE_CHAT_BINGO_BOARD = [
-  ['an alumni', 'courseplan member', 'a pm (not your team)', 'business member'],
-  ['is/was a TA', 'major or minor that is not cs or infosci', 'idol member', 'a newbie'],
-  ['from a different college', 'curaise member', 'cornellgo member', 'a tpm (not your team)'],
-  ['carriage member', 'qmi member', 'a lead (not your role)', 'cuapts member']
-];
