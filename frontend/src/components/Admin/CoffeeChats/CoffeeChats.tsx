35849af5302740ef583f1ee7d371626bf71c3e09
Patricia Huang <ph444@cornell.edu>
2024-10-25 22:02:58 -0400

    Show pending submissions count

--- frontend/src/components/Admin/CoffeeChats/CoffeeChats.tsx
+++ frontend/src/components/Admin/CoffeeChats/CoffeeChats.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
 import { createHash } from 'crypto';
 import styles from './CoffeeChats.module.css';
@@ -7,6 +7,22 @@ import CoffeeChatAPI from '../../../API/CoffeeChatAPI';
 const CoffeeChats: React.FC = () => {
   const [bingoBoard, setBingoBoard] = useState<string[][]>([[]]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [pendingChats, setPendingChats] = useState<CoffeeChat[]>([]);
+  const chatCount = useMemo(
+    () =>
+      pendingChats.reduce((acc, chat) => {
+        acc.set(chat.category, (acc.get(chat.category) ?? 0) + 1);
+        return acc;
+      }, new Map<string, number>()),
+    [pendingChats]
+  );
+
+  useEffect(() => {
+    CoffeeChatAPI.getAllCoffeeChats().then((coffeeChats) => {
+      setPendingChats(coffeeChats.filter((chat) => chat.status === 'pending'));
+    });
+    CoffeeChatAPI.getCoffeeChatBingoBoard().then((board) => setBingoBoard(board));
+  }, []);
 
   const hashString = (category: string) => createHash('sha256').update(category).digest('hex');
 
@@ -34,7 +50,12 @@ const CoffeeChats: React.FC = () => {
                     query: { category }
                   }}
                 >
-                  <div className={styles.bingo_text}>{category}</div>
+                  <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div className={styles.bingo_text}>{category}</div>
+                    <div className={styles.pending_text}>
+                      {chatCount.get(category) ? `${chatCount.get(category)} pending` : ''}{' '}
+                    </div>
+                  </div>
                 </Link>
               </div>
             </div>
