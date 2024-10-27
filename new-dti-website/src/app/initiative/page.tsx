09680a6db3f9b5446059254c4bdbe77db4829d1a
Chris Chen <cchen6479@gmail.com>
2024-10-27 13:14:32 -0400

    add call to action

--- new-dti-website/src/app/initiative/page.tsx
+++ new-dti-website/src/app/initiative/page.tsx
@@ -1,12 +1,16 @@
-'use client'
+'use client';
 
-import InitiativeDisplay from "../../../components/initiative/InitiativeDisplay";
+import RedBlob from '../../../components/blob';
+import InitiativeDisplay from '../../../components/initiative/InitiativeDisplay';
 
 const InitiativeHero = () => (
-  <div className="bg-black text-[#FEFEFE] min-h-[calc(100vh-136px)] h-full flex items-center">
+  <div
+    className="bg-black text-[#FEFEFE] min-h-[calc(100vh-136px)] h-full flex items-center
+    relative overflow-hidden"
+  >
     <div
       className="flex lg:flex-row xs:flex-col gap-x-[60px] lg:ml-[90px] lg:mr-[169px]
-      xs:mx-6 md:mx-[65px]"
+      xs:mx-6 md:mx-[65px] relative z-20"
     >
       <h1
         className="flex items-center md:text-[100px] xs:text-[48px] md:leading-[120px] 
@@ -30,13 +34,32 @@ const InitiativeHero = () => (
         </p>
       </div>
     </div>
+    <div className="relative">
+      <RedBlob intensity={0.5} className="right-[-300px] bottom-[-600px]" />
+    </div>
   </div>
 );
 
 const InitiativePage = () => (
-  <div className="bg-white flex flex-col overflow-hidden">
+  <div className="bg-white flex flex-col">
     <InitiativeHero />
     <InitiativeDisplay />
+    <div className="flex justify-center bg-[#F5F5F5] lg:py-32 xs:py-16 lg:px-20 xs:px-7">
+      <div className="max-w-7xl w-full flex flex-col lg:gap-6 xs:gap-3">
+        <h3 className="font-semibold md:text-[32px] xs:text-[22px] leading-10">
+          Want us to be a part of your next event?
+        </h3>
+        <p className="md:text-[22px] md:leading-[26px] xs:text-[12px] xs:leading-[14px]">
+          Feel free to coordinate with us over email, coffee, lunch-we're excited to work with you.
+        </p>
+        <button
+          className="rounded-xl py-3 px-[20px] bg-[#A52424] text-white 
+          font-bold hover:bg-[#D63D3D] w-fit"
+        >
+          <a href="mailto:hello@cornelldti.org">Get in touch</a>
+        </button>
+      </div>
+    </div>
   </div>
 );
 
