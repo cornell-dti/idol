22bd193d84a3d4e2a6b29bee4172d18113ff90f0
Chris Chen <cchen6479@gmail.com>
2024-11-03 15:03:08 -0500

    stretch image to width of column

--- new-dti-website/components/initiative/InitiativeDisplay.tsx
+++ new-dti-website/components/initiative/InitiativeDisplay.tsx
@@ -38,14 +38,13 @@ const Initiative = ({
         </div>
         <p className="font-semibold text-[22px] leading-[26px]">{subtitle}</p>
         <div className="flex justify-center">
-          <div
-            className="flex justify-center max-w-fit overflow-hidden rounded-xl"
+          <Image
+            {...image}
+            className="h-[260px] lg:w-full object-cover rounded-xl"
             style={{
               boxShadow: `${width >= LAPTOP_BREAKPOINT ? '0px 4px 10px 3px #00000036' : ''}`
             }}
-          >
-            <Image {...image} className="h-[260px] max-w-none rounded-xl" />
-          </div>
+          />
         </div>
       </div>
       <p
