02d2a076c93502a72eaf7530d7394880f36d836d
JasonMun7 <jasonmun020104@gmail.com>
2024-11-12 17:52:13 -0500

    Added z-0 back to blob to make it more consistent

--- new-dti-website/components/blob.tsx
+++ new-dti-website/components/blob.tsx
@@ -18,7 +18,7 @@
  */
 const RedBlob: React.FC<{ className?: string; intensity: number }> = ({ className, intensity }) => (
   <div
-    className={`absolute h-[600px] w-[600px] rounded-full ${className}`}
+    className={`absolute h-[600px] w-[600px] rounded-full z-0 ${className}`}
     style={{
       backgroundImage: `radial-gradient(rgba(192, 12, 12, ${intensity}) 5%, transparent 75%)`
     }}
