9248371eb9d324d0c87b1db6d0d102a9cf62ad0e
Andrew Chen <andrew032012@gmail.com>
2024-11-14 04:02:11 -0500

    IDOL/New Website: Add Pull from IDOL Images Job

---
+++ backend/scripts/create-idol-images-pr.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+import { Octokit } from '@octokit/rest';
+import { spawnSync } from 'child_process';
+
+function runCommand(program: string, ...programArguments: readonly string[]) {
+  const programArgumentsQuoted = programArguments
+    .map((it) => (it.includes(' ') ? `"${it}"` : it))
+    .join(' ');
+  console.log(`> ${program} ${programArgumentsQuoted}`);
+  return spawnSync(program, programArguments, { stdio: 'inherit' });
+}
+
+const main = async () => {
+  console.log('Creating PR...');
+
+  // Create commit
+  runCommand('git', 'config', '--global', 'user.name', 'dti-github-bot');
+  runCommand('git', 'config', '--global', 'user.email', 'admin@cornelldti.org');
+  const gitBranch = 'dti-github-bot/pull-from-idol-images';
+  const commitMessage = '[bot] Automatically pull images from Google Cloud Storage';
+  runCommand('git', 'add', '.');
+  runCommand('git', 'fetch', '--all');
+  runCommand('git', 'checkout', 'main');
+  runCommand('git', 'checkout', '-b', gitBranch);
+  if (runCommand('git', 'commit', '-m', commitMessage).status === 0) {
+    if (runCommand('git', 'push', '-f', 'origin', gitBranch).status !== 0) {
+      runCommand('git', 'push', '-f', '--set-upstream', 'origin', gitBranch);
+    }
+  }
+
+  // Create PR
+  const octokit = new Octokit({
+    auth: `token ${process.env.BOT_TOKEN}`,
+    userAgent: 'cornell-dti/big-diff-warning'
+  });
+  const prBody = `
+  ## Summary
+
+  This is a PR auto-generated from \`yarn workspace backend run pull-from-idol-images\`. and
+  \`yarn workspace backend run create-idol-images-pr\`.
+  It updates the new-dti-website with latest images from Google Cloud Storage.
+  Please verify that the images look fine on the website and that the image sizes aren't too large.`;
+  const existingPR = (
+    await octokit.pulls.list({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      state: 'open'
+    })
+  ).data.find((pr) => pr.title === commitMessage);
+  if (existingPR == null) {
+    await octokit.pulls.create({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      title: commitMessage,
+      body: prBody,
+      base: 'main',
+      head: gitBranch
+    });
+  } else {
+    await octokit.pulls.update({
+      owner: 'cornell-dti',
+      repo: 'idol',
+      pull_number: existingPR.number,
+      body: prBody
+    });
+  }
+};
+
+main();
---
+++ backend/scripts/pull-from-idol-images.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+import { allMemberImages } from '../src/API/imageAPI';
+
+const main = async () => {
+  console.log('Downloading all member images...');
+  const processedImages = await allMemberImages();
+
+  processedImages.forEach((image) => {
+    const outputFolder = 'temp';
+
+    const fullPath = join(outputFolder, image.fileName);
+
+    downloadImage(image.url, fullPath)
+      .then(() => console.log('Download complete'))
+      .catch((error) => console.error('Download failed:', error));
+  });
+};
+
+async function downloadImage(url: string, outputPath: string): Promise<void> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image: ${response.statusText}`);
+    }
+
+    const imageBuffer = await response.arrayBuffer();
+    await writeFile(outputPath, Buffer.from(imageBuffer));
+
+    console.log(`Image downloaded successfully to: ${outputPath}`);
+  } catch (error) {
+    console.error('Error downloading image:', error);
+    throw error;
+  }
+}
+
+main();
