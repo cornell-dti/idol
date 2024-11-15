6b403031ae9b36e4720fb4e969747eaacce299c1
Chris Chen <cchen6479@gmail.com>
2024-11-14 21:26:58 -0500

    other small team page fixes

--- new-dti-website/src/app/team/page.tsx
+++ new-dti-website/src/app/team/page.tsx
@@ -4,14 +4,19 @@ import TeamHero from '../../../components/team/TeamHero';
 import TeamAbout from '../../../components/team/TeamAbout';
 import MemberDisplay from '../../../components/team/MemberDisplay';
 import TeamAlumni from '../../../components/team/TeamAlumni';
+import useThemeContext from '../../hooks/useThemeContext';
 
-const TeamPage = () => (
-  <>
-    <TeamHero />
-    <TeamAbout />
-    <MemberDisplay />
-    <TeamAlumni />
-  </>
-);
+const TeamPage = () => {
+  const { theme } = useThemeContext();
+  theme?.setFooterTheme('light');
+  return (
+    <>
+      <TeamHero />
+      <TeamAbout />
+      <MemberDisplay />
+      <TeamAlumni />
+    </>
+  );
+};
 
 export default TeamPage;
