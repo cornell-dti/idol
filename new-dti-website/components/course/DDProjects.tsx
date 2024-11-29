5141ea224845da1c858c5494707e59d9e1a5e455
JasonMun7 <jason.mun484@gmail.com>
2024-11-28 20:58:46 -0700

    Finalized all changes for the courses page

--- new-dti-website/components/course/DDProjects.tsx
+++ new-dti-website/components/course/DDProjects.tsx
@@ -37,10 +37,10 @@ export default function DDProjects({ title, description, imageSrc }: DDProjectsP
       onClick={toggleCard}
     >
       <div className="flex justify-between items-center">
-        <h3 className={`md:text-3xl text-xl font-bold ${isOpen ? 'text-white' : 'text-black'}`}>
+        <h3 className={`md:text-2xl text-lg font-semibold ${isOpen ? 'text-white' : 'text-black'}`}>
           {title}
         </h3>
-        <p className={`md:text-4xl text-2xl font-thin ${isOpen ? 'text-white' : 'text-gray-700'}`}>
+        <p className={`md:text-3xl text-xl font-thin ${isOpen ? 'text-white' : 'text-gray-700'}`}>
           {isOpen ? '−' : '+'}
         </p>
       </div>
@@ -52,7 +52,7 @@ export default function DDProjects({ title, description, imageSrc }: DDProjectsP
         }`}
       >
         <div className="mt-4">
-          <p className="text-white">{description}</p>
+          <p className="text-white text-lg">{description}</p>
           <img src={imageSrc} alt={title} className="mt-4 w-full h-48 object-cover rounded-lg" />
         </div>
       </div>
