f7afc91f8a91447c551041f23cfa12c6f2b36b6d
jujulcrane <jujulcrane@gmail.com>
2024-12-02 20:26:03 -0500

    improved bingo layout

--- frontend/src/components/Admin/CoffeeChats/CoffeeChats.tsx
+++ frontend/src/components/Admin/CoffeeChats/CoffeeChats.tsx
@@ -18,9 +18,9 @@ const CoffeeChats: React.FC = () => {
   const [isChatLoading, setIsChatLoading] = useState<boolean>(true);
   const [displayMembers, setDisplayMembers] = useState<boolean>(false);
   const [selectedMember, setSelectedMember] = useState<IdolMember | null>(null);
-  
+
   const allMembers = useMembers();
-  
+
   const chatCount = useMemo(
     () =>
       pendingChats.reduce((acc, chat) => {
@@ -42,104 +42,104 @@ const CoffeeChats: React.FC = () => {
     });
   }, [isLoading]);
 
-    const handleMemberClick = (member: IdolMember) => {
-      setIsChatLoading(true);
-      setSelectedMember(member);
-  
-      CoffeeChatAPI.getCoffeeChatsByUser(member).then((coffeeChats) => {
-        setSpecificApprovedChats(coffeeChats.filter((chat) => chat.status === 'approved'));
-        setSpecificPendingChats(coffeeChats.filter((chat) => chat.status === 'pending'));
-        setSpecificRejectedChats(coffeeChats.filter((chat) => chat.status === 'rejected'));
-        setIsChatLoading(false);
-      });
+  const handleMemberClick = (member: IdolMember) => {
+    setIsChatLoading(true);
+    setSelectedMember(member);
 
-    CoffeeChatAPI.getCoffeeChatBingoBoard().then((board) => 
+    CoffeeChatAPI.getCoffeeChatsByUser(member).then((coffeeChats) => {
+      setSpecificApprovedChats(coffeeChats.filter((chat) => chat.status === 'approved'));
+      setSpecificPendingChats(coffeeChats.filter((chat) => chat.status === 'pending'));
+      setSpecificRejectedChats(coffeeChats.filter((chat) => chat.status === 'rejected'));
+      setIsChatLoading(false);
+    });
+
+    CoffeeChatAPI.getCoffeeChatBingoBoard().then((board) =>
       setSpecificBingoBoard(board)
-  );
-};
+    );
+  };
 
   return (
-    <div className={styles.flexContainer}> 
+    <div className={styles.flexContainer}>
       <div className="main-content">
-      {selectedMember ? (
-            <div className={[styles.formWrapper, styles.wrapper].join(' ')}>
-              <h2>
-                Bingo Board for {selectedMember.firstName} {selectedMember.lastName}
-              </h2>
+        {selectedMember ? (
+          <div className={[styles.formWrapper, styles.wrapper].join(' ')}>
+            <h2>
+              Bingo Board for {selectedMember.firstName} {selectedMember.lastName}
+            </h2>
             <CoffeeChatsBingoBoard
-            approvedChats={specificApprovedChats}
-            pendingChats={specificPendingChats}
-            rejectedChats={specificRejectedChats}
-            isChatLoading={isChatLoading}
-            bingoBoard={specificBingoBoard}
-            onCellClick={() => {}}
+              approvedChats={specificApprovedChats}
+              pendingChats={specificPendingChats}
+              rejectedChats={specificRejectedChats}
+              isChatLoading={isChatLoading}
+              bingoBoard={specificBingoBoard}
+              onCellClick={() => { }}
             />
-              </div>
-          ) : (
-            <div>
-      <div className={[styles.formWrapper, styles.wrapper].join(' ')}>
-        <h1>Review Coffee Chats</h1>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.bingo_board}>
-          {bingoBoard.flat().map((category, index) => (
-            <div key={index}>
-              <Link
-                key={category}
-                href={{
-                  pathname: `/admin/coffee-chat-details/${hashString(category)}`,
-                  query: { category }
-                }}
-              >
-                <div className={styles.bingo_cell}>
-                  <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <div className={styles.bingo_text}>{category}</div>
-                    <div className={styles.pending_text}>
-                      {chatCount.get(category) ? `${chatCount.get(category)} pending` : ''}{' '}
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div> 
-      </div>
-      )}
-      <div className={styles.buttonContainer}>
-        <Link href="/admin/coffee-chats/dashboard">
-          <Button>View Coffee Chats Dashboard</Button>
-        </Link>
-        </div>
-        <div className="dropdown-column">
-        <div className={styles.dropdownContainer}>
-          <button 
-          className={styles.dropdownButton}
-          onClick={() => {
-            if (selectedMember) {
-              setSelectedMember(null);
-            }
-            setDisplayMembers(!displayMembers)
-          }}>
-            View Member Bingo Board
-            </button>
-          {displayMembers && ( 
-          <ul className={styles.dropdownMenu}> 
-            {allMembers.map( (member, index) => (
-              <li key={index}>
-            <button 
-            className={styles.memberButton} 
-            onClick={() => handleMemberClick(member)}>
-              {member.firstName} {member.lastName}
-              </button> 
-              </li>
-            ))}
-          </ul> 
-          )}
           </div>
+        ) : (
+          <div>
+            <div className={[styles.formWrapper, styles.wrapper].join(' ')}>
+              <h1>Review Coffee Chats</h1>
+            </div>
+            <div className={styles.wrapper}>
+              <div className={styles.bingo_board}>
+                {bingoBoard.flat().map((category, index) => (
+                  <div key={index}>
+                    <Link
+                      key={category}
+                      href={{
+                        pathname: `/admin/coffee-chat-details/${hashString(category)}`,
+                        query: { category }
+                      }}
+                    >
+                      <div className={styles.bingo_cell}>
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                          <div className={styles.bingo_text}>{category}</div>
+                          <div className={styles.pending_text}>
+                            {chatCount.get(category) ? `${chatCount.get(category)} pending` : ''}{' '}
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        <div className={styles.buttonContainer}>
+          <Link href="/admin/coffee-chats/dashboard">
+            <Button>View Coffee Chats Dashboard</Button>
+          </Link>
         </div>
       </div>
+      <div className="dropdown-column">
+        <div className={styles.dropdownContainer}>
+          <button
+            className={styles.dropdownButton}
+            onClick={() => {
+              if (selectedMember) {
+                setSelectedMember(null);
+              }
+              setDisplayMembers(!displayMembers)
+            }}>
+            View Member Bingo Board
+          </button>
+          {displayMembers && (
+            <ul className={styles.dropdownMenu}>
+              {allMembers.map((member, index) => (
+                <li key={index}>
+                  <button
+                    className={styles.memberButton}
+                    onClick={() => handleMemberClick(member)}>
+                    {member.firstName} {member.lastName}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
+    </div>
   );
 };
 
