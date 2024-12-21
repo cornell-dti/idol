28abc88fc76ac857db82344ebd62e69990842367
Chris Chen <cchen6479@gmail.com>
2024-12-20 19:40:35 -0500

    faq and coffee chat section fixes

--- new-dti-website/src/app/apply/page.tsx
+++ new-dti-website/src/app/apply/page.tsx
@@ -76,10 +76,10 @@ const ApplyCoffeeChat = () => (
         Feel free to chat with any of us over email, coffee, lunch-we're happy to help!
       </p>
       <div className="flex md:flex-row xs:flex-col gap-3">
-        <a href={config.coffeeChatLink} className="primary-button">
+        <a href={config.coffeeChatLink} className="primary-button xs:w-full md:w-fit">
           Coffee chat with us
         </a>
-        <a href={config.coffeeChatFormLink} className="secondary-button secondary-button--red">
+        <a href={config.coffeeChatFormLink} className="secondary-button secondary-button--red xs:w-full md:w-fit">
           Don't know who to chat with?
         </a>
       </div>
