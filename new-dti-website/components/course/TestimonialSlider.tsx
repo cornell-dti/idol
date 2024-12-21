1c4867753e78cb56b41df91d9d61fa2c3f05d68a
JasonMun7 <jason.mun484@gmail.com>
2024-12-21 11:07:47 -0700

    Final Changes to courses page without typography

--- new-dti-website/components/course/TestimonialSlider.tsx
+++ new-dti-website/components/course/TestimonialSlider.tsx
@@ -10,61 +10,35 @@ export default function TestimonialSlider({
   testimonials,
   className = ''
 }: TestimonialSliderProps) {
-  const sliderRef = useRef<HTMLDivElement | null>(null);
-  const [isScrolling, setIsScrolling] = useState(false);
-  const [scrollAtEnd, setScrollAtEnd] = useState(false);
-
-  useEffect(() => {
-    const slider = sliderRef.current;
-
-    // Return early if there are no testimonials or if we've reached the end
-    if (!slider || testimonials.length === 0 || scrollAtEnd || isScrolling) {
-      return undefined; // Return `undefined` explicitly to satisfy consistent-return rule
-    }
-
-    const scrollInterval = setInterval(() => {
-      const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-
-      if (slider.scrollLeft < maxScrollLeft) {
-        slider.scrollLeft += 1; // Adjust the scroll speed as needed
-      } else {
-        setScrollAtEnd(true); // Stop scrolling when the end is reached
-        // slider.scrollLeft = 0 if we want to start from the begnning :)
-      }
-    }, 40); // Adjust the interval as needed for smoother/faster scrolling
-
-    // Return the cleanup function
-    return () => {
-      clearInterval(scrollInterval);
-    };
-  }, [testimonials, isScrolling, scrollAtEnd]);
-
-  const handleMouseEnter = () => {
-    setIsScrolling(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsScrolling(false);
-  };
-
   return (
-    <div
-      className={`overflow-x-auto pt-14 ${className}`}
-      ref={sliderRef}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="flex gap-x-12 flex-nowrap">
+    <div className={`w-full inline-flex flex-nowrap ${className}`}>
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
         {testimonials.map((testimonial, index) => (
-          <TestimonialCard
-            key={index}
-            profileImage={testimonial.profileImage}
-            description={testimonial.description}
-            name={testimonial.name}
-            semesterTaken={testimonial.semesterTaken}
-          />
+          <li key={index}>
+            <TestimonialCard
+              profileImage={testimonial.profileImage}
+              description={testimonial.description}
+              name={testimonial.name}
+              semesterTaken={testimonial.semesterTaken}
+            />
+          </li>
         ))}
-      </div>
+      </ul>
+      <ul
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        aria-hidden="true"
+      >
+        {testimonials.map((testimonial, index) => (
+          <li key={index}>
+            <TestimonialCard
+              profileImage={testimonial.profileImage}
+              description={testimonial.description}
+              name={testimonial.name}
+              semesterTaken={testimonial.semesterTaken}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
