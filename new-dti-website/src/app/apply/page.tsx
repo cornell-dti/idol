3eeaff70e4138a816eb413888da6e5fe39cbbd2c
Chris Chen <cchen6479@gmail.com>
2024-12-22 18:34:34 -0500

    add button text centering

--- new-dti-website/src/app/apply/page.tsx
+++ new-dti-website/src/app/apply/page.tsx
@@ -76,12 +76,12 @@ const ApplyCoffeeChat = () => (
         Feel free to chat with any of us over email, coffee, lunch-we're happy to help!
       </p>
       <div className="flex md:flex-row xs:flex-col gap-3">
-        <a href={config.coffeeChatLink} className="primary-button xs:w-full md:w-fit">
+        <a href={config.coffeeChatLink} className="primary-button xs:w-full md:w-fit text-center">
           Coffee chat with us
         </a>
         <a
           href={config.coffeeChatFormLink}
-          className="secondary-button secondary-button--red xs:w-full md:w-fit"
+          className="secondary-button secondary-button--red xs:w-full md:w-fit text-center"
         >
           Don't know who to chat with?
         </a>
