ba5064ac27dfe030ff5e735a304bc854dce6b33e
Adrienne <adriennel034@gmail.com>
2025-03-02 14:30:55 -0500

    added docs to interview scheduler dao

--- backend/src/dao/InterviewSchedulerDao.ts
+++ backend/src/dao/InterviewSchedulerDao.ts
@@ -3,6 +3,9 @@ import { interviewSchedulerCollection } from '../firebase';
 import BaseDao from './BaseDao';
 
 export default class InterviewSchedulerDao extends BaseDao<InterviewScheduler, InterviewScheduler> {
+  /**
+   * Initializes DAO with the interview scheduler collection.
+   */
   constructor() {
     super(
       interviewSchedulerCollection,
@@ -10,15 +13,27 @@ export default class InterviewSchedulerDao extends BaseDao<InterviewScheduler, I
       async (interviewScheduler) => interviewScheduler
     );
   }
-
+  /**
+   * Gets all instances of Interview Scheduler
+   * @returns a promise that resolves to an array of Interview Scheduler objects
+   */
   async getAllInstances(): Promise<InterviewScheduler[]> {
     return this.getDocuments();
   }
 
+  /**
+   * Gets a specific instance of Interview Scheduler with uuid
+   * @param uuid the uuid of a Interview Scheduler instance
+   * @returns a promise resolving to a specific instance if found and null otherwise 
+   */
   async getInstance(uuid: string): Promise<InterviewScheduler | null> {
     return this.getDocument(uuid);
   }
-
+/**
+ * Creates an instance of Interview Scheduler with uuid and assigns one if not provided
+ * @param instance the Interview Scheduler instance to create
+ * @returns uuid of created instance
+ */
   async createInstance(instance: InterviewScheduler): Promise<string> {
     const instanceWithUUID = {
       ...instance,
@@ -28,10 +43,18 @@ export default class InterviewSchedulerDao extends BaseDao<InterviewScheduler, I
     return doc.uuid;
   }
 
+  /**
+   * Updates an instance of Interview Scheduler
+   * @param instance the instance to update
+   * @returns a promise resolving to the updated instance
+   */
   async updateInstance(instance: InterviewScheduler): Promise<InterviewScheduler> {
     return this.updateDocument(instance.uuid, instance);
   }
-
+  /**
+   * Deletes an instance of Interview Scheduler
+   * @param uuid the uuid of the instance to delete
+   */
   async deleteInstance(uuid: string): Promise<void> {
     this.deleteDocument(uuid);
   }
