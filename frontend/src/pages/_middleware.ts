12a7575d0a3ba768035e093f646f7cbe32b94d65
keverie <kmr232@cornell.edu>
2025-03-11 19:40:37 -0400

    update RoutingMiddleware

--- frontend/src/pages/_middleware.ts
+++ frontend/src/pages/_middleware.ts
@@ -2,10 +2,10 @@ import { NextResponse, NextRequest } from 'next/server';
 import { useHasAdminPermission } from '../components/Common/FirestoreDataProvider';
 
 export default async function middleware(req: NextRequest) {
-    const hasAdminPersissions = useHasAdminPermission();
-    const { pathname } = req.nextUrl;
-    if (pathname.startsWith('/admin') && !hasAdminPersissions) {
-        return NextResponse.redirect('/');
-    }
-    return NextResponse.next();
+  const hasAdminPersissions = useHasAdminPermission();
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith('/admin') && !hasAdminPersissions) {
+    return NextResponse.redirect('/');
+  }
+  return NextResponse.next();
 }
