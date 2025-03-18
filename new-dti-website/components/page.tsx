8790ae0898092649eb4ea8225c23b809e2eed701
Clément <clementproze@gmail.com>
2025-03-18 19:49:00 -0400

    rm giving day banner (#891)
    Removed giving day banner, no need it anymore since we're past giving
    day date.

--- new-dti-website/components/page.tsx
+++ new-dti-website/components/page.tsx
@@ -4,7 +4,6 @@ import { ReactNode, useState } from 'react';
 import Navbar from './navbar';
 import Footer from './footer';
 import useThemeContext from '../src/hooks/useThemeContext';
-import Banner from './apply/Banner';
 
 type PageProps = {
   children: ReactNode;
@@ -17,15 +16,6 @@ const Page = ({ children }: PageProps) => {
     <>
       <ThemeContext.Provider value={{ setFooterTheme: (t) => setFooterTheme(t) }}>
         <Navbar />
-        <div className="relative">
-          <Banner
-            message={
-              "Thank you for supporting DTI! We appreciate your generosity. Click to see how much we've raised."
-            }
-            variant={'accent'}
-            link={'https://givingday.cornell.edu/campaigns/cornell-digital-tech-innovation'}
-          />
-        </div>
         {children}
         <Footer theme={footerTheme} />
       </ThemeContext.Provider>
