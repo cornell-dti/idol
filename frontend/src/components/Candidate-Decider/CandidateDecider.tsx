1ab16522f3916c6b87eb9de3acdbcf83d1f870e8
Clément <clementproze@gmail.com>
2025-03-26 19:45:04 -0400

    oops

--- frontend/src/components/Candidate-Decider/CandidateDecider.tsx
+++ frontend/src/components/Candidate-Decider/CandidateDecider.tsx
@@ -184,7 +184,7 @@ const CandidateDecider: React.FC<CandidateDeciderProps> = ({ uuid }) => {
           <Button.Group className={styles.previousNextButtonContainer}>
             <Button
               basic
-              color="red"
+              color="blue"
               disabled={currentCandidate === 0}
               onClick={() => {
                 handleCandidateChange(currentCandidate - 1);
