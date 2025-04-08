021baf2c8c2dceccf00e24e1f7ede847391d202b
Clément <clementproze@gmail.com>
2025-04-07 23:02:21 -0400

    add new elems to test page

--- new-dti-website-redesign/src/app/test/page.tsx
+++ new-dti-website-redesign/src/app/test/page.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import Link from 'next/link';
+import Button from '../components/Button';
+import IconButton from '../components/IconButton';
+import { Plus } from 'lucide-react';
 
 export default function TestPage() {
   return (
@@ -10,6 +15,7 @@ export default function TestPage() {
       <h3 className="text-accent-blue">TEST PAGE WITH STYLES & COMPONENTS:</h3>
 
       <div className="flex flex-col gap-8">
+        <h4>Typography</h4>
         <h1>Building the Future of Tech @ Cornell</h1>
         <h2>Building the Future of Tech @ Cornell</h2>
         <h3>Building the Future of Tech @ Cornell</h3>
@@ -23,10 +29,11 @@ export default function TestPage() {
       </div>
 
       <div className="flex flex-col gap-6">
+        <h4>Colors</h4>
         <div className="flex gap-4">
-          <div className="bg-background-1 w-24 h-24 rounded-md border border-border-1"></div>
-          <div className="bg-background-2 w-24 h-24 rounded-md"></div>
-          <div className="bg-background-3 w-24 h-24 rounded-md"></div>
+          <div className="bg-background-root w-24 h-24 rounded-md border border-border-1"></div>
+          <div className="bg-background-default w-24 h-24 rounded-md"></div>
+          <div className="bg-background-dimmer w-24 h-24 rounded-md"></div>
         </div>
 
         <div className="flex gap-4">
@@ -46,6 +53,34 @@ export default function TestPage() {
           <div className="bg-accent-blue w-24 h-24 rounded-md"></div>
           <div className="bg-accent-yellow w-24 h-24 rounded-md"></div>
           <div className="bg-accent-purple w-24 h-24 rounded-md"></div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h4>Buttons</h4>
+        <div className="flex gap-4">
+          <Button label="Apply today" />
+
+          <Button label="Apply" badge="12D 2H" />
+
+          <Button label="Apply today" variant="secondary" />
+
+          <Button label="Apply today" variant="tertiary" />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h4>Icon buttons</h4>
+        <div className="flex gap-4">
+          <IconButton aria-label="Create">
+            <Plus />
+          </IconButton>
+          <IconButton aria-label="Create" variant="secondary">
+            <Plus />
+          </IconButton>
+          <IconButton aria-label="Create" variant="tertiary">
+            <Plus />
+          </IconButton>
         </div>
       </div>
     </div>
