b6ac9110da5bdf78ded2693b9104f3dc5d6a3f73
Clément <clementproze@gmail.com>
2025-04-16 12:53:35 -0400

    responsivenss Hero

--- new-dti-website-redesign/src/app/components/Hero.tsx
+++ new-dti-website-redesign/src/app/components/Hero.tsx
@@ -28,8 +28,8 @@ const Hero = ({
 }: Props): ReactNode => {
   if (centered) {
     return (
-      <section>
-        <div className="flex flex-col p-8 outline-[0.5px] outline-accent-green">
+      <section className="flex flex-col-reverse md:flex-col">
+        <div className="flex flex-col p-4 outline-[0.5px] outline-accent-green sm:p-8">
           <div className="flex flex-col m-auto  items-center gap-4 max-w-120">
             <h1 className="text-center">{heading}</h1>
             <h6 className="text-center text-foreground-3">{subheading}</h6>
@@ -65,24 +65,35 @@ const Hero = ({
   return (
     <section>
       {image && (
-        <Image src={image} alt={imageAlt || 'Hero image alt text'} width={1184} height={600} />
+        <Image
+          src={image}
+          alt={imageAlt || 'Hero image alt text'}
+          width={1184}
+          height={600}
+          className="outline-[0.5px] outline-accent-green md:outline-0"
+        />
       )}
-      <div className="flex">
-        <div className="p-8 flex flex-col gap-2 flex-3/4 outline-[0.5px] outline-accent-green">
+      <div className="flex-col md:flex-row flex">
+        <div className="md:p-8 px-8 pt-8 flex flex-col gap-2 flex-3/4 md:outline-[0.5px] md:outline-accent-green">
           <h1>{heading}</h1>
           <h6 className="text-foreground-3">{subheading}</h6>
         </div>
 
-        <div className="p-8 flex flex-col gap-4 flex-1/4 outline-[0.5px] outline-accent-green justify-center">
+        <div className="p-8 flex md:flex-col gap-4 flex-1/4 md:outline-[0.5px] md:outline-accent-green md:justify-center">
           {button1Label && button1Link && (
-            <Button variant="primary" label={button1Label} href={button1Link} className="w-full" />
+            <Button
+              variant="primary"
+              label={button1Label}
+              href={button1Link}
+              className="md:w-full"
+            />
           )}
           {button2Label && button2Link && (
             <Button
               variant="secondary"
               label={button2Label}
               href={button2Link}
-              className="w-full"
+              className="md:w-full"
             />
           )}
         </div>
