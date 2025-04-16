975b459882b69b4d594de6f713c4c3776e820da9
Adrienne <adriennel034@gmail.com>
2025-04-15 22:03:50 -0400

    added more components to test page,adjusted focus

--- new-dti-website-redesign/src/app/components/FAQAccordion.tsx
+++ new-dti-website-redesign/src/app/components/FAQAccordion.tsx
@@ -21,13 +21,13 @@ export default function FAQAccordion({ header, children }: FAQAccordionProps) {
     <Accordion.Root type="single" collapsible>
       <Accordion.Item value="item-1" className="group w-[880px] border border-border-1">
         <Accordion.Header>
-          <Accordion.Trigger className="w-full focus-visible:outline focus-visible:outline-[2px] focus-visible:outline-offset-[3px] focus-visible:outline-[var(--foreground-1)]">
-            <div className=" p-[32px] group-data-[state=open]:pb-[16px] bg-background-1 group-data-[state=open]:bg-background-2 hover:bg-background-2 group-data-[state=open]:hover:bg-background-3">
-              <div className="flex justify-between items-center cursor-pointer">
-                <h6>{header}</h6>
-                <ChevronDown className="group-data-[state=open]:rotate-180 [transition:200ms_ease-out] flex-shrink-0" />
-              </div>
-            </div>
+          <Accordion.Trigger
+            className="w-full focus-visible:outline focus-visible:outline-[2px] focus-visible:outline-offset-[3px] focus-visible:outline-[var(--foreground-1)] focus-visible:relative 
+          p-[32px] group-data-[state=open]:pb-[16px] bg-background-1 group-data-[state=open]:bg-background-2 hover:bg-background-2 group-data-[state=open]:hover:bg-background-3 
+          lex justify-between items-center cursor-pointer"
+          >
+            <h6>{header}</h6>
+            <ChevronDown className="group-data-[state=open]:rotate-180 [transition:200ms_ease-out] flex-shrink-0" />
           </Accordion.Trigger>
         </Accordion.Header>
         <Accordion.Content className="p-[32px] pt-0 text-base text-foreground-3 bg-background-2">
