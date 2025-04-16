98da29332772e9231b42b92cd0d9dd5a2c6d5106
Clément <clementproze@gmail.com>
2025-04-15 22:45:14 -0400

    updated index home page

--- new-dti-website-redesign/src/app/page.tsx
+++ new-dti-website-redesign/src/app/page.tsx
@@ -1,15 +1,27 @@
 import Link from 'next/link';
 import Navbar from './components/Navbar';
+import Layout from './components/Layout';
+
+export const metadata = {
+  title: 'DIT HOMEPAGE',
+  description: 'DESCRIPTION'
+};
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <h2>
-        <Link href="/test" className="text-accent-red underline">
-          VIEW TEST PAGE
-        </Link>
-      </h2>
-    </div>
+    <>
+      <Layout>
+        <section aria-labelledby="hero">
+          <h1>Welcome</h1>
+          <p className="mt-2">testing testing 123</p>
+        </section>
+
+        <h2>
+          <Link href="/test" className="text-accent-red underline">
+            View Test Page
+          </Link>
+        </h2>
+      </Layout>
+    </>
   );
 }
