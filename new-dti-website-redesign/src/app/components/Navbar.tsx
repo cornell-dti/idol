0530e6bb190c017f319c1ed565922be0e047a9f4
Clément <clementproze@gmail.com>
2025-04-15 23:19:22 -0400

    added links + move logo to left

--- new-dti-website-redesign/src/app/components/Navbar.tsx
+++ new-dti-website-redesign/src/app/components/Navbar.tsx
@@ -4,20 +4,28 @@ import Button from './Button';
 export default function Navbar() {
   return (
     <nav className="flex justify-between p-16 max-w-[1184px] mx-4 md:mx-32 sm:mx-8 lg:mx-auto bg-background-1">
-      <p>Logo</p>
+      <Link href="/">Logo home</Link>
 
       <div className="flex gap-8">
         <ul className="flex gap-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-
           <li>
             <Link href="/team">Team</Link>
           </li>
 
           <li>
             <Link href="/products">Products</Link>
+          </li>
+
+          <li>
+            <Link href="/course">Course</Link>
+          </li>
+
+          <li>
+            <Link href="/initiatives">Initiatives</Link>
+          </li>
+
+          <li>
+            <Link href="/sponsor">Sponsor</Link>
           </li>
         </ul>
 
