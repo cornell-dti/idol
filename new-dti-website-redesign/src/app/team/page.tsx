87136b9982d6855f27ca06408dd95d8264a445b8
Clément <clementproze@gmail.com>
2025-04-15 22:45:06 -0400

    created team page

---
+++ new-dti-website-redesign/src/app/team/page.tsx
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+
+export const metadata = {
+  title: 'DTI TEAM PAGE',
+  description: 'DESCRIPTION'
+};
+
+export default function Team() {
+  return (
+    <Layout>
+      <section>
+        <h1>Team</h1>
+        <p className="mt-2">This is the team page</p>
+      </section>
+    </Layout>
+  );
+}
