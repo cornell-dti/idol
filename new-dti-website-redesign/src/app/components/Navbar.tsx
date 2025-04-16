58f59a48dd3409bda7dd56daf87264b027404753
Clément <clementproze@gmail.com>
2025-04-15 22:28:38 -0400

    create temp navbar

---
+++ new-dti-website-redesign/src/app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Button from './Button';
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between p-16">
+      <p>Logo</p>
+
+      <div className="flex gap-8">
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+
+          <li>
+            <Link href="/team">Team</Link>
+          </li>
+
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+        </ul>
+
+        <Button variant="primary" href="/apply" label="Apply" />
+      </div>
+    </nav>
+  );
+}
