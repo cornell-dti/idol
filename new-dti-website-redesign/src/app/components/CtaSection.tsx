a8d8399ea5d4a634de1cf6ee6d3179357b684bb7
Clément <clementproze@gmail.com>
2025-04-20 00:07:03 -0400

    fix bug

--- new-dti-website-redesign/src/app/components/CtaSection.tsx
+++ new-dti-website-redesign/src/app/components/CtaSection.tsx
@@ -17,38 +17,31 @@ const CtaSection = ({
   button1Link,
   button2Label,
   button2Link
-}: Props): ReactNode => {
-  return (
-    <section className="flex flex-col-reverse md:flex-col">
-      <div className="flex flex-col outline-[0.5px] outline-accent-green  p-4 sm:px-8 sm:py-16">
-        <div className="flex flex-col m-auto  items-center gap-4 max-w-120">
-          <div className="flex flex-col items-center gap-2">
-            <h2 className="text-center">{heading}</h2>
-            <h6 className="text-center text-foreground-3">{subheading}</h6>
-          </div>
+}: Props): ReactNode => (
+  <section className="flex flex-col-reverse md:flex-col">
+    <div className="flex flex-col outline-[0.5px] outline-accent-green  p-4 sm:px-8 sm:py-16">
+      <div className="flex flex-col m-auto  items-center gap-4 max-w-120">
+        <div className="flex flex-col items-center gap-2">
+          <h2 className="text-center">{heading}</h2>
+          <h6 className="text-center text-foreground-3">{subheading}</h6>
+        </div>
 
-          <div className="flex gap-4">
-            {button1Label && button1Link && (
-              <Button
-                variant="primary"
-                label={button1Label}
-                href={button1Link}
-                className="w-full"
-              />
-            )}
-            {button2Label && button2Link && (
-              <Button
-                variant="secondary"
-                label={button2Label}
-                href={button2Link}
-                className="w-full"
-              />
-            )}
-          </div>
+        <div className="flex gap-4">
+          {button1Label && button1Link && (
+            <Button variant="primary" label={button1Label} href={button1Link} className="w-full" />
+          )}
+          {button2Label && button2Link && (
+            <Button
+              variant="secondary"
+              label={button2Label}
+              href={button2Link}
+              className="w-full"
+            />
+          )}
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default CtaSection;
