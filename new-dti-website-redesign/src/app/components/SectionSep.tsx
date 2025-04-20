a15ac2653bbfef6b7c22528573ff46e51e2ec200
Clément <clementproze@gmail.com>
2025-04-20 00:17:07 -0400

    rm this piece of code too lmao we don't actually need it

--- new-dti-website-redesign/src/app/components/SectionSep.tsx
+++ new-dti-website-redesign/src/app/components/SectionSep.tsx
@@ -15,8 +15,7 @@ export default function SectionSep({ grid = false }: SectionSepProps) {
             key={i}
             className="aspect-square outline-[0.5px] outline-solid outline-border-1 shrink-0
                 w-[calc(100%/8)] 
-                md:w-[calc(100%/16)] 
-                lg:min-w-[74px]"
+                md:w-[calc(100%/16)]"
           />
         ))}
       </div>
