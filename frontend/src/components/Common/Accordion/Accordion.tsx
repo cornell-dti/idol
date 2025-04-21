a2a01462263607aa01356cadec9d6d897adb8774
Clément <clementproze@gmail.com>
2025-04-20 22:24:25 -0400

    used raw svg instead of lucide icon

--- frontend/src/components/Common/Accordion/Accordion.tsx
+++ frontend/src/components/Common/Accordion/Accordion.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ChevronDown } from 'lucide-react';
 import styles from './Accordion.module.css';
 
 type QuestionAccordionProps = {
@@ -26,7 +25,7 @@ const QuestionAccordion: React.FC<QuestionAccordionProps> = ({
         <h4>{header}</h4>
         <div className={styles.right}>
           <p className={`${styles.wordCount} small`}>{wordCount} words</p>
-          <ChevronDown />
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
         </div>
       </summary>
       <div className={styles.accordionContent}>
