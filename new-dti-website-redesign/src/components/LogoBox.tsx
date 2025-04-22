256e0755ffc1cbaea8ad543be88ecdd450b4892f
Clément <clementproze@gmail.com>
2025-04-22 19:45:44 -0400

    created LogoBox to separate it as a component

---
+++ new-dti-website-redesign/src/components/LogoBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+
+type Logo = {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+};
+
+const LogoBox: React.FC<Logo> = ({ src, alt, width, height }) => (
+  <div className="flex items-center justify-center w-40 h-24 border-l border-l-border-1">
+    <Image
+      src={src}
+      alt={alt}
+      unoptimized
+      width={width}
+      height={height}
+      style={{ width: `${width}px`, height: `${height ?? 'auto'}px` }}
+    />
+  </div>
+);
+
+export default LogoBox;
