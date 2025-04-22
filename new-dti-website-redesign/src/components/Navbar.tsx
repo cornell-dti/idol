7d22612eb78cc78518aa1ffc842d0a8a70f66707
Clément <clementproze@gmail.com>
2025-04-21 22:41:05 -0400

    minor reposiiotning

--- new-dti-website-redesign/src/components/Navbar.tsx
+++ new-dti-website-redesign/src/components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
                   href={href}
                   className={`transition-[color] h-10 duration-[120ms] hover:text-foreground-1 flex items-center relative focusState rounded-sm focus-visible:text-foreground-1 font-medium ${
                     pathname === href
-                      ? "text-foreground-1 after:content-[''] after:absolute after:bottom-[-21px] after:left-0 after:w-full after:h-[1px] after:bg-foreground-1 after:shadow-[0_-4px_8px_0_var(--foreground-1)]"
+                      ? "text-foreground-1 after:content-[''] after:absolute after:bottom-[-20px] after:left-0 after:w-full after:h-[1px] after:bg-foreground-1 after:shadow-[0_-4px_8px_0_var(--foreground-1)]"
                       : 'text-foreground-3'
                   }`}
                 >
