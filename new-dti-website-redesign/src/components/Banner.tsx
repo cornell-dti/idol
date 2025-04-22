f3dccb7ed1cb9a5671397f230fbb54c476f0e003
Clément <clementproze@gmail.com>
2025-04-21 22:05:12 -0400

    banner component

--- new-dti-website-redesign/src/components/Banner.tsx
+++ new-dti-website-redesign/src/components/Banner.tsx
@@ -1,14 +1,27 @@
 import React, { ReactNode } from 'react';
-import Button from './Button';
 
 type Props = {
   label: string;
+  type?: 'flushed' | 'pill';
 };
 
-const Banner = ({ label }: Props): ReactNode => (
-  <div className="px-5 py-3 flex items-center rounded-full border-1 border-border-1-transparent bg-background-3-transparent backdrop-blur-[32px] sticky z-10 top-4 m-auto w-fit mb-8">
-    <p>{label}</p>
-  </div>
-);
+const Banner = ({ label, type }: Props): ReactNode => {
+  if (type === 'pill') {
+    return (
+      <div className="flex items-center sticky top-0 z-10 pt-4 remove-next-section-outline">
+        <div className="px-5 py-3  rounded-full border-1 border-border-1-transparent bg-background-3-transparent backdrop-blur-[32px]  mx-auto w-fit">
+          <p>{label}</p>
+        </div>
+      </div>
+    );
+  }
+
+  // flushed or undefined
+  return (
+    <div className="p-6 sticky top-0 bg-background-2 border-t border-b border-border-1 flex items-center justify-center z-10 w-full">
+      <p>{label}</p>
+    </div>
+  );
+};
 
 export default Banner;
