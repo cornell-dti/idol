b9724a7ef49239078341a6f24ea0af1174a094a1
Clément <clementproze@gmail.com>
2025-04-22 00:04:36 -0400

    create scrollingmarquee comp

---
+++ new-dti-website-redesign/src/components/ScrolllingMarquee.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import Image from 'next/image';
+
+type Logo = {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+};
+
+const logos: Logo[] = [
+  { src: '/products/logos/cuapts.svg', alt: 'CU Apartments logo', width: 110, height: 80 },
+  { src: '/products/logos/queuemein.svg', alt: 'Queue Me In logo', width: 80, height: 80 },
+  { src: '/products/logos/zing.svg', alt: 'Zing logo', width: 111, height: 100 },
+  { src: '/products/logos/cureviews.svg', alt: 'CU Reviews logo', width: 80, height: 80 },
+  { src: '/products/logos/cornellgo.svg', alt: 'CornellGo logo', width: 80, height: 80 },
+  { src: '/products/logos/courseplan.svg', alt: 'Courseplan logo', width: 60, height: 60 },
+  { src: '/products/logos/carriage.svg', alt: 'Carriage logo', width: 70, height: 70 },
+  {
+    src: '/products/logos/design@cornell.svg',
+    alt: 'Design @ Cornell logo',
+    width: 125,
+    height: 52
+  }
+];
+
+const LogoBox: React.FC<Logo> = ({ src, alt, width, height }) => (
+  <div className="flex items-center justify-center w-40 h-24 border-l border-l-border-1">
+    <Image
+      src={src}
+      alt={alt}
+      unoptimized
+      width={width}
+      height={height}
+      style={{ width: `${width}px`, height: `${height ?? 'auto'}px` }}
+    />
+  </div>
+);
+
+const ScrollingMarquee = () => (
+  <Marquee gradient={false} speed={40} pauseOnHover className="flex w-fit">
+    {logos.map((logo, index) => (
+      <LogoBox key={index} {...logo} />
+    ))}
+  </Marquee>
+);
+
+export default ScrollingMarquee;
