3ebaa542430f10486e4f8235860734ddc64b2af1
Clément <clementproze@gmail.com>
2025-04-21 23:02:00 -0400

    more changes to banner

--- new-dti-website-redesign/src/components/Banner.tsx
+++ new-dti-website-redesign/src/components/Banner.tsx
@@ -2,24 +2,23 @@ import React, { ReactNode } from 'react';
 
 type Props = {
   label: string;
-  type?: 'flushed' | 'pill';
 };
 
-const Banner = ({ label, type }: Props): ReactNode => {
-  if (type === 'pill') {
-    return (
-      <div className="flex items-center sticky top-20 z-10 pt-4 remove-next-section-outline mt-[-67px]">
-        <div className="px-5 py-3  rounded-full border-1 border-border-1-transparent bg-background-3-transparent backdrop-blur-[32px]  mx-auto w-fit">
-          <p>{label}</p>
-        </div>
-      </div>
-    );
-  }
-
-  // flushed or undefined
+const Banner = ({ label }: Props): ReactNode => {
   return (
-    <div className="p-4 md:p-6 sticky top-20 bg-background-2 border-t border-b border-border-1 flex items-center justify-center z-10 w-full items-center text-center">
-      <p>{label}</p>
+    <div className="flex items-center justify-center sticky top-20 z-10 min-[1200px]:pt-4 min-[1200px]:mt-[-67px] remove-next-section-outline">
+      <div
+        className="
+        px-5 py-4 md:py-3 text-center w-full min-[1200px]:w-fit
+        border-t-1 border-b-1 border-t-border-1 border-b-border-1
+        min-[1200px]:border-t min-[1200px]:border-b min-[1200px]:border-border-1-transparent
+        min-[1200px]:rounded-full
+        bg-background-2 min-[1200px]:bg-background-3-transparent
+        backdrop-blur-[32px] min-[1200px]:mx-auto
+      "
+      >
+        <p>{label}</p>
+      </div>
     </div>
   );
 };
