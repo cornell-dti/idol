9a8c42d168e7fa3daf8d92e4795bcd03cbe97255
Clément <clementproze@gmail.com>
2025-04-23 11:07:39 -0400

    add instances in /product

--- new-dti-website-redesign/src/app/products/page.tsx
+++ new-dti-website-redesign/src/app/products/page.tsx
@@ -1,4 +1,7 @@
+import SectionSep from '@/components/SectionSep';
 import Layout from '../../components/Layout';
+import products from './products.json';
+import Product from './Product';
 
 export const metadata = {
   title: 'DTI PRODUCTS PAGE',
@@ -12,6 +15,23 @@ export default function Products() {
         <h1>PRODUCTS</h1>
         <p className="mt-2">This is the PRODUCTS page</p>
       </section>
+
+      <SectionSep />
+
+      {products.map((product, index) => (
+        <>
+          <Product
+            key={index}
+            image={product.image}
+            imageAlt={product.imageAlt}
+            name={product.name}
+            description={product.description}
+            link={product.link}
+          />
+          {index < products.length - 1 && <SectionSep grid />}
+        </>
+      ))}
+
       <section className="bg-background-3 h-[800px]">
         <h2>title</h2>
         <p className="mt-2">This is the PRODUCTS page</p>
