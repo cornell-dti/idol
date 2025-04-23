c4047f3162ebc563c88cfd9e399bcf5c31d56b4f
Adrienne <adriennel034@gmail.com>
2025-04-22 23:06:46 -0400

    size and format

--- new-dti-website-redesign/src/components/FeatureCard.tsx
+++ new-dti-website-redesign/src/components/FeatureCard.tsx
@@ -10,7 +10,7 @@ type FeatureCardProps = {
 
 export default function FeatureCard({ title, body, children }: FeatureCardProps) {
   return (
-    <div className="p-[32px] min-w-[395px] border border-border-1 flex flex-col">
+    <div className="p-[32px] max-w-[395px] border border-border-1 flex flex-col">
       <IconWrapper className="icon-wrapper">{children}</IconWrapper>
       <h5 className="text-foreground-1 pt-[16px]">{title}</h5>
       <p className="text-foreground-3">{body}</p>
