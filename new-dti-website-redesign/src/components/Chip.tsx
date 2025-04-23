c148c5c6e918b0d20aedc017a86969932cfde466
Clément <clementproze@gmail.com>
2025-04-23 11:25:38 -0400

    new <Chip> conponetn

---
+++ new-dti-website-redesign/src/components/Chip.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  label: string;
+  color?: 'default' | 'red' | 'green' | 'blue' | 'yellow' | 'purple';
+};
+
+export default function Chip({ label, color = 'default' }: Props) {
+  const colorMap: Record<string, string> = {
+    default: 'bg-background-2 text-foreground-2',
+    red: 'bg-accent-red-transparent text-accent-red',
+    green: 'bg-accent-green-transparent text-accent-green',
+    blue: 'bg-accent-blue-transparent text-accent-blue',
+    yellow: 'bg-accent-yellow-transparent text-accent-yellow',
+    purple: 'bg-accent-purple-transparent text-accent-purple'
+  };
+
+  const chipColor = colorMap[color] ?? colorMap.default;
+
+  return (
+    <p className={`rounded-full whitespace-nowrap px-3 py-1 caps small ${chipColor}`}>{label}</p>
+  );
+}
