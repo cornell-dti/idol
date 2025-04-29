e3ff5ad51a3fe07d191f4b43010e7366f387871f
Clément <clementproze@gmail.com>
2025-04-29 10:32:23 -0400

    [New website] Updates to `<Button>`, `<Tabs>` component, heading semantic fixes  (#937)
    # Changes
    
    - Made changes to `<Button>`:
      - Refactored into `forwardRef` to support native props better
    - Added `role` so that we can differentiate `role="tab"` and the regular
    `role="button"`
    - `<Hero>` and `<CtaSection>` components: made the subheadings more
    semantic by using `<p>` instead of `<h6>`
    - Created `<Tabs>` component that uses `<Button>`
    - `label` (str) and `content` (`React.ReactNode`) for the tab label and
    for the actual children of the tab panel
    - Added some logic to support proper keyboard navigation between tabs
    for accessibility
      - Used instances of `<Tabs>` on the `/test-components` page
    
    
    
    # Video
    
    
    https://github.com/user-attachments/assets/07dc0386-8d21-476d-8187-7a8a6e2c1ec7
    
    # Test plan
    
    - Head to `/test-components`
    - Click the tabs and make sure they change the panel
    - Use `tab` on your keyboard, and the left/right arrows to switch
    between tabs
    - For tab panel 1 and tab panel 2, hitting `tab` should focus the panel
    itself. For tab panel 2, `tab` should focus the `<Input>`
    
    ---------
    
    Co-authored-by: Chris Chen <144409850+cchrischen@users.noreply.github.com>

--- new-dti-website-redesign/src/components/Button.tsx
+++ new-dti-website-redesign/src/components/Button.tsx
@@ -1,58 +1,68 @@
 'use client';
 
+import { forwardRef, Ref } from 'react';
 import Link from 'next/link';
 
 type ButtonProps = {
   label: string;
-  onClick?: () => void;
   href?: string;
-  className?: string;
   variant?: 'primary' | 'secondary' | 'tertiary';
   badge?: React.ReactNode;
-};
+  role?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+  React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-export default function Button({
-  label,
-  onClick,
-  href,
-  className = '',
-  variant = 'primary',
-  badge
-}: ButtonProps) {
-  const baseStyles = `
-    px-6 h-12 w-fit rounded-full cursor-pointer inline-flex items-center justify-center gap-2
-    transition-[background-color] duration-[120ms] focusState text-nowrap`;
+const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+  (
+    { label, onClick, href, className = '', variant = 'primary', badge, role = 'button', ...rest },
+    ref
+  ) => {
+    const baseStyles = `
+      px-6 h-12 w-fit rounded-full cursor-pointer inline-flex items-center justify-center gap-2
+      transition-[background-color] duration-[120ms] focusState text-nowrap`;
 
-  const variantStyles = {
-    primary: `bg-foreground-1 text-background-1 hover:bg-foreground-2 ${badge ? 'gap-1 pr-3' : ''}`,
-    secondary: `bg-background-2 border border-border-1 text-foreground-1 hover:bg-background-3`,
-    tertiary: `bg-transparent border border-border-1 text-foreground-1 hover:bg-background-2`
-  }[variant];
+    const variantStyles = {
+      primary: `bg-foreground-1 text-background-1 hover:bg-foreground-2 ${
+        badge ? 'gap-1 pr-3' : ''
+      }`,
+      secondary: `bg-background-2 border border-border-1 text-foreground-1 hover:bg-background-3`,
+      tertiary: `bg-transparent border border-border-1 text-foreground-1 hover:bg-background-2`
+    }[variant];
 
-  const sharedClasses = `${baseStyles} ${variantStyles} ${className}`;
+    const sharedClasses = `${baseStyles} ${variantStyles} ${className}`;
 
-  const content = (
-    <>
-      <span className="text-rg font-medium">{label}</span>
-      {badge && (
-        <span className="p-2 bg-[#0000001a] text-background-1 rounded-full text-xs font-medium uppercase tracking-wider">
-          {badge}
-        </span>
-      )}
-    </>
-  );
+    const content = (
+      <>
+        <span className="text-rg font-medium">{label}</span>
+        {badge && (
+          <span className="p-2 bg-[#0000001a] text-background-1 rounded-full text-xs font-medium uppercase tracking-wider">
+            {badge}
+          </span>
+        )}
+      </>
+    );
 
-  if (href) {
+    if (href) {
+      return (
+        <Link href={href} className={sharedClasses} {...rest} ref={ref as Ref<HTMLAnchorElement>}>
+          {content}
+        </Link>
+      );
+    }
+
     return (
-      <Link href={href} className={sharedClasses}>
+      <button
+        onClick={onClick}
+        className={sharedClasses}
+        role={role}
+        {...rest}
+        ref={ref as Ref<HTMLButtonElement>}
+      >
         {content}
-      </Link>
+      </button>
     );
   }
+);
 
-  return (
-    <button onClick={onClick} className={sharedClasses} type="button">
-      {content}
-    </button>
-  );
-}
+Button.displayName = 'Button';
+export default Button;
