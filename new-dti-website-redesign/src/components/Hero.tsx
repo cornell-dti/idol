e638d05b239fb305ff749c462105900f7685aea0
Clément <clementproze@gmail.com>
2025-04-30 02:04:36 -0400

    fixed hero seciton img height

--- new-dti-website-redesign/src/components/Hero.tsx
+++ new-dti-website-redesign/src/components/Hero.tsx
@@ -78,7 +78,7 @@ const Hero = ({
           alt={imageAlt || 'Hero image alt text'}
           width={1184}
           height={600}
-          className="outline-[0.5px] outline-border-1 md:outline-0"
+          className="outline-[0.5px] outline-border-1 md:outline-0 h-[600px]"
         />
       )}
       <div className="flex-col md:flex-row flex">
