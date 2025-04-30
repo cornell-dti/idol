1153d22a0fe544fdcaff901da42172ac4e736de8
Clément <clementproze@gmail.com>
2025-04-30 11:22:03 -0400

    logo box on product

--- new-dti-website-redesign/src/app/products/page.tsx
+++ new-dti-website-redesign/src/app/products/page.tsx
@@ -4,11 +4,90 @@ import Layout from '../../components/Layout';
 import products from './products.json';
 import Product from './Product';
 import Hero from '../../components/Hero';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'DTI PRODUCTS PAGE',
   description: 'DESCRIPTION'
 };
+
+type Logo = {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  anchor: string;
+};
+
+const logos: Logo[] = [
+  {
+    src: '/products/logos/cuapts.svg',
+    alt: 'CU Apartments logo',
+    width: 110,
+    height: 80,
+    anchor: '#cuapts'
+  },
+  {
+    src: '/products/logos/queuemein.svg',
+    alt: 'Queue Me In logo',
+    width: 80,
+    height: 80,
+    anchor: '#queuemein'
+  },
+  { src: '/products/logos/zing.svg', alt: 'Zing logo', width: 96, height: 96, anchor: '#zing' },
+  {
+    src: '/products/logos/cureviews.svg',
+    alt: 'CU Reviews logo',
+    width: 80,
+    height: 80,
+    anchor: '#cureviews'
+  },
+  {
+    src: '/products/logos/cornellgo.svg',
+    alt: 'CornellGo logo',
+    width: 80,
+    height: 80,
+    anchor: '#cornellgo'
+  },
+  {
+    src: '/products/logos/courseplan.svg',
+    alt: 'Courseplan logo',
+    width: 60,
+    height: 60,
+    anchor: '#courseplan'
+  },
+  {
+    src: '/products/logos/carriage.svg',
+    alt: 'Carriage logo',
+    width: 70,
+    height: 70,
+    anchor: '#carriage'
+  },
+  {
+    src: '/products/logos/design@cornell.svg',
+    alt: 'Design @ Cornell logo',
+    width: 125,
+    height: 52,
+    anchor: '#design@cornell'
+  }
+];
+
+const LogoBox: React.FC<Logo> = ({ src, alt, width, height, anchor }) => (
+  <Link
+    className="flex items-center justify-center w-1/4 h-24 border border-border-1"
+    href={anchor}
+  >
+    <Image
+      src={src}
+      alt={alt}
+      unoptimized
+      width={width}
+      height={height}
+      style={{ width: `${width}px`, height: `${height ?? 'auto'}px` }}
+    />
+  </Link>
+);
 
 export default function Products() {
   return (
@@ -23,6 +102,14 @@ export default function Products() {
         image="/products/hero.png"
         imageAlt="DTI students brainstorming with sticky notes"
       />
+
+      <SectionSep />
+
+      <div className="flex flex-wrap">
+        {logos.map((logo, index) => (
+          <LogoBox key={index} {...logo} />
+        ))}
+      </div>
 
       <SectionSep />
 
