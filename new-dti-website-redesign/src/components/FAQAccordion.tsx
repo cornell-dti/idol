2c4aeaf4abfb775d128e4da6198be7fdcaee81d3
Adrienne <adriennel034@gmail.com>
2025-05-05 03:09:56 -0400

    changed icon to svg

--- new-dti-website-redesign/src/components/FAQAccordion.tsx
+++ new-dti-website-redesign/src/components/FAQAccordion.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ChevronDown } from 'lucide-react';
 
 // Props type for the FAQAccordion component
 type FAQAccordionProps = {
@@ -26,7 +25,27 @@ export default function FAQAccordion({ header, children }: FAQAccordionProps) {
           flex justify-between items-center cursor-pointer"
       >
         <h6>{header}</h6>
-        <ChevronDown className="group-open:rotate-180 [transition:200ms_ease-out]" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="25"
+          viewBox="0 0 24 25"
+          className="
+      w-6 h-6
+      flex-shrink-0
+      transition-transform duration-200 ease-out
+      group-open:-rotate-180
+    "
+          fill="none"
+        >
+          <path
+            d="M6 9.5L12 15.5L18 9.5"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </summary>
 
       <div className="p-[32px] pt-0 text-base text-foreground-3 bg-background-2">{children}</div>
