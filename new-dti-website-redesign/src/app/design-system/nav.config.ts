a39a16f0a67a4024c8a86bca05a00f09a23c06a9
Clément <clementproze@gmail.com>
2025-05-15 15:51:39 -0400

    [New website] Dev navbar and design system library page (#950)
    # Changes
    
    - Added new directory called `design-system` containing documentation +
    components for the design system
    - They are currenlty mostly temporary – this PR is more for establishing
    the skeleton layout
    - Added `nav.config.ts` for sidebar navigation on these pages
    - Added pages for Introduction, Styles (Color/Typography/Layout) and
    Components (Button/Input) – all are empty for now, this is intentional!
    - Created dev navbar component to easily switch between test pages 
    - You can hide the dev navbar for a more immersive experience if you
    want (reloading the page brings it back)
    - I think in the future, we'll remove the `/test-page` and
    `/test-components` pages specifically to integrate them into thee design
    system library
    
    # Pics
    
    
    Dev navbar
    
    
    
    https://github.com/user-attachments/assets/0c19f8fc-d3dc-4c57-814b-ea85d2fad06b
    
    
    
     Design system library:
    
    https://github.com/user-attachments/assets/b31c81ec-f760-40f6-8fe6-876c5b645d6f
    
    
    # Test plan
    - On localhost, check that dev navbar shows correctly on all pages and
    that the links bring you to correct pages
    - make sure "Hide" hides the navbar, but reloading the page brings it
    back
    - Test navigation in the sidebar in the design system page
    
    
    
    ## NOTE: i'm aware there's lots of missing content and that this design
    system page might not be perfectly responsive (for now). Am choosing to
    move ahead with this PR as a first milestone and to just get all the
    basic skeleton layout in place. Will fix all these issues subsequently!

---
+++ new-dti-website-redesign/src/app/design-system/nav.config.ts
@@ -0,0 +1,55 @@
+const navItems = [
+  {
+    category: null,
+    items: [
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system',
+        label: 'Introduction',
+        description: 'Overview of the design system.'
+      }
+    ]
+  },
+  {
+    category: 'Styles',
+    items: [
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system/styles/color',
+        label: 'Color',
+        description: 'Color description blah.'
+      },
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system/styles/typography',
+        label: 'Typography',
+        description: 'Typography description blah.'
+      },
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system/styles/layout',
+        label: 'Layout',
+        description: 'Layout description blah.'
+      }
+    ]
+  },
+  {
+    category: 'Components',
+    items: [
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system/components/button',
+        label: 'Button',
+        description: 'Buttons description bruh.'
+      },
+      {
+        thumbnail: '/design-system/thumb.jpg',
+        href: '/design-system/components/input',
+        label: 'Input',
+        description: 'Input description bruh.'
+      }
+    ]
+  }
+];
+
+export default navItems;
