871ea430ae6bacc265f8d5e5f666e242c6c2c97f
Clément <clementproze@gmail.com>
2025-05-15 10:02:35 -0400

    [New Website] Added new type to `<IconWrapper>` (#961)
    # Changes
    
    [Figma
    specs](https://www.figma.com/design/ttAGEX3pHmzuhMzp8uAyhz/SP25-Cornelldti.org-Website-Revamp?node-id=1336-16568&m=dev)
    Realized that we needed a white colored iconwrapper so added that type
    (i had forgotten to add it when i created the component initially lol)
    
    
    # Pic 
    <img width="326" alt="image"
    src="https://github.com/user-attachments/assets/b571acd6-b9e9-43f0-a765-9bc2b051701f"
    />
    
    
    # Test plan
    - Navigate to `/test-components`
    - Scroll down to "Icon wrappers" section
    - Ensure that there are 4 icon wrappers
      - 2 primary (white colored), 1 default (`64px`) and 1 small (`48px`)
    - 2 default (dark gray colored), 1 default (`64px`) and 1 small (`48px`

--- new-dti-website-redesign/src/components/IconWrapper.tsx
+++ new-dti-website-redesign/src/components/IconWrapper.tsx
@@ -1,22 +1,28 @@
 type IconWrapperProps = {
   children: React.ReactNode;
   size?: 'default' | 'small';
+  type?: 'primary' | 'default';
   className?: string;
 };
 
 export default function IconWrapper({
   children,
   size = 'default',
+  type = 'default',
   className = ''
 }: IconWrapperProps) {
   const wrapperSize = size === 'small' ? 'w-12 h-12' : 'w-16 h-16';
+  const wrapperColor =
+    type === 'primary'
+      ? 'bg-foreground-1 text-background-1'
+      : 'bg-background-2 border border-border-1';
   // default: parent wrapper = 64px, child icon = 32px
   // small: parent wrapper = 48px, child icon = 24px
   const iconSize = size === 'small' ? '[&>svg]:w-6 [&>svg]:h-6' : '[&>svg]:w-8 [&>svg]:h-8';
 
   return (
     <div
-      className={`flex items-center justify-center rounded-full bg-background-2 border border-border-1 ${wrapperSize} ${iconSize} ${className}`}
+      className={`flex items-center justify-center rounded-full ${wrapperSize} ${wrapperColor} ${iconSize} ${className}`}
     >
       {children}
     </div>
