46bb41d9680b5cba4742ddb0b10bee32dff23216
Adrienne <adriennel034@gmail.com>
2025-05-15 23:34:32 -0400

    format

--- new-dti-website-redesign/src/app/test-components/page.tsx
+++ new-dti-website-redesign/src/app/test-components/page.tsx
@@ -459,11 +459,11 @@ export default function TestComponents() {
           </FAQAccordion>
           <FAQAccordion header="How do we evaluate the applications?">
             <p>
-              We look through every single application we receive. We fully understand and don&apos;t
-              expect that everyone has a significant amount of past experience. Taking into account
-              your year and past coursework, we will assess your interest in being a part of Cornell
-              Digital Tech & Innovation and your willingness to learn and make a significant
-              contribution.
+              We look through every single application we receive. We fully understand and
+              don&apos;t expect that everyone has a significant amount of past experience. Taking
+              into account your year and past coursework, we will assess your interest in being a
+              part of Cornell Digital Tech & Innovation and your willingness to learn and make a
+              significant contribution.
             </p>
             <p>
               We have one behavioral and one technical round for each role interview. During these
@@ -475,10 +475,10 @@ export default function TestComponents() {
           <FAQAccordion header="What could I do to learn more about DTI?">
             <p>
               Coffee chats are casual conversations allow you to find out more about DTI and ask any
-              questions about our team. A &lsquo;coffee chat&rsquo; doesn&apos;t need to actually be over coffee, but
-              should be 30 minutes like an actual coffee catch up with a friend. Get the most out of
-              the coffee chat by preparing your questions ahead of time and researching the other
-              person&apos;s experiences first.
+              questions about our team. A &lsquo;coffee chat&rsquo; doesn&apos;t need to actually be
+              over coffee, but should be 30 minutes like an actual coffee catch up with a friend.
+              Get the most out of the coffee chat by preparing your questions ahead of time and
+              researching the other person&apos;s experiences first.
             </p>
           </FAQAccordion>
         </div>
