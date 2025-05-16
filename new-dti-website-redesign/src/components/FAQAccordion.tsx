e19257767508702f9b0cd02731714e77642726b8
Adrienne <adriennel034@gmail.com>
2025-05-15 22:48:24 -0400

    review changes

--- new-dti-website-redesign/src/components/FAQAccordion.tsx
+++ new-dti-website-redesign/src/components/FAQAccordion.tsx
@@ -16,12 +16,12 @@ type FAQAccordionProps = {
  */
 export default function FAQAccordion({ header, children }: FAQAccordionProps) {
   return (
-    <details className="group border border-border-1">
+    <details className="group border border-border-1 border-b-0 last:border-b">
       <summary
         className="
           list-none p-8
-          focus-visible:outline focus-visible:outline-[2px] focus-visible:outline-offset-[3px] focus-visible:outline-[var(--foreground-1)] focus-visible:relative 
-          group-open:pb-[16px] bg-background-1 group-open:bg-background-2 hover:bg-background-2 group-open:hover:bg-background-3
+          focusState focus-visible:relative 
+          group-open:pb-[16px] bg-background-1 group-open:bg-background-2 hover:bg-background-2 group-open:hover:bg-background-3 transition-[background-color] duration-[120ms]
           flex justify-between items-center cursor-pointer"
       >
         <h6>{header}</h6>
