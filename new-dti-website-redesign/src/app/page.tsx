11532cf4f8c7365458bc33050f3c968bb83a0319
Clément <clementproze@gmail.com>
2025-05-16 13:23:20 -0400

    rm old import

--- new-dti-website-redesign/src/app/page.tsx
+++ new-dti-website-redesign/src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import FeatureSection from '../components/FeatureSection';
