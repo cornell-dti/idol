2e6cfb2731cb41abc5375de555bb70d952f5d6df
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-05-16 17:44:36 -0400

    fix shadow on selected

--- new-dti-website-redesign/src/components/TeamCard.tsx
+++ new-dti-website-redesign/src/components/TeamCard.tsx
@@ -49,7 +49,7 @@ export const MemberCard = ({ user, image, selected, onClick }: MemberCardProps) 
 
   return (
     <button
-      className={`${baseStyles} ${selected ? 'bg-background-2 border-b-foreground-1 shadow-white shadow-[0_-4px_8px_0]' : ''}`}
+      className={`${baseStyles} ${selected ? `relative bg-background-2 border-b-foreground-1 after:content-[''] after:absolute after:bottom-[-1px] after:left-0 after:w-full after:h-[1px] after:bg-foreground-1 after:shadow-[0_-4px_8px_0_var(--foreground-1)] after:rounded-full` : ''}`}
       onClick={onClick}
     >
       <MemberSummary user={user} image={image} />
