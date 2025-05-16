4b5f1b559e2f55172b8148e7dbb492c46bd85b1b
Clément <clementproze@gmail.com>
2025-05-15 22:20:30 -0400

    update icons in icons page

--- new-dti-website-redesign/src/app/design-system/components/icon/icons.tsx
+++ new-dti-website-redesign/src/app/design-system/components/icon/icons.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { RocketIcon } from './RocketIcon';
+import { PlusIcon } from './PlusIcon';
 
 export const icons = [
   { label: 'Rocket', svg: <RocketIcon /> },
+  { label: 'Plus', svg: <PlusIcon /> },
   { label: 'Rocket', svg: <RocketIcon /> },
+  { label: 'Plus', svg: <PlusIcon /> },
   { label: 'Rocket', svg: <RocketIcon /> },
-  { label: 'Rocket', svg: <RocketIcon /> },
-  { label: 'Rocket', svg: <RocketIcon /> },
-  { label: 'Rocket', svg: <RocketIcon /> },
+  { label: 'Plus', svg: <PlusIcon /> },
   { label: 'Rocket', svg: <RocketIcon /> }
 ];
