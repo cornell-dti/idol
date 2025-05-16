ea16babb6feb3338768760c43e41c2bbfa248184
Clément <clementproze@gmail.com>
2025-05-15 20:05:51 -0400

    updaet page section styling

--- new-dti-website-redesign/src/app/design-system/PageSection.tsx
+++ new-dti-website-redesign/src/app/design-system/PageSection.tsx
@@ -8,7 +8,7 @@ export default function PageSection({ title, description, children }: PageSectio
   return (
     <section className="p-12 flex flex-col gap-6">
       <div className="flex flex-col gap-2">
-        <h2>{title}</h2>
+        <h2 className="h4">{title}</h2>
         {description && <p className="text-foreground-3">{description}</p>}
       </div>
 
