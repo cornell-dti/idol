2e15662b58c421e814098408b15f1ef76feff64a
Clément <clementproze@gmail.com>
2025-05-15 22:19:55 -0400

    added plus icon + used it on Button

--- new-dti-website-redesign/src/app/design-system/components/button/page.tsx
+++ new-dti-website-redesign/src/app/design-system/components/button/page.tsx
@@ -3,6 +3,7 @@ import PageLayout from '../../PageLayout';
 import PageSection from '../../PageSection';
 import Button from '../../../../components/Button';
 import IconButton from '../../../../components/IconButton';
+import { PlusIcon } from '../icon/PlusIcon';
 
 export default function ButtonPage() {
   return (
@@ -19,55 +20,13 @@ export default function ButtonPage() {
 
       <PageSection title="Icon button" description="Use when you just need an icon">
         <IconButton aria-label="Create">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-plus-icon lucide-plus"
-          >
-            <path d="M5 12h14" />
-            <path d="M12 5v14" />
-          </svg>
+          <PlusIcon />
         </IconButton>
         <IconButton aria-label="Create" variant="secondary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-plus-icon lucide-plus"
-          >
-            <path d="M5 12h14" />
-            <path d="M12 5v14" />
-          </svg>
+          <PlusIcon />
         </IconButton>
         <IconButton aria-label="Create" variant="tertiary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-plus-icon lucide-plus"
-          >
-            <path d="M5 12h14" />
-            <path d="M12 5v14" />
-          </svg>
+          <PlusIcon />
         </IconButton>
       </PageSection>
     </PageLayout>
