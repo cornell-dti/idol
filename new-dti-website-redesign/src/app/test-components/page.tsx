069a20991fe961e06d98c877403ea4ab04b33d01
Clément <clementproze@gmail.com>
2025-05-17 01:50:43 -0400

    update test components in /test-page

--- new-dti-website-redesign/src/app/test-components/page.tsx
+++ new-dti-website-redesign/src/app/test-components/page.tsx
@@ -237,6 +237,26 @@ export default function TestComponents() {
       <div className="flex flex-col gap-6">
         <h4>Icon wrappers</h4>
         <div className="flex gap-4">
+          <IconWrapper size="large" type="primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-rocket-icon lucide-rocket"
+            >
+              <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
+              <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
+              <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
+              <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
+            </svg>
+          </IconWrapper>
+
           <IconWrapper size="default" type="primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -279,6 +299,26 @@ export default function TestComponents() {
         </div>
 
         <div className="flex gap-4">
+          <IconWrapper size="large" type="default">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-rocket-icon lucide-rocket"
+            >
+              <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
+              <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
+              <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
+              <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
+            </svg>
+          </IconWrapper>
+
           <IconWrapper size="default" type="default">
             <svg
               xmlns="http://www.w3.org/2000/svg"
