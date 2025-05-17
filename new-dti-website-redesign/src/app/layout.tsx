46a8934f7577936f0a147f5df07d1ee40d7d92d7
Clément <clementproze@gmail.com>
2025-05-17 12:30:32 -0400

    move Navbar directly inside root layout.tsx

--- new-dti-website-redesign/src/app/layout.tsx
+++ new-dti-website-redesign/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
