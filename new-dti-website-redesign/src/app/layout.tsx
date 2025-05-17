0a7231c94759b481113bf035c2155359b5fcee4b
Clément <clementproze@gmail.com>
2025-05-17 13:20:39 -0400

    update improt

--- new-dti-website-redesign/src/app/layout.tsx
+++ new-dti-website-redesign/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
