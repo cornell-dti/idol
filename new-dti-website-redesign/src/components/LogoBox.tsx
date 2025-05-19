2f815acae511962f8c8bed2e02b39e41987e5a16
Clément <clementproze@gmail.com>
2025-05-18 23:50:50 -0400

    update Logobox comp

--- new-dti-website-redesign/src/components/LogoBox.tsx
+++ new-dti-website-redesign/src/components/LogoBox.tsx
@@ -9,9 +9,10 @@ type Logo = {
   height?: number;
   fillWidth?: boolean;
   href?: string;
+  noLink?: boolean;
 };
 
-const LogoBox: React.FC<Logo> = ({ src, alt, width, height, fillWidth, href }) => {
+const LogoBox: React.FC<Logo> = ({ src, alt, width, height, fillWidth, href, noLink }) => {
   const content = (
     <div
       className="flex items-center justify-center h-24 border-border-1 border-l-1 border-b-1 p-4"
@@ -28,15 +29,17 @@ const LogoBox: React.FC<Logo> = ({ src, alt, width, height, fillWidth, href }) =
     </div>
   );
 
-  return href ? (
+  if (noLink || !href) {
+    return content;
+  }
+
+  return (
     <Link
       href={href}
       className="hover:bg-background-2 transition-[background-color] duration-[120ms] focusState"
     >
       {content}
     </Link>
-  ) : (
-    content
   );
 };
 
