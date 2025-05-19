a6be07c6071974c2d40240e607e0d50ec3dec878
Clément <clementproze@gmail.com>
2025-05-18 23:50:54 -0400

    update deisgn system

--- new-dti-website-redesign/src/app/design-system/components/logo-box/page.tsx
+++ new-dti-website-redesign/src/app/design-system/components/logo-box/page.tsx
@@ -4,23 +4,22 @@ import Image from 'next/image';
 import PageLayout from '../../PageLayout';
 import PageSection from '../../PageSection';
 import LogoBox from '../../../../components/LogoBox';
-import { logos as regularLogos } from '../../../page';
-import { logos as linkLogos } from '../../../products/page';
+import logos from '../../../products/logos.json';
 
 export default function LogoBoxPage() {
   return (
     <PageLayout title="Logo box" description="Guidelines for logo box component.">
       <PageSection title="Regular logo box" description="">
         <div className="grid grid-cols-4 border-t-1 border-border-1">
-          {regularLogos.map((logo, index) => (
-            <LogoBox key={index} {...logo} fillWidth />
+          {logos.map((logo, index) => (
+            <LogoBox key={index} {...logo} fillWidth noLink />
           ))}
         </div>
       </PageSection>
 
       <PageSection title="Logo box with link" description="">
         <div className="grid grid-cols-4 border-t-1 border-border-1">
-          {linkLogos.map((logo, index) => (
+          {logos.map((logo, index) => (
             <LogoBox key={index} {...logo} fillWidth />
           ))}
         </div>
@@ -28,8 +27,8 @@ export default function LogoBoxPage() {
 
       <PageSection title="Logo box width fixed width" description="">
         <div className="flex flex-wrap">
-          {regularLogos.map((logo, index) => (
-            <LogoBox key={index} {...logo} />
+          {logos.map((logo, index) => (
+            <LogoBox key={index} {...logo} noLink />
           ))}
         </div>
       </PageSection>
