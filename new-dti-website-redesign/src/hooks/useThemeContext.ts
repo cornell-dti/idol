eeb88eb844559721166c079a29c8a065a1064988
jujulcrane <jujulcrane@gmail.com>
2025-05-20 16:55:33 -0400

    deleted unused things

--- new-dti-website-redesign/src/hooks/useThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const ThemeContext = createContext<
-  { setFooterTheme: (theme: 'dark' | 'light') => void } | undefined
->(undefined);
-
-const useThemeContext = () => ({ ThemeContext, theme: useContext(ThemeContext) });
-
-export default useThemeContext;
--- new-dti-website-redesign/src/hooks/useTitle.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useTitle = (title?: string) => {
-  useEffect(() => {
-    document.title = `${title ? `${title} | ` : ''} Cornell DTI`;
-  }, [title]);
-};
-
-export default useTitle;
