c65285b1138c85b3123f42a31263eaf8f0cd3e53
jujulcrane <jujulcrane@gmail.com>
2025-05-20 21:40:28 -0400

    Update page.tsx

--- new-dti-website-redesign/src/app/course/page.tsx
+++ new-dti-website-redesign/src/app/course/page.tsx
@@ -2,7 +2,7 @@ import Hero from '../../components/Hero';
 import Layout from '../../components/Layout';
 import CtaSection from '../../components/CtaSection';
 import SectionSep from '../../components/SectionSep';
-import Timeline, { Event } from '@/components/Timeline';
+import Timeline, { Event } from '../../components/Timeline';
 
 export const metadata = {
   title: 'DTI COURSE PAGE',
