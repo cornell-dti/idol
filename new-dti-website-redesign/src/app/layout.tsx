73795a7b4f36a829d6e5cc9494d91cf2ad957eed
Clément <clementproze@gmail.com>
2025-05-22 01:37:18 -0400

    fix typo

--- new-dti-website-redesign/src/app/layout.tsx
+++ new-dti-website-redesign/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <a
           id="skip-to-main"
           href="#main-content"
-          className={`${baseStyles} absolute -inset-4 opacity-0 z-50 p-2
+          className={`${baseStyles} absolute -inset-4 opacity-0 z-50
           focus-visible:opacity-100 focus-visible:top-4 focus-visible:left-4 transition-all duration-[120ms] px-6 h-12 bg-foreground-1 text-background-1 hover:bg-foreground-2 w-fit`}
         >
           Skip to main content
