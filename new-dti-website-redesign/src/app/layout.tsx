6fba2bb7b70d74b8dddc98f2f4835527c40bd265
Clément <clementproze@gmail.com>
2025-05-22 01:28:38 -0400

    import button base styles

--- new-dti-website-redesign/src/app/layout.tsx
+++ new-dti-website-redesign/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import './globals.css';
 import { usePathname } from 'next/navigation';
 import Navbar from '../components/Navbar';
+import { baseStyles } from '../components/Button';
 
 export default function RootLayout({
   children
@@ -27,8 +28,8 @@ export default function RootLayout({
         <a
           id="skip-to-main"
           href="#main-content"
-          className="absolute -inset-4 opacity-0 z-50 p-2
-          focus-visible:opacity-100 focus-visible:top-4 focus-visible:left-4 transition-[top, background-color] duration-[120ms] px-6 h-12 bg-foreground-1 text-background-1 hover:bg-foreground-2 w-fit rounded-full flex items-center justify-center font-medium focusState "
+          className={`${baseStyles} absolute -inset-4 opacity-0 z-50 p-2
+          focus-visible:opacity-100 focus-visible:top-4 focus-visible:left-4 transition-all duration-[120ms] px-6 h-12 bg-foreground-1 text-background-1 hover:bg-foreground-2 w-fit`}
         >
           Skip to main content
         </a>
