db63dfefae7f915ec8ac8c7739feefea3b7c1357
ladriennel <adriennel034@gmail.com>
2025-05-22 14:56:47 -0400

    [New Website] Created new FAQ Accordion component (#917)
    ### Summary <!-- Required -->
    
    <!-- Provide a general summary of your changes in the Title above -->
    <!-- Itemize bug fixes, new features, and other changes -->
    <!-- Feel free to break this into sub-sections, i.e. features, fixes,
    etc. -->
    <!-- Some examples are shown below. -->
    
    - [x] Created FAQ Accordion component for new website
    - [x] Added components to test page
    - [x] Adjusted code to reduce unnecessary wrappers and fix focus states
    
    ### Notion/Figma Link <!-- Optional -->
    Notion:
    https://www.notion.so/Idol-SP25-1890ad723ce180a19f3ff99268d34e53?p=1cf0ad723ce180c4895af1e98084dd98&pm=s
    
    Figma:
    https://www.figma.com/design/ttAGEX3pHmzuhMzp8uAyhz/SP25-Cornelldti.org-Website-Revamp?node-id=1800-6489&t=VZTqa61PZPvia3dF-0
    <!-- If the changes have associated Notion pages/Figma design(s), please
    include the links here.-->
    
    <img width="942" alt="Screenshot 2025-04-15 at 10 00 41 PM"
    src="https://github.com/user-attachments/assets/d3ff43d0-b17b-4500-bd21-29bbc51228f7"
    />

--- new-dti-website-redesign/src/app/test-components/page.tsx
+++ new-dti-website-redesign/src/app/test-components/page.tsx
@@ -12,6 +12,7 @@ import IconWrapper from '../../components/IconWrapper';
 import FeatureCard from '../../components/FeatureCard';
 import Chip from '../../components/Chip';
 import Tabs from '../../components/Tabs';
+import FAQAccordion from '../../components/FAQAccordion';
 import TimelineCard, { RecruitmentEvent } from '../../components/TimelineCard';
 
 export default function TestComponents() {
@@ -495,6 +496,47 @@ export default function TestComponents() {
           ]}
         />
       </div>
+
+      <div className="flex flex-col gap-6">
+        <h4>FAQ Accordion</h4>
+        <div className="flex flex-col">
+          <FAQAccordion header="When will I hear back about a decision?">
+            <p>
+              Shortly after applications close, we will offer interviews to candidates we are
+              interested in. These interviews serve as a chance to get to know you in person. We
+              have one behavioral and one technical round for each role interview. During these
+              interviews, talk about your past experience and interest in our team. Then, we will
+              initiate some role-specific exercises to further gauge your thought process and skill
+              set.
+            </p>
+          </FAQAccordion>
+          <FAQAccordion header="How do we evaluate the applications?">
+            <p>
+              We look through every single application we receive. We fully understand and
+              don&apos;t expect that everyone has a significant amount of past experience. Taking
+              into account your year and past coursework, we will assess your interest in being a
+              part of Cornell Digital Tech & Innovation and your willingness to learn and make a
+              significant contribution.
+            </p>
+            <p>
+              We have one behavioral and one technical round for each role interview. During these
+              interviews, talk about your past experience and interest in our team. Then, we will
+              initiate some role-specific exercises to further gauge your thought process and skill
+              set.
+            </p>
+          </FAQAccordion>
+          <FAQAccordion header="What could I do to learn more about DTI?">
+            <p>
+              Coffee chats are casual conversations allow you to find out more about DTI and ask any
+              questions about our team. A &lsquo;coffee chat&rsquo; doesn&apos;t need to actually be
+              over coffee, but should be 30 minutes like an actual coffee catch up with a friend.
+              Get the most out of the coffee chat by preparing your questions ahead of time and
+              researching the other person&apos;s experiences first.
+            </p>
+          </FAQAccordion>
+        </div>
+      </div>
+
       <div className="flex flex-col gap-6">
         <h4>Timeline Card</h4>
         <div className="flex flex-col gap-4">
