91242ac478230f1dbf515a1caf5330458908a9f8
Clément <clementproze@gmail.com>
2025-05-23 23:01:21 -0400

    changes to accordion

---
+++ new-dti-website-redesign/src/components/Accordion.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { ReactNode, useRef, useState, useEffect } from 'react';
+import { useId } from 'react';
+
+type FAQAccordionProps = {
+  header: string;
+  children: ReactNode;
+};
+
+export default function Accordion({ header, children }: FAQAccordionProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(contentRef.current.scrollHeight);
+    }
+  }, [isOpen]);
+
+  // generate a unique ID for the accordion
+  const id = useId();
+  const contentId = `accordion-content-${id}`;
+
+  return (
+    <div className="border border-border-1 border-b-0 last:border-b">
+      <h3 className="h6">
+        <button
+          id={`${contentId}-header`} // unique ID for the header, referenced by aria-labelledby in <div>
+          aria-controls={contentId} // points to ID of controlled content region (the sibling <div>)
+          aria-expanded={isOpen}
+          onClick={() => setIsOpen(!isOpen)}
+          className="w-full p-8 flex justify-between items-center cursor-pointer bg-background-1 hover:bg-background-2 transition-colors duration-[120ms] focusState"
+          style={{
+            background: isOpen ? `var(--background-2)` : ''
+          }}
+        >
+          {header}
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            className={`w-6 h-6 flex-shrink-0 transform transition-transform duration-[120ms] ease-out ${
+              isOpen ? '-rotate-180' : ''
+            }`}
+            fill="none"
+          >
+            <path
+              d="M6 9.5L12 15.5L18 9.5"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </h3>
+
+      <div
+        aria-labelledby={`${contentId}-header`} // uses <button>'s ID to label this region
+        id={contentId} // unique ID for content panel, referenced by aria-controls in <button>
+        ref={contentRef}
+        role="region"
+        aria-hidden={!isOpen}
+        style={{
+          height: isOpen ? `${height}px` : '0px'
+        }}
+        className="overflow-hidden transition-all duration-300 ease-in-out bg-background-2"
+      >
+        <div className="p-[32px] pt-0 text-base text-foreground-3">{children}</div>
+      </div>
+    </div>
+  );
+}
