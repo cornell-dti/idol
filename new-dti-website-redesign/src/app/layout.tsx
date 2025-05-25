7832c2f960cf06ccc317ae3fc2f9e3bc7e88bae9
Clément <clementproze@gmail.com>
2025-05-25 16:49:15 +0200

    make more offset

--- new-dti-website-redesign/src/app/layout.tsx
+++ new-dti-website-redesign/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           href="#main-content"
           className={`${baseStyles} absolute -inset-4 opacity-0 z-70
           focus-visible:opacity-100 ${
-            skipToMainOffset ? 'focus-visible:top-16' : 'focus-visible:top-4'
+            skipToMainOffset ? 'focus-visible:top-20' : 'focus-visible:top-4'
           } focus-visible:left-4 transition-all duration-[120ms] px-6 h-12 bg-foreground-1 text-background-1 hover:bg-foreground-2 w-fit`}
         >
           Skip to main content
