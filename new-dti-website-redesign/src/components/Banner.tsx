40a036a6456a022c267ad3c7a88f149f9ddb7864
Clément <clementproze@gmail.com>
2025-05-25 15:19:36 -0400

    [New Website] Mobile nav & banner polish, added "Skip to main" link (#972)
    # Changes
    
    - Made `<Banner>` flushed top to the navbar
    - Fixed bug where you could scroll on rest of page when mobile navbar
    was open
    - Added `id`s to `<main>`
    - added "Skip to main content" links
    
    # Video
    
    
    
    https://github.com/user-attachments/assets/9a52fa39-2e42-4320-8920-1019bfac71a5
    
    # Test plan
    
    ### Skip to main content link
    
    It's a little technical but here are some instructions
    
    - Open `localhost:3000`
    - Press `tab`
      - Ensure that the "Skip to main content" link is visible
    - Press `tab` a few times 
    - Ensure that you're able to navigate around the links **in the navbar**
    (eg: logo, Team, Products, Course, etc.)
    - Reload the page and press `tab` once again
      - Now click `enter`
    - Ensure that when you press `tab` again, you skip over all the links in
    the navbar.
    - The first focused element should be the white CTA button in the hero
    section (eg: "Apply to DTI")
    - Then, using `tab`, navigate to some other page using any of the links 
    - Press `tab` again, and ensure that "Skip to main content" is still the
    first element that you focus on
    
    
    ### Banner
    
    - To test the banner, open the website and resize the window
    - Head to `/apply` 
    - Make sure there isn't an awkward gap between the banner and the navbar
    (like
    [this](https://github.com/cornell-dti/idol/pull/972#discussion_r2101676524)
    – it should be completely flushed)

--- new-dti-website-redesign/src/components/Banner.tsx
+++ new-dti-website-redesign/src/components/Banner.tsx
@@ -5,15 +5,15 @@ type Props = {
 };
 
 const Banner = ({ label }: Props): ReactNode => (
-  <div className="flex items-center justify-center sticky top-20 z-20 min-[1200px]:pt-4 min-[1200px]:mt-[-67px]">
+  <div className="flex items-center justify-center sticky top-[72px] z-20 md:pt-4 md:mt-[-67px] w-fit mx-auto">
     <div
       className="
-        px-5 py-4 md:py-3 text-center w-full min-[1200px]:w-fit
+        px-5 py-4 md:py-3 text-center w-full md:w-fit
         border-t-1 border-b-1 border-t-border-1 border-b-border-1
-        min-[1200px]:border-t min-[1200px]:border-b min-[1200px]:border-border-1-transparent
-        min-[1200px]:rounded-full
-        bg-background-2 min-[1200px]:bg-background-3-transparent
-        backdrop-blur-[32px] min-[1200px]:mx-auto
+        md:border-t md:border-b md:border-border-1-transparent
+        md:rounded-full
+        bg-background-2 md:bg-background-3-transparent
+        backdrop-blur-[32px] md:mx-auto
       "
     >
       <p>{label}</p>
