6c2cb6f80cef03b3c81763e105b222025f84319a
Clément <clementproze@gmail.com>
2025-05-29 00:19:14 +0200

    [New Website] Update `<Accordion>` component, add to "Apply" and Design system library pages (#973)
    # Changes
    
    - Changes to `<Accordion>` component:
      - Made a custom component instead of using `<details>` and `<summary>`
      - Animated opening/closing of accordion
      - Minor stylistic changes including color, padding, border
      - Added prop for icon
    
    - "Apply" page:
    - Added FAQ section, comprised of 3 tabs and their respective accordions
    for Q&A
      - Added Q&A content inside a `.json`
    - Used `react-markdown` package so that the page could parse newlines +
    links from the `.json`
    
    - Design system library:
      - Added `/design-system/accordion` page with examples
    
    # Video
    
    (if the videos don't play try downloading them)
    
    ### New component changes:
    
    https://github.com/user-attachments/assets/87b33a6c-697d-4763-aaae-2625ac226391
    
    
    ### Responsiveness
    
    https://github.com/user-attachments/assets/ed7ae4e1-cb70-403b-8fcf-240c2b3b27e4
    
    
    
    
    
    # Test plan
    - Head to /apply
      - Basic functionality/visuals:
        - Make sure FAQ section is there
        - Make sure switching tabs shows different content
        - Make sure accordions work as expected
    - Make sure the "How do we evaluate applications?" question's content
    has a newline
    - Make sure the "What could I do to learn more about DTI?" question's
    content has a clickable link
        - Make sure the accordions in "Technical Prep" tab have icons
      - Accessibility:
    - Using `tab` on your keyboard, make sure arrow keys navigate you
    between the tabs
    - Using `tab`, make sure `enter` or `space` correctly triggers the
    accordion open/close
        - Make sure focus state outlines appear
      - Responsiveness:
        - Make window width smaller and make sure it flows correctly
    - Head to `/design-system/accordion`
      - Make sure two examples (with and w/o icon) are visible

--- new-dti-website-redesign/src/app/apply/RoleDescriptionCard.tsx
+++ new-dti-website-redesign/src/app/apply/RoleDescriptionCard.tsx
@@ -10,7 +10,7 @@ export default function RoleDescriptionCard({
   responsibilities
 }: RoleDescriptionCardProps) {
   return (
-    <div className="min-[1200px]:w-200 w-fill border-border-1 md:border-x-1 border-y-1 md:rounded-lg flex flex-col overflow-hidden">
+    <div className="min-[800px]:w-200 w-fill border-border-1 md:border-x-1 border-y-1 md:rounded-lg flex flex-col overflow-hidden">
       <div className="p-4 bg-background-2 border-b-1 border-border-1">
         <h3 className="h5">{role} application</h3>
       </div>
