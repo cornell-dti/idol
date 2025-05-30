efe23b9cb42d0751eff9a07344adb50f7cb020fe
jujulcrane <jujulcrane@gmail.com>
2025-05-30 15:03:06 -0400

    format

--- new-dti-website-redesign/src/components/SectionSep.tsx
+++ new-dti-website-redesign/src/components/SectionSep.tsx
@@ -1,6 +1,6 @@
-import XIcon from "../app/design-system/components/icon/XIcon";
-import useIsMobile from "../hooks/useIsMobile";
-import useScreenSize from "../hooks/useScreenSize";
+import XIcon from '../app/design-system/components/icon/XIcon';
+import useIsMobile from '../hooks/useIsMobile';
+import useScreenSize from '../hooks/useScreenSize';
 
 type SectionSepProps = {
   grid?: boolean;
@@ -8,7 +8,11 @@ type SectionSepProps = {
   onClickX?: () => void;
 };
 
-export default function SectionSep({ grid = false, hasX = false, onClickX = () => { } }: SectionSepProps) {
+export default function SectionSep({
+  grid = false,
+  hasX = false,
+  onClickX = () => {}
+}: SectionSepProps) {
   if (!grid) {
     return <div className="w-full h-16 md:h-32 sectionSep border-border-1 border-l-1 border-r-1" />;
   }
@@ -29,7 +33,10 @@ export default function SectionSep({ grid = false, hasX = false, onClickX = () =
                   md:w-[calc(100%/16)] ${hasX && i === lastIndex ? 'relative' : ''}`}
           >
             {hasX && i === lastIndex && (
-              <div className="absolute cursor-pointer inset-0 flex items-center justify-center" onClick={onClickX}>
+              <div
+                className="absolute cursor-pointer inset-0 flex items-center justify-center"
+                onClick={onClickX}
+              >
                 <XIcon />
               </div>
             )}
