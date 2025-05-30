c298b4ebbb0fe4b4cde300ae115c0eb7074478f3
Clément <clementproze@gmail.com>
2025-05-30 18:26:55 +0200

    fix imports

--- new-dti-website-redesign/src/components/FancyTabs/FancyTabs.tsx
+++ new-dti-website-redesign/src/components/FancyTabs/FancyTabs.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { useState, useRef, useEffect } from 'react';
 import TabEdge from './TabEdges';
-import Image from 'next/image';
 
 type Tab = {
   icon?: string;
