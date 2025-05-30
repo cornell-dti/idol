02930357aef3090524f193338099d44d66353d51
Clément <clementproze@gmail.com>
2025-05-30 16:22:28 +0200

    use rocket icon comp

--- new-dti-website-redesign/src/app/design-system/page-sections/feature-section/page.tsx
+++ new-dti-website-redesign/src/app/design-system/page-sections/feature-section/page.tsx
@@ -5,6 +5,7 @@ import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
 import FeatureSection from '@/components/FeatureSection';
 import SectionWrapper from '../../util/SectionWrapper';
+import RocketIcon from '../../components/icon/RocketIcon';
 
 export default function FeatureSectionPage() {
   return (
@@ -46,25 +47,7 @@ export default function FeatureSectionPage() {
       >
         <SectionWrapper>
           <FeatureSection
-            eyebrowIcon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-rocket-icon lucide-rocket"
-              >
-                <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
-                <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
-                <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
-                <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
-              </svg>
-            }
+            eyebrowIcon={<RocketIcon />}
             heading="We are Cornell DTI, a project team"
             description="Founded in 2017, DTI is a project team of 80+ designers, developers, product managers, and business members passionate about making change on campus and beyond."
             image="/rock.png"
@@ -79,25 +62,7 @@ export default function FeatureSectionPage() {
       >
         <SectionWrapper>
           <FeatureSection
-            eyebrowIcon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-rocket-icon lucide-rocket"
-              >
-                <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
-                <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
-                <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
-                <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
-              </svg>
-            }
+            eyebrowIcon={<RocketIcon />}
             heading="We are Cornell DTI, a project team"
             description="Founded in 2017, DTI is a project team of 80+ designers, developers, product managers, and business members passionate about making change on campus and beyond."
             image="/rock.png"
