3952666d18905eadce8b6fc63dc1b7b16fe499f9
Clément <clementproze@gmail.com>
2025-06-03 23:44:33 +0200

    fix lint svg

--- new-dti-website-redesign/src/app/design-system/components/icon/XIcon.tsx
+++ new-dti-website-redesign/src/app/design-system/components/icon/XIcon.tsx
@@ -8,9 +8,9 @@ const XIcon = ({ size = 24 }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path d="M18 6 6 18" />
     <path d="m6 6 12 12" />
