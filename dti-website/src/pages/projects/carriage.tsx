df3af80755d513520065bcb0817d87c60bb3eee3
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-06-23 21:35:40 -0700

    remove deprecated packages

--- dti-website/src/pages/projects/carriage.tsx
+++
@@ -1,5 +0,0 @@
-import DtiProject from '../../components/DtiProject';
-
-export default function Carriage(): JSX.Element {
-  return <DtiProject teamId="carriage" />;
-}
--- dti-website/src/pages/projects/clubview.tsx
+++
@@ -1,5 +0,0 @@
-import DtiProject from '../../components/DtiProject';
-
-export default function ClubView(): JSX.Element {
-  return <DtiProject teamId="clubview" />;
-}
