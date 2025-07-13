dab647bda2ddd6072bd2c2a4cc01de125183a2b7
Clément <clementproze@gmail.com>
2025-07-13 16:53:03 +0200

    new icons

---
+++ new-dti-website-redesign/src/components/icons/CalendarIcon.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type CalendarIconProps = {
+  size?: number;
+  color?: string;
+  className?: string;
+  decoration?: boolean;
+};
+
+const CalendarIcon = ({
+  size = 24,
+  color = 'currentColor',
+  className = '',
+  decoration
+}: CalendarIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className={className}
+    aria-hidden={decoration}
+  >
+    <path d="M16 14v2.2l1.6 1" />
+    <path d="M16 2v4" />
+    <path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5" />
+    <path d="M3 10h5" />
+    <path d="M8 2v4" />
+    <circle cx="16" cy="16" r="6" />
+  </svg>
+);
+
+export default CalendarIcon;
---
+++ new-dti-website-redesign/src/components/icons/PinIcon.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type PinIconProps = {
+  size?: number;
+  color?: string;
+  className?: string;
+  decoration?: boolean;
+};
+
+const PinIcon = ({
+  size = 24,
+  color = 'currentColor',
+  className = '',
+  decoration
+}: PinIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className={className}
+    aria-hidden={decoration}
+  >
+    <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+    <circle cx="12" cy="10" r="3" />
+  </svg>
+);
+
+export default PinIcon;
