5f22fd199fdd0895bf3e59f22c618ce36b13deb2
Clément <clementproze@gmail.com>
2025-08-17 23:56:16 +0200

    [New Website] Quick fixes (#1015)
    # Changes
    
    - Updated Trends instructors
    - Changed Trends timeline to TBD for now
    - Removed capitalization of "Chat"
    
    <img width="1022" height="958" alt="image"
    src="https://github.com/user-attachments/assets/555fbe5e-651c-4522-8a8b-a40ef734deb4"
    />
    
    <img width="1176" height="841" alt="Screenshot 2025-08-17 at 3 17 12 PM"
    src="https://github.com/user-attachments/assets/c63ea9a5-5e8b-41b8-9a46-8f35641a16d7"
    />

--- new-dti-website-redesign/src/app/team/page.tsx
+++ new-dti-website-redesign/src/app/team/page.tsx
@@ -50,7 +50,7 @@ export default function Team() {
         subheading="Be part of something greater today."
         button1Label="Apply to DTI"
         button1Link="/apply"
-        button2Label="Coffee Chat with us"
+        button2Label="Coffee chat with us"
         button2Link="https://docs.google.com/spreadsheets/d/1fgVngmjJIivQ0Rnvcg3VMtu0kNQzCYdEyLfrvUX5gTQ/edit?usp=sharing"
         button2LinkNewTab={true}
       />
