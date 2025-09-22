8f4d212df7efc2ad61d42a896b1a0ea6890af8ef
Oscar Wang <oscarwang22396@gmail.com>
2025-09-21 22:48:57 -0400

    use ul and li

--- new-dti-website-redesign/src/app/products/ProductNav.tsx
+++ new-dti-website-redesign/src/app/products/ProductNav.tsx
@@ -59,45 +59,49 @@ export default function ProductNav({
     >
       <div className="absolute left-0 top-0 bottom-0 w-4 bg-gradient-to-r from-background-1 to-transparent pointer-events-none z-10"></div>
       <div className="absolute right-0 top-0 bottom-0 w-4 bg-gradient-to-l from-background-1 to-transparent pointer-events-none z-10"></div>
-      <div className="flex flex-row overflow-scroll h-full items-center pl-4 gap-x-2 fancyTabsContainer">
+      <ul className="flex flex-row overflow-scroll h-full items-center pl-4 gap-x-2 fancyTabsContainer">
         {products.map((product, index) => (
-          <Button
-            variant={activeProduct === product.id ? 'primary' : 'tertiary'}
-            label={product.name}
-            key={product.name}
-            onClick={(e) =>
-              handleMobileMenuClick(product.id, e as React.MouseEvent<HTMLButtonElement>)
-            }
-            ref={(el) => {
-              buttonRefs.current[index] = el as HTMLButtonElement;
-            }}
-          >
-            {product.name}
-          </Button>
+          <li key={product.name} className="flex flex-row items-center gap-x-2">
+            <Button
+              variant={activeProduct === product.id ? 'primary' : 'tertiary'}
+              label={product.name}
+              key={product.name}
+              onClick={(e) =>
+                handleMobileMenuClick(product.id, e as React.MouseEvent<HTMLButtonElement>)
+              }
+              ref={(el) => {
+                buttonRefs.current[index] = el as HTMLButtonElement;
+              }}
+            >
+              {product.name}
+            </Button>
+          </li>
         ))}
-      </div>
+      </ul>
     </aside>
   ) : (
     <aside className="flex flex-col sticky top-[80px] h-fit w-104 px-12 gap-2 py-4">
-      {products.map((product) => (
-        <div key={product.name} className="flex flex-row items-center gap-x-2">
-          <ArrowLogoIcon
-            className={`rotate-90 ml-[-24px] transition-all duration-200 ease-in-out ${
-              activeProduct === product.id ? 'opacity-100 scale-100' : 'opacity-0 scale-75'
-            }`}
-          />
-          <Link
-            href={`#${product.id}`}
-            key={product.name}
-            className={`flex flex-row gap-x-2 cursor-pointer select-none items-center ${activeProduct === product.id ? 'text-foreground-1 pl-1' : 'text-foreground-3'} transition-all duration-300 ease-in-out`}
-          >
-            <p className="h6">{product.name}</p>
-            {product.comingSoon && (
-              <Chip label="soon" color="red" allCaps className="h-7 justify-center" />
-            )}
-          </Link>
-        </div>
-      ))}
+      <ul className="flex flex-col gap-2">
+        {products.map((product) => (
+          <li key={product.name} className="flex flex-row items-center gap-x-2">
+            <ArrowLogoIcon
+              className={`rotate-90 ml-[-24px] transition-all duration-200 ease-in-out ${
+                activeProduct === product.id ? 'opacity-100 scale-100' : 'opacity-0 scale-75'
+              }`}
+            />
+            <Link
+              href={`#${product.id}`}
+              key={product.name}
+              className={`flex flex-row gap-x-2 cursor-pointer select-none items-center ${activeProduct === product.id ? 'text-foreground-1 pl-1' : 'text-foreground-3'} transition-all duration-300 ease-in-out`}
+            >
+              <p className="h6">{product.name}</p>
+              {product.comingSoon && (
+                <Chip label="soon" color="red" allCaps className="h-7 justify-center" />
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
