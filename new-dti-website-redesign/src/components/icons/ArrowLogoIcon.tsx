29a71212753b8960a0d6a4c87c4fb5b9785467ca
Oscar Wang <oscarwang22396@gmail.com>
2025-09-25 21:20:41 -0400

    Product navbar (#1053)
    ### Summary <!-- Required -->
    
    - implements the product navbar
    - adds `clsx` utility to merge className more safely
    
    ### Notion/Figma Link <!-- Optional -->
    
    <!-- If the changes have associated Notion pages/Figma design(s), please
    include the links here.-->
    
    ### Test Plan <!-- Required -->
    
    <!-- Provide screenshots or point out the additional unit tests -->
    
    ### Notes <!-- Optional -->
    
    <!--- List any important or subtle points, future considerations, or
    other items of note. -->
    
    ### Breaking Changes <!-- Optional -->
    
    <!-- Keep items that apply: -->
    
    ---------
    
    Co-authored-by: Clément <clementproze@gmail.com>

--- new-dti-website-redesign/src/components/icons/ArrowLogoIcon.tsx
+++ new-dti-website-redesign/src/components/icons/ArrowLogoIcon.tsx
@@ -3,10 +3,18 @@ import Image from 'next/image';
 type ArrowLogoIconProps = {
   width?: number;
   height?: number;
+  className?: string;
 };
 
-const ArrowLogoIcon = ({ width = 20, height = 20 }: ArrowLogoIconProps) => (
-  <Image src="/arrowLogo.svg" alt="" width={width} height={height} className="min-w-5 min-h-5" />
+const ArrowLogoIcon = ({ width = 20, height = 20, ...props }: ArrowLogoIconProps) => (
+  <Image
+    src="/arrowLogo.svg"
+    alt=""
+    width={width}
+    height={height}
+    className={`min-w-5 min-h-5 ${props.className}`}
+    {...props}
+  />
 );
 
 export default ArrowLogoIcon;
