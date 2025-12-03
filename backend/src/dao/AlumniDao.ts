9de737372edd4a0077c90bc9c0e2797cbc6f93d4
Esha Shah <es999@cornell.edu>
2025-12-03 18:35:25 -0500

    create alumni API

--- backend/src/dao/AlumniDao.ts
+++ backend/src/dao/AlumniDao.ts
@@ -4,7 +4,7 @@ import BaseDao from './BaseDao';
 import { ALUM_DTI_ROLES, ALUM_JOB_CATEGORY } from '../../../common-types/constants';
 import { BadRequestError } from '../utils/errors';
 
-function validateDtiRole(r: any): AlumDtiRole {
+function validateDtiRole(r: string): AlumDtiRole {
   const dr = r as AlumDtiRole;
   if (!ALUM_DTI_ROLES.includes(dr)) {
     throw new BadRequestError(`Role ${r} is invalid.`);
@@ -12,7 +12,7 @@ function validateDtiRole(r: any): AlumDtiRole {
   return dr;
 }
 
-function validateJobCategory(j: any): AlumJobCategory {
+function validateJobCategory(j: string): AlumJobCategory {
   const jc = j as AlumJobCategory;
   if (!ALUM_JOB_CATEGORY.includes(jc)) {
     throw new BadRequestError(`Job category ${j} is invalid.`);
@@ -25,7 +25,15 @@ function dbAlumniToAlumni(db: DBAlumni): Alumni {
     ...db,
     dtiRoles: db.dtiRoles ? db.dtiRoles.map(validateDtiRole) : null,
     jobCategory: validateJobCategory(db.jobCategory)
-  } satisfies Alumni;
+  } as Alumni;
+}
+
+function alumniToDbAlumni(alum: Alumni): DBAlumni {
+  return {
+    ...alum,
+    dtiRoles: alum.dtiRoles ? [...alum.dtiRoles] : null,
+    jobCategory: alum.jobCategory
+  } as DBAlumni;
 }
 
 export default class AlumniDao extends BaseDao<DBAlumni, DBAlumni> {
@@ -40,22 +48,17 @@ export default class AlumniDao extends BaseDao<DBAlumni, DBAlumni> {
   /**
    * Gets all alumni
    * @returns An `Alumni` list with all current alumni
-   * @throws `BadRequestError` if fields do not match their types
+   * @throws `BadRequestError` if fields stored in `DBAlumni` do not match their types in `Alumni`
    */
   async getAllAlumni(): Promise<Alumni[]> {
-    return alumniCollection.get().then((vals) =>
-      vals.docs.map((doc) => {
-        const db = doc.data() as DBAlumni;
-        return dbAlumniToAlumni(db);
-      })
-    );
+    return this.getDocuments().then((vals) => vals.map((val) => dbAlumniToAlumni(val)));
   }
 
   /**
    * Gets an alumni from their uuid
    * @param uuid - The uuid
    * @returns The `Alumni` with a matching uuid (null if they don't exist)
-   * @throws `BadRequestError` if fields do not match their types
+   * @throws `BadRequestError` if fields stored in `DBAlumni` do not match their types in `Alumni`
    */
   async getAlumni(uuid: string): Promise<Alumni | null> {
     return this.getDocument(uuid).then((val) => {
@@ -70,24 +73,22 @@ export default class AlumniDao extends BaseDao<DBAlumni, DBAlumni> {
    * Creates a new alumni document
    * @param alumni - The alumni data
    * @returns The newly created 'Alumni'
-   * @throws `BadRequestError` if fields do not match their types
+   * @throws `BadRequestError` if fields stored in `DBAlumni` do not match their types in `Alumni`
    */
-  async createAlumni(alumni: DBAlumni): Promise<Alumni> {
-    return this.createDocument(alumni.uuid, alumni).then((val) => {
-      return dbAlumniToAlumni(val);
-    });
+  async createAlumni(alumni: Alumni): Promise<Alumni> {
+    const dbAlumni = alumniToDbAlumni(alumni);
+    return this.createDocument(dbAlumni.uuid, dbAlumni).then((val) => dbAlumniToAlumni(val));
   }
 
   /**
    * Updates alumni information given their uuid
    * @param alumni - The updated alumni data
    * @returns The updated `Alumni`
-   * @throws `BadRequestError` if fields do not match their types
+   * @throws `BadRequestError` if fields stored in `DBAlumni` do not match their types in `Alumni`
    */
-  async updateAlumni(alumni: DBAlumni): Promise<Alumni> {
-    return this.updateDocument(alumni.uuid, alumni).then((val) => {
-      return dbAlumniToAlumni(val);
-    });
+  async updateAlumni(alumni: Alumni): Promise<Alumni> {
+    const dbAlumni = alumniToDbAlumni(alumni);
+    return this.updateDocument(dbAlumni.uuid, dbAlumni).then((val) => dbAlumniToAlumni(val));
   }
 
   /**
