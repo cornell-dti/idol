cd8b0c1d0d317d92fa805ddd2b5946627c5bed90
Max Shi <mshi0107@gmail.com>
2026-02-24 00:53:44 -0500

    formatted with yarn format, fixed datetime logic, and standardized code based on suggestions from Adrienne.

--- backend/scripts/send-dev-portfolio-notifications.ts
+++ backend/scripts/send-dev-portfolio-notifications.ts
@@ -2,6 +2,7 @@
 import admin from 'firebase-admin';
 import sendMail from './utils';
 import { configureAccount } from '../src/utils/firebase-utils';
+
 require('dotenv').config();
 
 const serviceAcc = require('../resources/idol-b6c68-firebase-adminsdk-h4e6t-40e4bd5536.json');
@@ -15,75 +16,84 @@ admin.initializeApp({
 const db = admin.firestore();
 
 const main = async () => {
-    // current date: 12PM the day before the deadline (created in EST)
-    const currentDate = new Date();
-    // compute 11:59:59.999 the day after (end of day of the deadline)
-    const deadline = new Date(currentDate);
-    deadline.setDate(currentDate.getDate() + 1);
-    deadline.setHours(23, 59, 59, 999);
-    // compute start of that same day (Sunday 00:00:00.000)
-    const startOfDeadlineDay = new Date(deadline);
-    startOfDeadlineDay.setHours(0, 0, 0, 0);
+  // current date: 12:00:00 EST the day before the deadline (17:00:00 UTC)
+  const currentDate = new Date();
+  // compute 23:59:59.999 EST the day after, i.e. 4:59:59.999 UTC two days after (end of day of the deadline)
+  const deadline = new Date(currentDate);
+  deadline.setDate(currentDate.getDate() + 2);
+  deadline.setHours(4, 59, 59, 999);
 
-    // query Firestore for dev portfolios due on that day
-    console.log('querying for dev portfolios due tomorrow...');
-    const devPortfolioSnapshot = await db.collection('dev-portfolio')
-        .where('deadline', '>=', startOfDeadlineDay.getTime())
-        .where('deadline', '<=', deadline.getTime())
-        .get();
+  // query Firestore for dev portfolios due on that day
+  console.log('querying for dev portfolios due tomorrow...');
+  const devPortfolioSnapshot = await db
+    .collection('dev-portfolio')
+    .where('deadline', '>=', currentDate.getTime())
+    .where('deadline', '<=', deadline.getTime())
+    .get();
 
-    if (devPortfolioSnapshot.empty) {
-        console.error('Error: No dev portfolios found due tomorrow');
-        process.exit(1);
-    } else if (devPortfolioSnapshot.size != 1){
-        console.error('Error: Multiple dev portfolios found due tomorrow');
-        process.exit(1);
-    } else {
-        console.log('One dev portfolio found due tomorrow, success!')
-    }
+  if (devPortfolioSnapshot.empty) {
+    console.log('No dev portfolios found due tomorrow, exiting...');
+    process.exit(0);
+  } else if (devPortfolioSnapshot.size !== 1) {
+    console.error('Error: Multiple dev portfolios found due tomorrow');
+    process.exit(1);
+  } else {
+    console.log('One dev portfolio found due tomorrow, success!');
+  }
 
-    const devPortfolioDoc = devPortfolioSnapshot.docs[0]; // first (and only) document
-    const devPortfolioData = devPortfolioDoc.data();
-    const submissions = devPortfolioData.submissions; // the submissions array
-    const devPortfolioName = devPortfolioData.name;
+  const devPortfolioDoc = devPortfolioSnapshot.docs[0]; // first (and only) document
+  const devPortfolioData = devPortfolioDoc.data();
+  const { submissions } = devPortfolioData; // the submissions array
+  const devPortfolioName = devPortfolioData.name;
+  if (devPortfolioData.name.toLowerCase().includes('extensions')) {
+    console.log('Notifications not required for dev portfolio extensions, exiting...');
+    process.exit(0);
+  }
 
-    const devsSubmitted = await Promise.all( 
-        submissions.map(async submission => {
-            let memberRef = submission.member;
-            const memberDoc = await memberRef.get();
-            if (memberDoc.exists) {
-                const memberData = memberDoc.data();
-                return memberData.email;
-            } else {
-                console.log("Member document does not exist");
-            }
-        })
-    );
+  const devsSubmitted = (
+    await Promise.all(
+      submissions.map(async (submission) => {
+        const memberRef = submission.member;
+        const memberDoc = await memberRef.get();
 
-    const EMAIL_SUBJECT = '[ACTION REQUIRED] Dev Portfolio Submission';
-    const EMAIL_BODY = `If you are not taking DTI for credit this semester, please ignore.\n\nThis is a reminder to submit your portfolio for ${devPortfolioName} which is due by ${deadline.toLocaleString()}. You must contact leads if you require an extension.`;
+        if (!memberDoc.exists) {
+          console.log('Member document does not exist');
+          return null;
+        }
 
-    const allDevsEmails = ( await db.collection('members')
-        .where('role', 'in', ['developer', 'tpm'])
-        .get() 
-        .then(snapshot => snapshot.docs.map(doc => doc.data()))
-        .then(devs => devs.map(dev => dev.email))
-    );
+        const memberData = memberDoc.data();
+        return memberData.email;
+      })
+    )
+  ).filter((email): email is string => email !== null);
 
-    const devsNotSubmitted = allDevsEmails.filter(email => !devsSubmitted.includes(email));
-    console.log("Devs submitted count: " + [...new Set(devsSubmitted)].length)
-    console.log("Devs not submitted count: " + [...new Set(devsNotSubmitted)].length);
-    console.log("All devs count: " + allDevsEmails.length);
-    console.log(`${devsNotSubmitted.length} devs not submitted a portfolio this week, sending email notification...`);
-    try {
-      await Promise.all(
-        devsNotSubmitted.map((email) => sendMail(email, EMAIL_SUBJECT, EMAIL_BODY))
-      );
-    } catch (e) {
-      console.error(e);
-      process.exit(1);
-    }
-    console.log('Email notification successfully sent :)');
+  const EMAIL_SUBJECT = '[ACTION REQUIRED] Dev Portfolio Submission';
+  const deadlineString = devPortfolioData.deadline.toLocaleString('en-US', {
+    timeZone: 'America/New_York'
+  });
+  const EMAIL_BODY = `If you are not taking DTI for credit this semester, please ignore.\n\nThis is a reminder to submit your portfolio for ${devPortfolioName} which is due by ${deadlineString}. You must contact leads if you require an extension.`;
+
+  const allDevsEmails = await db
+    .collection('members')
+    .where('role', 'in', ['developer', 'tpm'])
+    .get()
+    .then((snapshot) => snapshot.docs.map((doc) => doc.data()))
+    .then((devs) => devs.map((dev) => dev.email));
+
+  const devsNotSubmitted = allDevsEmails.filter((email) => !devsSubmitted.includes(email));
+  console.log(`Devs submitted count: ${[...new Set(devsSubmitted)].length}`);
+  console.log(`Devs not submitted count: ${[...new Set(devsNotSubmitted)].length}`);
+  console.log(`All devs count: ${allDevsEmails.length}`);
+  console.log(
+    `${devsNotSubmitted.length} devs not submitted a portfolio this week, sending email notification...`
+  );
+  try {
+    await Promise.all(devsNotSubmitted.map((email) => sendMail(email, EMAIL_SUBJECT, EMAIL_BODY)));
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+  console.log('Email notification successfully sent :)');
 };
 
 main();
