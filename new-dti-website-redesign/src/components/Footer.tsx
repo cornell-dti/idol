a842343229eeb07f40322c04be2f96da1b3e8cc2
Clement Roze <clementproze@gmail.com>
2026-09-09 00:18:09 -0400

    Remove unused import for Chip component

--- new-dti-website-redesign/src/components/Footer.tsx
+++ new-dti-website-redesign/src/components/Footer.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from './Button';
-import Chip from './Chip';
 import MailIcon from './icons/MailIcon';
 import GitHubIcon from './icons/GitHubIcon';
 import FacebookIcon from './icons/FacebookIcon';
