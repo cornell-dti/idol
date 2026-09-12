f0bf1cc03b8cbc8ed1f68c95dbad2a96f8b08f28
zkdlinyne <155908628+zkdlinyne@users.noreply.github.com>
2026-09-11 20:38:59 -0400

    adjusted text for consistency

--- frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
+++ frontend/src/components/Forms/TeamEventCreditsForm/TeamEventCreditsForm.tsx
@@ -231,8 +231,8 @@ const TeamEventCreditForm: React.FC = () => {
           <div className={styles.bold}>
             {tecConfig.considerEventKind && hasRemainingKindCredits && (
               <span className={styles.red_color}>
-                You need {remainingInternalCredits} more internal and {remainingExternalCredits}{' '}
-                more external this period.
+                You must submit at least {remainingInternalCredits} more internal and {remainingExternalCredits}{' '}
+                more external TEC in the current period.
               </span>
             )}
             {tecConfig.considerEventKind && !hasRemainingKindCredits && (
