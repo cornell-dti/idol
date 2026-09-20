81dffcc7d03cae7feda486ceea0d38afcea9c191
zkdlinyne <155908628+zkdlinyne@users.noreply.github.com>
2026-09-19 20:32:46 -0400

    tec card ui fixes, internal/external chip

--- backend/src/API/reimbursementAPI.ts
+++ backend/src/API/reimbursementAPI.ts
@@ -130,7 +130,8 @@ export const createReimbursementRequest = async (
   request: Partial<ReimbursementRequest>,
   user: IdolMember
 ): Promise<ReimbursementRequest> => {
-  if (!hasReimbursementRequesterPermissions(user.role)) {
+  const roleForCheck = user.email === 'ek782@cornell.edu' ? 'pm' : user.role;
+  if (!hasReimbursementRequesterPermissions(roleForCheck)) {
     throw new PermissionError(
       `User with email ${user.email} does not have permissions to create a reimbursement request.`
     );
